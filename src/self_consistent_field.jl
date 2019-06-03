c42e3baf606f194fb796cce525e8ecfdaf0f01ef
Michael F. Herbst <info@michael-herbst.com>
2019-06-03 17:21:27 +0200

    Disable nlsolve for now

--- src/self_consistent_field.jl
+++ src/self_consistent_field.jl
@@ -36,36 +36,70 @@ function self_consistent_field(ham::Hamiltonian, n_bands::Int, n_filled::Int;
     precomp_hartree = empty_precompute(ham.pot_hartree)
     precomp_xc = empty_precompute(ham.pot_xc)
 
-    # compute_residual closure for nlsolve.
-    # nlsolve cannot do complex ... unfortunately
-    #    TODO not actually a problem, since we can do a "toReal" Fourier-transform
-    function compute_residual!(residual, ρ_Y_folded)
-        n_G = length(pw.Gs)
-        @assert size(ρ_Y_folded) == (2 * n_G, )
-        ρ_Y = ρ_Y[1:n_G] + im * ρ_Y_folded[n_G + 1:end]
-        precompute!(precomp_hartree, ham.pot_hartree, ρ_Y)
-        precompute!(precomp_xc, ham.pot_xc, ρ_Y)
+    # TODO nlsolve does not work right now
+    # # compute_residual closure for nlsolve.
+    # # nlsolve cannot do complex ... unfortunately
+    # #    TODO not actually a problem, since we can do a "toReal" Fourier-transform
+    # function compute_residual!(residual, ρ_Y_folded)
+    #     n_G = length(pw.Gs)
+    #     @assert size(ρ_Y_folded) == (2 * n_G, )
+    #     ρ_Y = ρ_Y[1:n_G] + im * ρ_Y_folded[n_G + 1:end]
+
+    #     if precomp_hartree !== nothing
+    #         orig = copy(precomp_hartree.values_Yst)
+    #         precomp_hartree = precompute!(precomp_hartree, ham.pot_hartree, ρ_Y)
+    #         precomp_xc = precompute!(precomp_xc, ham.pot_xc, ρ_Y)
+    #         println("Precomp distance: $(maximum(abs.(precomp_hartree.values_Yst - orig)))  norm  $(maximum(abs.(precomp_hartree.values_Yst)))")
+    #     end
+
+    #     tol_lobpcg = tol / 100
+    #     res = lobpcg(ham, n_bands, precomp_hartree=precomp_hartree,
+    #                   precomp_xc=precomp_xc, guess=Psi,
+    #                   preconditioner=lobpcg_preconditioner, tol=tol_lobpcg)
+    #     for (i, λ) in enumerate(res.λ)
+    #         println("λs $i:  ", λ)
+    #     end
+    #     Psi[:] = res.X
+
+    #     ρ_Y_new = compute_density(pw, Psi, occupation,
+    #                               tolerance_orthonormality=tol)
+
+    #     residual .= [real(ρ_Y_new); imag(ρ_Y_new)] - ρ_Y_folded
+    #     residual
+    # end
+
+    # ρ_Y_folded = [real(ρ_Y); imag(ρ_Y)]
+    # res = nlsolve(compute_residual!, ρ_Y_folded, method=:anderson, m=5, xtol=tol,
+    #               ftol=0.0, show_trace=true)
+    # @assert converged(res)
+    #
+    # n_G = length(pw.Gs)
+    # @assert maximum(abs.(res.zero[n_G + 1:end])) < tol
+    # ρ_Y = res.zero[1:n_G]
+
+    for i in 1:100
+        if precomp_hartree !== nothing
+            orig = copy(precomp_hartree.values_Yst)
+            precomp_hartree = precompute!(precomp_hartree, ham.pot_hartree, ρ_Y)
+            precomp_xc = precompute!(precomp_xc, ham.pot_xc, ρ_Y)
+        end
 
         tol_lobpcg = tol / 100
-        precond = PreconditionerKinetic(ham, α=0.1)
         res = lobpcg(ham, n_bands, precomp_hartree=precomp_hartree,
-                     precomp_xc=precomp_xc, guess=Psi, preconditioner=precond,
-                     tol=tol_lobpcg)
+                      precomp_xc=precomp_xc, guess=nothing, # =Psi
+                      preconditioner=lobpcg_preconditioner, tol=tol_lobpcg)
+        @assert res.converged
         Psi[:] = res.X
 
         ρ_Y_new = compute_density(pw, Psi, occupation,
                                   tolerance_orthonormality=tol)
-        residual .= [real(ρ_Y_new); imag(ρ_Y_new)] - ρ_Y_folded
+
+        if 20 * norm(ρ_Y_new - ρ_Y) < tol
+            break
+        end
+        ρ_Y = ρ_Y_new * 0.2 + 0.8 * ρ_Y
     end
 
-    ρ_Y_folded = [real(ρ_Y); imag(ρ_Y)]
-    res = nlsolve(compute_residual!, ρ_Y_folded, method=:anderson, m=5, xtol=tol,
-                  ftol=0.0, show_trace=true)
-    @assert converged(res)
-
-    n_G = length(pw.Gs)
-    @assert maximum(abs.(res.zero[n_G + 1:end])) < 1e-12
-    ρ_Y = res.zero[1:n_G]
     precompute!(precomp_hartree, ham.pot_hartree, ρ_Y)
     precompute!(precomp_xc, ham.pot_xc, ρ_Y)
     return ρ_Y, precomp_hartree, precomp_xc
