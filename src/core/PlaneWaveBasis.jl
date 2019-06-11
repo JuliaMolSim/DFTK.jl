53df7770de81008b80940fb1bde7be280c38c2c5
Michael F. Herbst <info@michael-herbst.com>
2019-06-11 15:13:42 +0200

    Fixup FFT in PlaneWaveBasis

--- src/core/PlaneWaveBasis.jl
+++ src/core/PlaneWaveBasis.jl
@@ -1,4 +1,5 @@
 using FFTW
+using StaticArrays
 
 struct PlaneWaveBasis{T <: Real}
     # Lattice and reciprocal lattice vectors in columns
@@ -27,7 +28,7 @@ struct PlaneWaveBasis{T <: Real}
 end
 
 @doc raw"""
-    PlaneWaveBasis(lattice::SMatrix{3, 3, T}, grid_size::SVector{3, I},
+    PlaneWaveBasis(lattice::SMatrix{3, 3, T, 9}, grid_size::SVector{3, I},
                    Ecut::Number, kpoints, kweights) where {T <: Real, I <: Integer}
 
 Create a plane-wave basis from a specification for the Fourier grid size
@@ -54,39 +55,42 @@ julia> b = PlaneWaveBasis(TODO)
 """
 function PlaneWaveBasis(lattice::AbstractMatrix{T}, grid_size,
                         Ecut::Number, kpoints, kweights) where {T <: Real}
-    lattice = SMatrix{3, 3, T}(lattice)
+    lattice = SMatrix{3, 3, T, 9}(lattice)
     recip_lattice = 2π * inv(lattice')
 
     # Plan a FFT, spending some time on finding an optimal algorithm
     # for the machine on which the computation runs
-    grid_size = SVector{3, Int}(grid_size)
     tmp = Array{Complex{T}}(undef, grid_size...)
     fft_plan = plan_fft!(tmp, flags=FFTW.MEASURE)
     ifft_plan = plan_ifft!(tmp, flags=FFTW.MEASURE)
 
     pw = PlaneWaveBasis{T}(lattice, recip_lattice, det(lattice), det(recip_lattice),
-                           grid_size, Ecut, [], [], [], fft_plan, ifft_plan)
+                           Ecut, grid_size, [], [], [], fft_plan, ifft_plan)
     set_kpoints!(pw, kpoints, kweights)
 end
 
 """
 Reset the kpoints of an existing Plane-wave basis and change the basis accordingly
 """
-function set_kpoints!(pw::PlaneWaveBasis{T}, kpoints, kweights) where T
+function set_kpoints!(pw::PlaneWaveBasis{T}, kpoints, kweights; Ecut=pw.Ecut) where T
     @assert(length(kpoints) == length(kweights),
             "Lengths of kpoints and length of kweights need to agree")
     @assert sum(kweights) ≈ 1 "kweights are assumed to be normalized."
+    max_E = sum(abs2, pw.recip_lattice * floor.(Int, pw.grid_size ./ 2)) / 2
+    @assert(Ecut ≤ max_E, "Ecut should be less than the maximal kinetic energy " *
+            "the grid supports (== $max_E)")
+
     resize!(pw.kpoints, length(kpoints)) .= kpoints
     resize!(pw.kweights, length(kweights)) .= kweights
+    resize!(pw.wfctn_basis, length(kpoints))
 
-    # Update kbasis: For each k-Point select those G coords,
+    # Update wfctn_basis: For each k-Point select those G coords,
     # satisfying the energy cutoff
     for (ik, k) in enumerate(kpoints)
         energy(q) = sum(abs2, pw.recip_lattice * q) / 2
         p = [G for G in gcoords(pw) if energy(k + G) ≤ pw.Ecut]
-        pw.kbasis[ik] = [G for G in gcoords(pw) if energy(k + G) ≤ pw.Ecut]
+        pw.wfctn_basis[ik] = [G for G in gcoords(pw) if energy(k + G) ≤ pw.Ecut]
     end
-
     pw
 end
 
@@ -104,34 +108,25 @@ function gcoords(pw::PlaneWaveBasis)
 end
 
 
-
 #
 # Perform FFT
 #
-# TODO Different function names for these, try to do a to-real transform
-"""
-Perform an in-place FFT to translate between the X_k or X basis
-and the real-space Y* grid.
+@doc raw"""
+    G_to_R!(pw::PlaneWaveBasis, f_fourier, f_real[, gcoords])
 
-# Arguments
-- `pw`          PlaneWaveBasis object
-- `f_fourier`   input argument, the representation of F on X or X_k
-- `f_real`      output argument, the resulting representation of F on Y*
-                (all data in this array will be destroyed)
-- `idx_to_fft`  Map applied to translate f_fourier from the
-                density basis Y or the k-point-specific part of the
-                (smaller) orbital basis X_k to the (larger) potential
-                basis Y. In the case of X_k this involves zero padding
-                as well.
+Perform an in-place FFT to translate between `f_fourier`, a fourier representation
+of a function using the wave vectors specified in `gcoords` and a representation
+on the real-space density grid ``B^∗_ρ``. The function will destroy all data
+in `f_real`. If `gcoords` is absent, the full density grid ``B_ρ`` is used.
 """
-function G_to_R!(pw::PlaneWaveBasis, f_fourier, f_real, idx_to_fft)
-    @assert length(f_fourier) == length(idx_to_fft)
-    @assert size(f_real) == size(pw.grid_Yst)
+function G_to_R!(pw::PlaneWaveBasis, f_fourier, f_real; gcoords=gcoords(pw))
+    @assert length(f_fourier) == length(gcoords)
+    @assert size(f_real) == Tuple(pw.grid_size)
 
-    # Pad the input data to reach the full size of the Y grid,
-    # then perform an FFT Y -> Y*
+    # Pad the input data, then perform an FFT on the rectangular cube
     f_real .= 0
-    for (ig, idx_fft) in enumerate(idx_to_fft)
+    for (ig, G) in enumerate(gcoords)
+        idx_fft = 1 .+ mod.(G, pw.grid_size)
         f_real[idx_fft...] = f_fourier[ig]
     end
     f_real = pw.iFFT * f_real  # Note: This destroys data in f_real
@@ -140,76 +135,32 @@ function G_to_R!(pw::PlaneWaveBasis, f_fourier, f_real, idx_to_fft)
     # but the normalisation convention used in this code is
     # e_G(x) = e^iGx / sqrt(|Γ|), so we need to use the factor
     # below in order to match both conventions.
-    f_real .*= prod(size(pw.grid_Yst))
-end
-
-"""
-Perform an in-place FFT to translate between a representation of
-a function f in the Y basis and the real-space Y* grid.
-All data in `f_Yst` will be destroyed.
-"""
-function Y_to_Yst!(pw::PlaneWaveBasis, f_Y, f_Yst)
-    G_to_R!(pw, f_Y, f_Yst, pw.idx_to_fft)
-end
-
-"""
-Perform an in-place FFT to translate between a representation of
-a function f in the X_k basis specific to the k-Point with index `ik`
-and the real-space Y* grid.
-All data in `f_Yst` will be destroyed.
-"""
-function Xk_to_Yst!(pw::PlaneWaveBasis, ik::Int, f_Xk, f_Yst)
-    G_to_R!(pw, f_Xk, f_Yst, pw.idx_to_fft[pw.kmask[ik]])
+    f_real .*= prod(size(pw.grid_size))
 end
 
 
-"""
-Perform an in-place iFFT to translate between the Y* grid and the
-X_k or X basis.
+@doc raw"""
+    R_to_G!(pw::PlaneWaveBasis, f_real, f_fourier[, gcoords])
 
-# Arguments
-- `pw`          PlaneWaveBasis object
-- `f_real`      input argument, the representation of F on Y*
-                (all data in this array will be destroyed)
-- `f_fourier`   output argument, the resulting representation of F
-                on X or X_k (all data in this array will be destroyed)
-- `idx_to_fft`  Map applied to translate between the representation
-                of f in the potential basis Y, i.e. the result from
-                applying the discrete Fourier transform to `f_fourier`,
-                and the basis X_k or Y. In the case of X_k, where the
-                basis is smaller, this involves truncation as well.
+Perform an in-place FFT to translate between `f_real`, a representation of a
+function on the real-space density grid ``B^∗_ρ`` and a fourier representation
+using the wave vectors specified in `gcoords`. If `gcoords` is less than the
+wave vectors required to exactly represent `f_real`, than this function implies
+a truncation. On call all data in `f_real` and `f_fourier` will be destroyed.
+If `gcoords` is absent, the full density grid ``B_ρ`` is used.
 """
-function R_to_G!(pw::PlaneWaveBasis, f_real, f_fourier, idx_to_fft)
-    @assert length(f_fourier) == length(idx_to_fft)
-    @assert size(f_real) == size(pw.grid_Yst)
+function R_to_G!(pw::PlaneWaveBasis, f_real, f_fourier; gcoords=gcoords(pw))
+    @assert length(f_fourier) == length(gcoords)
+    @assert size(f_real) == Tuple(pw.grid_size)
 
     # Do FFT on the full FFT plan, but truncate the resulting frequency
     # range to the part defined by the idx_to_fft array
     f_fourier_extended = pw.FFT * f_real  # This destroys data in f_real
     f_fourier .= 0
-    for (ig, idx_fft) in enumerate(idx_to_fft)
+    for (ig, G) in enumerate(gcoords)
+        idx_fft = 1 .+ mod.(G, pw.grid_size)
         f_fourier[ig] = f_fourier_extended[idx_fft...]
     end
     # Again adjust normalisation as in G_to_R
-    f_fourier .*= 1 / prod(size(pw.grid_Yst))
-end
-
-
-"""
-Perform an in-place iFFT to translate between a representation of
-a function f on the Y* grid and the Y basis
-All data in `f_Yst` and `f_Y` will be destroyed.
-"""
-function Yst_to_Y!(pw::PlaneWaveBasis, f_Yst, f_Y)
-    R_to_G!(pw, f_Yst, f_Y, pw.idx_to_fft)
-end
-
-"""
-Perform an in-place iFFT to translate between a representation of
-a function f in the Y* grid and the X_k basis specific to the
-k-Point with index `ik`.
-All data in `f_Yst` and `f_X` will be destroyed.
-"""
-function Yst_to_Xk!(pw::PlaneWaveBasis, ik::Int, f_Yst, f_Xk)
-    R_to_G!(pw, f_Yst, f_Xk, pw.idx_to_fft[pw.kmask[ik]])
+    f_fourier .*= 1 / prod(size(pw.grid_size))
 end
