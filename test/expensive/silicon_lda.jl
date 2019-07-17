a8becc3316a7113d5d70caaed9697192bac51a89
Michael F. Herbst <info@michael-herbst.com>
2019-07-17 15:53:58 +0200

    Remove print statement

--- test/expensive/silicon_lda.jl
+++ test/expensive/silicon_lda.jl
@@ -1,5 +1,5 @@
 include("../silicon_runners.jl")
 
 @testset "Silicon LDA (large, Float64)" begin
-    run_silicon_lda(Float64, Ecut=25, test_tol=2e-7, n_ignored=0, grid_size=33, scf_tol=1e-7)
+    run_silicon_lda(Float64, Ecut=25, test_tol=4e-7, n_ignored=0, grid_size=33, scf_tol=1e-7)
 end
