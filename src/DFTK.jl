3b81effa22afe30fc874e71833dd00bf722c704a
Michael F. Herbst <info@michael-herbst.com>
2019-09-27 14:46:19 +0200

    Disable pretty much the complete package

--- src/DFTK.jl
+++ src/DFTK.jl
@@ -16,88 +16,95 @@ export Vec3
 export Mat3
 include("core/constants.jl")
 
-export PlaneWaveBasis
-export set_kpoints!
-export basis_ρ
-export PotLocal
-include("core/PlaneWaveBasis.jl")
-include("core/PotLocal.jl")
+export Model
+export PlaneWaveModel
+export basis_Cρ
+export Kpoint
+include("core/Model.jl")
+include("core/PlaneWaveModel.jl")
 
-export Kinetic
-include("core/Kinetic.jl")
-
-export PspHgh
-export eval_psp_local_fourier
-export eval_psp_local_real
-export eval_psp_projection_radial
-include("core/PspHgh.jl")
-
-include("core/occupation.jl")
-
-export compute_density
-include("core/compute_density.jl")
-
-export PotHartree
-include("core/PotHartree.jl")
-
-export PotNonLocal
-include("core/PotNonLocal.jl")
-
-export PotXc
-include("core/PotXc.jl")
-
-export Hamiltonian
-export apply_hamiltonian!
-include("core/Hamiltonian.jl")
-
-export PreconditionerKinetic
-include("core/Preconditioner.jl")
-
-export lobpcg
-include("core/lobpcg.jl")
-
-export scf_nlsolve_solver
-export scf_damping_solver
-export scf_anderson_solver
-export scf_CROP_solver
-include("core/scf.jl")
-include("core/scf_solvers.jl")
-
-export energy_ewald
-include("core/energy_ewald.jl")
-
-# Utilities
-
-export Species
-export charge_nuclear
-export charge_ionic
-export n_elec_valence
-export n_elec_core
-include("utils/Species.jl")
-
-export bzmesh_uniform
-export bzmesh_ir_wedge
-include("utils/bzmesh.jl")
-
-export build_local_potential
-export build_nonlocal_projectors
+# export PlaneWaveBasis
+# export set_kpoints!
+# export basis_ρ
+# export PotLocal
+# include("core/PlaneWaveBasis.jl")
+# include("core/PotLocal.jl")
+#
+# export Kinetic
+# include("core/Kinetic.jl")
+#
+# export PspHgh
+# export eval_psp_local_fourier
+# export eval_psp_local_real
+# export eval_psp_projection_radial
+# include("core/PspHgh.jl")
+#
+# include("core/occupation.jl")
+#
+# export compute_density
+# include("core/compute_density.jl")
+#
+# export PotHartree
+# include("core/PotHartree.jl")
+#
+# export PotNonLocal
+# include("core/PotNonLocal.jl")
+#
+# export PotXc
+# include("core/PotXc.jl")
+#
+# export Hamiltonian
+# export apply_hamiltonian!
+# include("core/Hamiltonian.jl")
+#
+# export PreconditionerKinetic
+# include("core/Preconditioner.jl")
+#
+# export lobpcg
+# include("core/lobpcg.jl")
+#
+# export scf_nlsolve_solver
+# export scf_damping_solver
+# export scf_anderson_solver
+# export scf_CROP_solver
+# include("core/scf.jl")
+# include("core/scf_solvers.jl")
+#
+# export energy_ewald
+# include("core/energy_ewald.jl")
+#
+# # Utilities
+#
+# export Species
+# export charge_nuclear
+# export charge_ionic
+# export n_elec_valence
+# export n_elec_core
+# include("utils/Species.jl")
+#
+# export bzmesh_uniform
+# export bzmesh_ir_wedge
+# include("utils/bzmesh.jl")
+#
+# export build_local_potential
+# export build_nonlocal_projectors
 export determine_grid_size
-export guess_gaussian_sad
-export guess_hcore
-export kblock_as_matrix
-export load_psp
-export self_consistent_field
-include("utils/build_local_potential.jl")
-include("utils/build_nonlocal_projectors.jl")
+# export guess_gaussian_sad
+# export guess_hcore
+# export kblock_as_matrix
+# export load_psp
+# export self_consistent_field
+# include("utils/build_local_potential.jl")
+# include("utils/build_nonlocal_projectors.jl")
 include("utils/determine_grid_size.jl")
-include("utils/guess_gaussian_sad.jl")
-include("utils/guess_hcore.jl")
-include("utils/kblock_as_matrix.jl")
-include("utils/self_consistent_field.jl")
-include("utils/load_psp.jl")
-
-export energy_nuclear_psp_correction
-export energy_nuclear_ewald
-include("utils/energy_nuclear.jl")
+# include("utils/guess_gaussian_sad.jl")
+# include("utils/guess_hcore.jl")
+# include("utils/kblock_as_matrix.jl")
+# include("utils/self_consistent_field.jl")
+# include("utils/load_psp.jl")
+#
+# export energy_nuclear_psp_correction
+# export energy_nuclear_ewald
+# include("utils/energy_nuclear.jl")
 
 end # module DFTK
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,24 +3,24 @@ using DFTK
 
 include("determine_grid_size.jl")
 include("fourier_transforms.jl")
-include("PlaneWaveBasis.jl")
-include("PspHgh.jl")
-include("Species.jl")
-include("bzmesh.jl")
-
-include("build_local_potential.jl")
-include("build_nonlocal_projectors.jl")
-
-include("lobpcg.jl")
-include("xc_fallback.jl")
-
-include("energy_ewald.jl")
-include("energy_nuclear.jl")
-include("occupation.jl")
-include("energies_guess_density.jl")
-include("compute_density.jl")
-
-include("scf_compare.jl")
-include("silicon_noXC.jl")
-include("silicon_lda.jl")
-include("silicon_pbe.jl")
+# include("PlaneWaveBasis.jl")
+# include("PspHgh.jl")
+# include("Species.jl")
+# include("bzmesh.jl")
+#
+# include("build_local_potential.jl")
+# include("build_nonlocal_projectors.jl")
+#
+# include("lobpcg.jl")
+# include("xc_fallback.jl")
+#
+# include("energy_ewald.jl")
+# include("energy_nuclear.jl")
+# include("occupation.jl")
+# include("energies_guess_density.jl")
+# include("compute_density.jl")
+#
+# include("scf_compare.jl")
+# include("silicon_noXC.jl")
+# include("silicon_lda.jl")
+# include("silicon_pbe.jl")
