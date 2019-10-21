9417e11bcd39d64d9df6bd98d2abd0ec0679f589
Antoine Levitt <antoine.levitt@gmail.com>
2019-10-21 17:14:44 +0200

    Gga doc (#23)
    Extend GGA XC documentation

--- src/core/PotXc.jl
+++ src/core/PotXc.jl
@@ -1,5 +1,66 @@
 include("xc_evaluate.jl")
 
+#=
+The total energy is
+Etot = ∫ ρ E(ρ,σ), where σ = |∇ρ|^2 and E(ρ,σ) is the energy per unit particle
+libxc provides the scalars
+Vρ = ∂(ρ E)/∂ρ
+Vσ = ∂(ρ E)/∂σ
+
+Consider a variation dϕi of an orbital (considered real for
+simplicity), and let dEtot be the corresponding variation of the
+energy. Then the potential Vxc is defined by
+dEtot = 2 ∫ Vxc ϕi dϕi
+
+dρ = 2 ϕi dϕi
+dσ = 2 ∇ρ ⋅ ∇dρ = 4 ∇ρ ⋅ ∇(ϕi dϕi)
+dEtot = ∫ Vρ dρ + Vσ dσ
+     = 2 ∫ Vρ ϕi dϕi + 4 ∫ Vσ ∇ρ ⋅ ∇(ϕi dϕi)
+     = 2 ∫ Vρ ϕi dϕi - 4 ∫ div(Vσ ∇ρ) ϕi dϕi
+where we performed an integration by parts in the last equation (boundary terms drop by periodicity).
+
+Therefore,
+Vxc = Vρ - 2 div(Vσ ∇ρ)
+
+See eg Richard Martin, Electronic stucture, p. 158
+
+A more algebraic way of deriving these for GGA, which helps see why
+this is correct at the discrete level, is the following. Let
+
+B the (Fourier, spherical) space of orbitals
+C the (Fourier, cube) space of Fourier densities/potentials
+C* the (real-space, cube) space of real-space densities/potentials
+
+X : B -> C be the extension operator
+R : C -> B the restriction (adjoint of X)
+IF : C -> C* be the IFFT
+F : C* -> be the FFT, adjoint of IF
+K : C -> C be the gradient operator (multiplication by iK, assuming dimension 1 to
+simplify notations)
+
+Then
+
+ρ = IF(Xϕ).^2
+ρf = F ρ
+∇ρf = K ρf
+∇ρ = IF ∇ρf
+
+dρ = 2 IF(Xϕ) .* IF(X dϕ)
+d∇ρ = IF K F dρ
+    = 2 IF K F (IF(Xϕ) .* IF(X dϕ))
+
+Etot = sum(ρ . * E(ρ,∇ρ.^2))
+dEtot = sum(Vρ .* dρ + 2 (Vσ .* ∇ρ) .* d∇ρ)
+      = sum(Vρ .* dρ + (IF K F) (Vσ .* ∇ρ) .* dρ)
+(IF K F is self-adjoint; this is the discrete integration by parts)
+
+Now note that
+
+sum(V .* dρ) = sum(V .* IF(Xϕ) .* IF(X dϕ))
+             = sum(R(F(V .* IF(Xϕ))) .* dϕ)
+and the result follows.
+=#
+
 struct PotXc
     basis::PlaneWaveBasis
     supersampling::Int  # Supersampling for the XC grid
@@ -21,7 +82,6 @@ function PotXc(basis::PlaneWaveBasis, functional...; supersampling=2)
     make_functional(symb::Symbol) = Functional(symb)
     PotXc(basis, supersampling, [make_functional(f) for f in functional])
 end
-
 
 function update_energies_potential!(energies, potential, op::PotXc, ρ)
     T = real(eltype(ρ))
@@ -47,6 +107,7 @@ function update_energies_potential!(energies, potential, op::PotXc, ρ)
     potential .= 0
     E = similar(ρ_real)
     for xc in op.functional
+        # Compute xc potential: Vρ - 2 div(Vσ ∇ρ)
         if xc.family == Libxc.family_lda
             Vρ = similar(ρ_real)
             evaluate_lda!(xc, ρ_real, Vρ=Vρ, E=E)
@@ -56,17 +117,7 @@ function update_energies_potential!(energies, potential, op::PotXc, ρ)
             Vσ = similar(ρ_real)
             evaluate_gga!(xc, ρ_real, σ_real, Vρ=Vρ, Vσ=Vσ, E=E)
 
-            # TODO Check the literature how this expression comes about in detail.
-            #      Following Richard Martin, Electronic stucture, p. 158, the XC potential
-            #      can be split as:
-            #        Vxc = ∂(ρ ϵ_{XC})/∂ρ - ∇( ∂(ρ ϵ_{XC})/∂(∇ρ) )
-            #      Now the second term can be rewritten by the chain rule
-            #        -∇( ∂(ρ ϵ_{XC})/∂(∇ρ) ) = -∇( ∂(ρ ϵ_{XC})/∂(|∇ρ|^2) ∂(|∇ρ|^2)/∂(∇ρ) )
-            #                                = -∇( ∂(ρ ϵ_{XC})/∂(|∇ρ|^2) 2∇ρ )
-            #    libxc yields
-            #        Vρ === ∂(ρ ϵ_{XC})/∂ρ
-            #        Vσ === ∂(ρ ϵ_{XC})/∂(|∇ρ|^2)
-
+            # 2 div(Vσ ∇ρ)
             gradterm = sum(1:3) do α
                 # Compute term inside -∇(  ) in Fourier space and take derivative
                 Vσ∇ρ = 2r_to_G!(pw, Vσ .* ∇ρ_real[α] .+ 0im, similar(ρ, Complex{T}))
