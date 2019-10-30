99dc8dd95da3b5ee45b399ba78e2d1ee3cf884b6
Antoine Levitt <antoine.levitt@gmail.com>
2019-10-30 16:01:27 +0100

    Add supercell

--- examples/silicon.jl
+++ examples/silicon.jl
@@ -1,6 +1,7 @@
 using PyCall
 using DFTK
 using Printf
+using PyCall
 
 calc_type = :lda
 
@@ -8,7 +9,7 @@ calc_type = :lda
 # Calculation parameters
 kgrid = [4, 4, 4]
 Ecut = 15  # Hartree
-n_bands = 8
+n_bands_plot = 8 # number of bands to plot in the bandstructure
 kline_density = 20
 
 # Setup silicon lattice
@@ -16,6 +17,18 @@ a = 10.263141334305942  # Silicon lattice constant in Bohr
 lattice = a / 2 .* [[0 1 1.]; [1 0 1.]; [1 1 0.]]
 Si = Species(14, psp=load_psp("si-pade-q4.hgh"))
 composition = [Si => [ones(3)/8, -ones(3)/8]]
+
+# Possibly supercell
+supercell = [1, 1, 1]
+pystruct = pymatgen_structure(model, composition...)
+pystruct.make_supercell(supercell)
+for i in 1:3, j in 1:3
+    A_to_bohr = pyimport("pymatgen.core.units").ang_to_bohr
+    lattice[i, j] = A_to_bohr * get(get(pystruct.lattice.matrix, j-1), i-1)
+end
+composition = [Si => [s.frac_coords for s in pystruct.sites]]
+
+
 
 # Setup model and discretisation
 if calc_type == :rhf
@@ -27,7 +40,7 @@ elseif calc_type == :pbe
 elseif calc_type == :indep_electrons
     model = model_hcore(lattice, composition...)
 elseif calc_type == :free
-    n_electrons = 8
+    n_electrons = 8*prod(supercell)
     model = Model(lattice, n_electrons)
 else
     error("Unknown calc_type $(calc_type)")
@@ -54,7 +67,7 @@ end
 # Band structure calculation along high-symmetry path
 kpoints, klabels, kpath = determine_high_symmetry_kpath(basis, kline_density, composition...)
 println("Computing bands along kpath:\n     $(join(kpath[1], " -> "))")
-band_data = compute_bands(ham, kpoints, n_bands)
+band_data = compute_bands(ham, kpoints, n_bands_plot)
 
 # Plot bandstructure using pymatgen
 plotter = pyimport("pymatgen.electronic_structure.plotter")
