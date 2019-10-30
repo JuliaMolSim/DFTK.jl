e8c9a6506a53213911c08460ee59572f50e8f60f
Antoine Levitt <antoine.levitt@gmail.com>
2019-10-30 15:49:06 +0100

    Refactor Silicon example

--- examples/silicon.jl
+++ examples/silicon.jl
@@ -2,20 +2,36 @@ using PyCall
 using DFTK
 using Printf
 
+calc_type = :lda
+
+
 # Calculation parameters
 kgrid = [4, 4, 4]
-Ecut = 25  # Hartree
+Ecut = 15  # Hartree
 n_bands = 8
 kline_density = 20
 
-# Setup silicon lattice and structure
+# Setup silicon lattice
 a = 10.263141334305942  # Silicon lattice constant in Bohr
 lattice = a / 2 .* [[0 1 1.]; [1 0 1.]; [1 1 0.]]
-Si = Species(14, psp=load_psp("si-pbe-q4.hgh"))
+Si = Species(14, psp=load_psp("si-pade-q4.hgh"))
 composition = [Si => [ones(3)/8, -ones(3)/8]]
 
-# Setup PBE model and discretisation
-model = model_dft(lattice, [:gga_x_pbe, :gga_c_pbe], composition...)
+# Setup model and discretisation
+if calc_type == :rhf
+    model = model_reduced_hf(lattice, composition...)
+elseif calc_type == :lda
+    model = model_dft(lattice, :lda_xc_teter93, composition...)
+elseif calc_type == :pbe
+    model = model_dft(lattice, [:gga_x_pbe, :gga_c_pbe], composition...)
+elseif calc_type == :indep_electrons
+    model = model_hcore(lattice, composition...)
+elseif calc_type == :free
+    n_electrons = 8
+    model = Model(lattice, n_electrons)
+else
+    error("Unknown calc_type $(calc_type)")
+end
 kpoints, ksymops = bzmesh_ir_wedge(kgrid, lattice, composition...)
 fft_size = determine_grid_size(lattice, Ecut)
 basis = PlaneWaveModel(model, fft_size, Ecut, kpoints, ksymops)
@@ -23,7 +39,7 @@ basis = PlaneWaveModel(model, fft_size, Ecut, kpoints, ksymops)
 # Run SCF, note Silicon metal is an insulator, so no need for all bands here
 n_bands_scf = Int(model.n_electrons / 2)
 ham = Hamiltonian(basis, guess_gaussian_sad(basis, composition...))
-scfres = self_consistent_field!(ham, n_bands_scf, tol=1e-8)
+scfres = self_consistent_field!(ham, n_bands_scf, tol=1e-6)
 
 # Print obtained energies
 energies = scfres.energies
@@ -46,6 +62,5 @@ bs = pymatgen_bandstructure(basis, band_data, klabels, fermi_level=scfres.εF)
 bsplot = plotter.BSPlotter(bs)
 plt = bsplot.get_plot()
 plt.autoscale()
-plt.savefig("silicon_pbe.pdf")
-plt.legend()
+plt.savefig("silicon_$(calc_type).pdf")
 plt.show()
