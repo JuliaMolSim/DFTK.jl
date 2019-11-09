8edac21ce7038d4d260a08b2222185686d842bbe
Antoine Levitt <antoine.levitt@gmail.com>
2019-11-09 14:48:23 +0100

    Wrap in a testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,35 +1,37 @@
 using Test
 using DFTK
 
-# Test something that exercises the standard code paths first, so we
-# know quickly if we did something stupid
-include("silicon_pbe.jl")
+# Wrap in an outer testset to get a full report if one test fails
+@testset "All tests" begin
+    # Synthetic tests at the beginning, so it fails faster if
+    # something has gone badly wrong
+    include("silicon_redHF.jl")
+    include("silicon_lda.jl")
+    include("silicon_pbe.jl")
+    include("scf_compare.jl")
 
-include("determine_grid_size.jl")
-include("fourier_transforms.jl")
-include("PlaneWaveBasis.jl")
-include("PspHgh.jl")
-include("Species.jl")
-include("bzmesh.jl")
+    include("determine_grid_size.jl")
+    include("fourier_transforms.jl")
+    include("PlaneWaveBasis.jl")
+    include("PspHgh.jl")
+    include("Species.jl")
+    include("bzmesh.jl")
 
-include("term_external.jl")
-include("term_nonlocal.jl")
-# TODO Test for term_hartree
-# TODO Test for term_xc
+    include("term_external.jl")
+    include("term_nonlocal.jl")
+    # TODO Test for term_hartree
+    # TODO Test for term_xc
 
-include("HamiltonianBlock.jl")
-include("lobpcg.jl")
-include("xc_fallback.jl")
+    include("HamiltonianBlock.jl")
+    include("lobpcg.jl")
+    include("xc_fallback.jl")
 
-include("energy_ewald.jl")
-include("energy_nuclear.jl")
-include("occupation.jl")
-include("energies_guess_density.jl")
-include("compute_density.jl")
+    include("energy_ewald.jl")
+    include("energy_nuclear.jl")
+    include("occupation.jl")
+    include("energies_guess_density.jl")
+    include("compute_density.jl")
 
-include("silicon_redHF.jl")
-include("silicon_lda.jl")
-include("scf_compare.jl")
-
-include("variational.jl")
-include("compute_bands.jl")
+    include("variational.jl")
+    include("compute_bands.jl")
+end
