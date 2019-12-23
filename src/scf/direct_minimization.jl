5b1654e879b13e5d06c3c5b578f3de0c5e04612c
Antoine Levitt <antoine.levitt@gmail.com>
2019-12-23 14:27:01 +0100

    review

--- src/scf/direct_minimization.jl
+++ src/scf/direct_minimization.jl
@@ -3,27 +3,28 @@
 using Optim
 
 # This is all a bit annoying because our Psi is represented as Psi[k][G,n], and Optim accepts only dense arrays
-# We do a bit of back and forth using custom `vec` (ours -> optim's) and `devec` (optim's -> ours) functions
+# We do a bit of back and forth using custom `pack` (ours -> optim's) and `unpack` (optim's -> ours) functions
 
-# Impose the orthogonality constraint on orbitals inside each k-block
+# Orbitals inside each kblock must be kept orthogonal: the
+# project_tangent and retract work per kblock
 struct DMManifold <: Optim.Manifold
     Nk::Int
-    devec_fun::Function
+    unpack_fun::Function
 end
 function Optim.project_tangent!(m::DMManifold, g, x)
-    g_devec = m.devec_fun(g)
-    x_devec = m.devec_fun(x)
+    g_unpack = m.unpack_fun(g)
+    x_unpack = m.unpack_fun(x)
     for ik = 1:m.Nk
         Optim.project_tangent!(Optim.Stiefel(),
-                               g_devec[ik],
-                               x_devec[ik])
+                               g_unpack[ik],
+                               x_unpack[ik])
     end
     g
 end
 function Optim.retract!(m::DMManifold, x)
-    x_devec = m.devec_fun(x)
+    x_unpack = m.unpack_fun(x)
     for ik = 1:m.Nk
-        Optim.retract!(Optim.Stiefel(), x_devec[ik])
+        Optim.retract!(Optim.Stiefel(), x_unpack[ik])
     end
     x
 end
@@ -32,26 +33,26 @@ end
 struct DMPreconditioner
     Nk::Int
     Pks::Array # Pks[ik] is the preconditioner for kpoint ik
-    devec_fun::Function
+    unpack_fun::Function
 end
 function LinearAlgebra.ldiv!(p, P::DMPreconditioner, d)
-    p_devec = P.devec_fun(p)
-    d_devec = P.devec_fun(d)
+    p_unpack = P.unpack_fun(p)
+    d_unpack = P.unpack_fun(d)
     for ik = 1:P.Nk
-        ldiv!(p_devec[ik], P.Pks[ik], d_devec[ik])
+        ldiv!(p_unpack[ik], P.Pks[ik], d_unpack[ik])
     end
     p
 end
 function LinearAlgebra.dot(x, P::DMPreconditioner, y)
-    x_devec = P.devec_fun(x)
-    y_devec = P.devec_fun(y)
-    sum(dot(x_devec[ik], P.Pks[ik], y_devec[ik])
+    x_unpack = P.unpack_fun(x)
+    y_unpack = P.unpack_fun(y)
+    sum(dot(x_unpack[ik], P.Pks[ik], y_unpack[ik])
         for ik = 1:P.Nk)
 end
-function Optim.precondprep!(P::DMPreconditioner, x)
-    x_devec = P.devec
+function precondprep!(P::DMPreconditioner, x)
+    x_unpack = P.unpack_fun(x)
     for ik = 1:P.Nk
-        precondprep!(Pks[ik], x_devec[ik])
+        precondprep!(P.Pks[ik], x_unpack[ik])
     end
     P
 end
@@ -66,20 +67,20 @@ function direct_minimization(basis::PlaneWaveBasis{T}, Psi0;
                              optim_solver=Optim.LBFGS, kwargs...) where T
     model = basis.model
     @assert model.spin_polarisation in (:none, :spinless)
-    @assert model.assume_band_gap
+    @assert model.assume_band_gap # temperature is not yet supported
     filled_occ = filled_occupation(model)
     n_bands = div(model.n_electrons, filled_occ)
-    ortho(Psik) = Matrix{Complex{T}}(Matrix(qr(Psik).Q))
+    ortho(Psik) = Matrix(qr(Psik).Q)
     Nk = length(basis.kpoints)
 
     if Psi0 === nothing
         Psi0 = [ortho(randn(Complex{T}, length(k.basis), n_bands)) for k in basis.kpoints]
     end
-    occupation = [filled_occ*ones(T, n_bands) for ik = 1:Nk]
+    occupation = [filled_occ * ones(T, n_bands) for ik = 1:Nk]
 
     ham = Hamiltonian(basis)
 
-    ## vec and devec
+    ## vec and unpack
     # length of Psi[ik]
     lengths = [length(Psi0[ik]) for ik = 1:Nk]
     # psi[ik] is in psi_flat[starts[ik]:starts[ik]+lengths[ik]-1]
@@ -88,15 +89,15 @@ function direct_minimization(basis::PlaneWaveBasis{T}, Psi0;
     for ik = 1:Nk-1
         starts[ik+1] = starts[ik] + lengths[ik]
     end
-    vec(Psi) = vcat(Base.vec.(Psi)...) # TODO as an optimization, do that lazily? See LazyArrays
-    devec(Psi) = [@views reshape(Psi[starts[ik]:starts[ik]+lengths[ik]-1], size(Psi0[ik])) for ik in 1:Nk]
+    pack(Psi) = vcat(Base.vec.(Psi)...) # TODO as an optimization, do that lazily? See LazyArrays
+    unpack(Psi) = [@views reshape(Psi[starts[ik]:starts[ik]+lengths[ik]-1], size(Psi0[ik])) for ik in 1:Nk]
 
     # this will get updated
     energies = nothing
 
     # computes energies and gradients
     function fg!(E, G, Psi)
-        Psi = devec(Psi)
+        Psi = unpack(Psi)
         ρ = compute_density(basis, Psi, occupation)
         ham = update_hamiltonian(ham, ρ)
         if E != nothing
@@ -104,23 +105,26 @@ function direct_minimization(basis::PlaneWaveBasis{T}, Psi0;
             E = sum(values(energies))
         end
 
-        # TODO check for possible 1/2 factors
+        # The energy has terms like occ * <ψ|H|ψ>, so the gradient is 2occ Hψ
         if G != nothing
-            G = devec(G)
+            G = unpack(G)
             for ik = 1:Nk
                 mul!(G[ik], kblock(ham, basis.kpoints[ik]), Psi[ik])
+                G[ik] .*= 2*filled_occ
             end
         end
         E
     end
 
-    manif = DMManifold(Nk, devec)
+    manif = DMManifold(Nk, unpack)
 
     Pks = [prec_type(ham, kpt) for kpt in basis.kpoints]
-    P = DMPreconditioner(Nk, Pks, devec)
+    P = DMPreconditioner(Nk, Pks, unpack)
 
-    res = Optim.optimize(Optim.only_fg!(fg!), vec(Psi0), optim_solver(P=P, manifold=manif), Optim.Options(; allow_f_increases=true, show_trace=true, kwargs...))
-    Psi = devec(res.minimizer)
+    res = Optim.optimize(Optim.only_fg!(fg!), pack(Psi0),
+                         optim_solver(P=P, precondprep=precondprep!, manifold=manif),
+                         Optim.Options(; allow_f_increases=true, show_trace=true, kwargs...))
+    Psi = unpack(res.minimizer)
     ρ = compute_density(basis, Psi, occupation)
     ham = update_hamiltonian(ham, ρ)
     # These concepts do not make sense in direct minimization,
