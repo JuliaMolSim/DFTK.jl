b1db69f00d5a8ff141d5b123da32572c2762ace3
Michael F. Herbst <info@michael-herbst.com>
2020-01-07 21:25:29 +0100

    Take occupation into account for DOS (#98)

--- src/postprocess/DOS.jl
+++ src/postprocess/DOS.jl
@@ -13,11 +13,14 @@ using ForwardDiff
 Total density of states at energy ε
 """
 function DOS(ε, basis, orben; smearing=basis.model.smearing, T=basis.model.temperature)
+    filled_occ = filled_occupation(basis.model)
     D = zero(ε)
     T != 0 || error("DOS only supports finite temperature")
     for ik = 1:length(orben)
         for iband = 1:length(orben[ik])
-            D += basis.kweights[ik] * ForwardDiff.derivative(ε -> smearing((orben[ik][iband] - ε) / T), ε)
+            D += (filled_occ * basis.kweights[ik]
+                  * ForwardDiff.derivative(ε -> smearing((orben[ik][iband] - ε) / T), ε)
+                 )
         end
     end
     D
@@ -27,13 +30,14 @@ end
 Local density of states, in real space
 """
 function LDOS(ε, basis, orben, Psi; smearing=basis.model.smearing, T=basis.model.temperature)
+    filled_occ = filled_occupation(basis.model)
     D = zeros(real(eltype(Psi[1])), basis.fft_size)
     T != 0 || error("LDOS only supports finite temperature")
     for ik = 1:length(orben)
         ψreal = G_to_r(basis, basis.kpoints[ik], Psi[ik])
         for iband = 1:length(orben[ik])
-            D += (basis.kweights[ik]
-                  * ForwardDiff.derivative(ε -> smearing((orben[ik][iband] - ε) / T), ε) 
+            D += (filled_occ * basis.kweights[ik]
+                  * ForwardDiff.derivative(ε -> smearing((orben[ik][iband] - ε) / T), ε)
                   * abs2.(ψreal[:, :, :, iband]))
         end
     end
