1b973f6bbf2b26c96c2971a1cb25d96c780bb533
Michael F. Herbst <info@michael-herbst.com>
2020-01-14 15:48:50 +0100

    Amend check_real (#99)
    * Amend check_real to ignore imaginary parts which are smaller than a certain threshold.

--- src/common/check_real.jl
+++ src/common/check_real.jl
@@ -1,7 +1,12 @@
 check_real(A::AbstractArray) = nothing
 function check_real(A::AbstractArray{Complex{T}}) where T
-    discrepancy = norm(imag(A) ./ abs.(A))
-    if discrepancy > 1000 * eps(real(eltype(A)))
-        @warn "Large imaginary part" discrepancy
+    epsT = eps(real(eltype(A)))
+    rtol = 1000epsT
+    atol = 100epsT
+
+    if any(abs(imag(x)) > rtol * abs(x) && abs(imag(x)) > atol for x in A)
+        relerror = imag(A) ./ abs.(A)
+        relerror[map(x -> abs(imag(x)) < atol, A)] = 0
+        @warn "Large imaginary part" norm(relerror)
     end
 end
