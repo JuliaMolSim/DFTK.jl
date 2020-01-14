5b4be64670e9247ba2e89ea0264b709644ec6b69
Michael F. Herbst <info@michael-herbst.com>
2020-01-14 11:03:33 +0100

    Amend check_real

--- src/common/check_real.jl
+++ src/common/check_real.jl
@@ -1,7 +1,11 @@
 check_real(A::AbstractArray) = nothing
 function check_real(A::AbstractArray{Complex{T}}) where T
-    discrepancy = norm(imag(A) ./ abs.(A))
-    if discrepancy > 1000 * eps(real(eltype(A)))
+    epsT = eps(real(eltype(A)))
+    error = imag(A) ./ abs.(A)
+    error[map(x -> abs(imag(x)) < 100epsT, A)] .= 0
+
+    discrepancy = norm(error)
+    if discrepancy > 1000epsT
         @warn "Large imaginary part" discrepancy
     end
 end
