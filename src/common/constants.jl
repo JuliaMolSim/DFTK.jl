debc77780d5ea1324e2eccab50a527687ab88100
Michael F. Herbst <info@michael-herbst.com>
2020-01-15 21:23:05 +0100

    Add more units

--- src/common/constants.jl
+++ src/common/constants.jl
@@ -3,6 +3,8 @@ DFTK_DATADIR = joinpath(dirname(pathof(DFTK)), "..", "data")
 
 # Commonly used constants. The factors convert from the respective unit to AU
 module units
-    eV = 0.03674932248  # Hartree
-    Ǎ = 1.8897261254578281  # Bohr
+    eV = 0.03674932248       # electron volt -> Hartree
+    Ǎ  = 1.8897261254578281  # Ǎngström -> Bohr
+    K  = 3.166810508e-6      # Kelvin -> Hartree
+    Ry = 0.5                 # Rydberg -> Hartree
 end
