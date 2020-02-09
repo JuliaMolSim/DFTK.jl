abf3bc5f44a407bddbf2c26c2a1bc9afc4966a00
Michael F. Herbst <info@michael-herbst.com>
2020-02-09 09:38:16 +0100

    Fix ABINIT tests

--- test/testcases_ABINIT/silicon_E10_k3_LDA/generate.jl
+++ test/testcases_ABINIT/silicon_E10_k3_LDA/generate.jl
@@ -5,7 +5,6 @@ atoms = [Element(14, load_psp("hgh/lda/si-q4")) => silicon.positions]
 model = model_dft(silicon.lattice, :lda_xc_teter93, atoms)
 
 abinitpseudos = [joinpath(@__DIR__, "Si-q4-pade.abinit.hgh")]
-DFTK.run_abinit_scf(model, @__DIR__;
-                    abinitpseudos=abinitpseudos, pspmap=pspmap,
+DFTK.run_abinit_scf(model, @__DIR__; abinitpseudos=abinitpseudos,
                     Ecut=10, kgrid=[3, 3, 3], n_bands=6, tol=1e-10,
                     iscf=3)  # Use Anderson mixing instead of minimisation
--- test/testcases_ABINIT/silicon_E25_k3_GGA/generate.jl
+++ test/testcases_ABINIT/silicon_E25_k3_GGA/generate.jl
@@ -5,7 +5,6 @@ atoms = [Element(14, load_psp("hgh/pbe/si-q4")) => silicon.positions]
 model = model_dft(silicon.lattice, [:gga_c_pbe, :gga_x_pbe], atoms)
 
 abinitpseudos = [joinpath(@__DIR__, "Si-q4-pbe.abinit.hgh")]
-DFTK.run_abinit_scf(model, @__DIR__;
-                    abinitpseudos=abinitpseudos, pspmap=pspmap,
+DFTK.run_abinit_scf(model, @__DIR__; abinitpseudos=abinitpseudos,
                     Ecut=25, kgrid=[3, 3, 3], n_bands=10, tol=1e-10,
                     iscf=3) # Use Anderson mixing instead of minimisation
