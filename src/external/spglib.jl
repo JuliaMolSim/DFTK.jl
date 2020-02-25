7279f16def7f64aae319c21fcc896579b1466cc8
Michael F. Herbst <info@michael-herbst.com>
2020-02-25 21:53:50 +0100

    Implement fallback kpoint symmetry search

--- src/external/spglib.jl
+++ src/external/spglib.jl
@@ -39,3 +39,49 @@ function spglib_cell(lattice, atoms)
     #       For future reference: The C interface spglib also uses columns.
     (lattice', spg_positions, spg_numbers)
 end
+
+function spglib_get_symmetry(lattice, atoms; tol_symmetry=1e-5)
+    spglib = import_spglib()
+
+    # Ask spglib for symmetry operations and for irreducible mesh
+    spg_symops = spglib.get_symmetry(spglib_cell(lattice, atoms),
+                                     symprec=tol_symmetry)
+
+    # If spglib does not find symmetries give an error
+    spg_symops !== nothing || error(
+        "spglib failed to get the symmetries. Check your lattice, or use a uniform BZ mesh."
+    )
+
+    Stildes = [St for St in eachslice(spg_symops["rotations"]; dims=1)]
+    τtildes = [rationalize.(τt, tol=tol_symmetry)
+               for τt in eachslice(spg_symops["translations"]; dims=1)]
+    @assert length(τtildes) == length(Stildes)
+
+    # Checks: (A Stilde A^{-1}) is unitary
+    for Stilde in Stildes
+        Scart = lattice * Stilde * inv(lattice)  # Form S in cartesian coords
+        if maximum(abs, Scart'Scart - I) > sqrt(eps(Float64))
+            error("spglib returned non-unitary rotation matrix")
+        end
+    end
+
+    # Check (Stilde, τtilde) maps atoms to equivalent atoms in the lattice
+    for (Stilde, τtilde) in zip(Stildes, τtildes)
+        for (elem, positions) in atoms
+            for coord in positions
+                diffs = [rationalize.(Stilde * coord + τtilde - pos, tol=tol_symmetry)
+                         for pos in positions]
+
+                # If all elements of a difference in diffs is integer, then
+                # Stilde * coord + τtilde and pos are equivalent lattice positions
+                if !any(all(isinteger, d) for d in diffs)
+                    error("Cannot map the atom at position $coord to another atom of the " *
+                          "same element under the symmetry operation (Stilde, τtilde):\n" *
+                          "($Stilde, $τtilde)")
+                end
+            end
+        end
+    end
+
+    Stildes, τtildes
+end
