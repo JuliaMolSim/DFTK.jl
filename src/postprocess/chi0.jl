5a50c45c30705ff2416855dd87b11f8c298268af
Antoine Levitt <antoine.levitt@gmail.com>
2020-03-19 17:48:09 +0100

    apply_χ0 first draft

--- src/postprocess/chi0.jl
+++ src/postprocess/chi0.jl
@@ -5,7 +5,7 @@ using ProgressMeter
 """
 Compute the independent-particle susceptibility. Will blow up for large systems
 """
-function compute_χ0(ham)
+function compute_χ0(ham; diagonal_only=false)
     # We're after χ0(r,r') such that δρ = ∫ χ0(r,r') δV(r') dr'
     # where (up to normalizations)
     # ρ = ∑_nk f(εnk - εF) |ψnk|^2
@@ -25,8 +25,8 @@ function compute_χ0(ham)
     # δεF = 1/DOS ∫ δV(r) LDOS(r)dr
 
     # for δρ we note ρnm = ψn* ψm, and we get
-    # δρ = LDOS δεF + ∑_n f'n <ρn|δV> ρn + ∑_{n != m} 2Re fn ρnm <ρmn|δV> / (εn-εm)
-    # δρ = LDOS δεF + ∑_n f'n <ρn|δV> ρn + ∑_{n != m} (fn-fm)/(εn-εm) ρnm <ρmn|δV>
+    # δρ = LDOS δεF + ∑_n f'n <ρn|δV> ρn + ∑_{n,m != n} 2Re fn ρnm <ρmn|δV> / (εn-εm)
+    # δρ = LDOS δεF + ∑_n f'n <ρn|δV> ρn + ∑_{n,m != n} (fn-fm)/(εn-εm) ρnm <ρmn|δV>
     # The last two terms merge with the convention that (f(x)-f(x))/(x-x) = f'(x) into
     # δρ = LDOS δεF + ∑_{n,m} (fn-fm) ρnm <ρmn|δV> / (εn-εm)
     # Therefore the kernel is LDOS(r) LDOS(r') / DOS + ∑_{n,m} (fn-fm)/(εn-εm) ρnm(r) ρmn(r')
@@ -34,13 +34,13 @@ function compute_χ0(ham)
     model = basis.model
     fft_size = basis.fft_size
     @assert model.spin_polarisation == :none
-    filled_occ = DFTK.filled_occupation(model)
+    filled_occ = filled_occupation(model)
     dVol = basis.model.unit_cell_volume / prod(basis.fft_size)
 
     EVs = [eigen(Hermitian(Array(Hk))) for Hk in ham.blocks]
     Es = [EV.values for EV in EVs]
     Vs = [EV.vectors for EV in EVs]
-    occ, εF = DFTK.find_occupation(basis, Es)
+    occ, εF = find_occupation(basis, Es)
 
     χ0 = zeros(eltype(basis), prod(fft_size), prod(fft_size))
     for ik = 1:length(basis.kpoints)
@@ -54,6 +54,7 @@ function compute_χ0(ham)
         Vr = hcat(G_to_r.(Ref(basis), Ref(basis.kpoints[ik]), eachcol(V))...)
         Vr = reshape(Vr, prod(fft_size), N)
         @showprogress "Computing χ0 for kpoint $ik/$(length(basis.kpoints)) ..." for m = 1:N, n = 1:N
+            diagonal_only && (n != m) && continue
             enred = (E[n] - εF) / model.temperature
             @assert occ[ik][n] ≈ filled_occ * Smearing.occupation(model.smearing, enred)
             factor = filled_occ * Smearing.occupation_divided_difference(model.smearing, E[m], E[n], εF, model.temperature)
@@ -70,5 +71,62 @@ function compute_χ0(ham)
         dos = DOS(εF, basis, Es)
         χ0 .+= (ldos .* ldos') .* dVol ./ dos
     end
+
     χ0
+end
+
+"""
+Returns the change in density δρ for a given δV
+"""
+function apply_chi0(ham, δV, ψ, occupation, εF, eigenvalues; diagonal_only=false)
+    # δρ = ∑_nk (f'n δεn |ψn|^2 + 2Re fn ψn* δψn - f'n δεF |ψn|^2
+    basis = ham.basis
+    model = basis.model
+    fft_size = basis.fft_size
+    @assert model.spin_polarisation == :none
+    filled_occ = filled_occupation(model)
+    dVol = basis.model.unit_cell_volume / prod(basis.fft_size)
+
+    δρ = zero(δV)
+    for ik = 1:length(basis.kpoints)
+        if length(basis.ksymops[ik]) != 1
+            error("Kpoint symmetry not supported")
+        end
+        for iband = 1:size(ψ[ik], 2)
+            ψnk = @views ψ[ik][:, iband]
+            ψnk_real = G_to_r(basis, basis.kpoints[ik], ψnk)
+            εnk = eigenvalues[ik][iband]
+            fnk = occupation[ik][iband]
+            @assert fnk ≈ filled_occ * Smearing.occupation(model.smearing, (εnk - εF)/model.temperature)
+
+            if model.temperature != 0
+                # f'n δεn |ψn|^2
+                fpnk = (filled_occ * 1 / model.temperature *
+                       Smearing.occupation_derivative(model.smearing, (εnk-εF) / model.temperature))
+                δεnk = dot(abs2.(ψnk_real), δV) * dVol
+                δρ .+= basis.kweights[ik] .* fpnk .* δεnk .* abs2.(ψnk_real)
+            end
+
+            diagonal_only && continue
+
+            # 2Re fn ψn* δψn
+            # we split δψn into its component on the computed and uncomputed states:
+            # δψn = P ψn + Q ψn
+            # we compute Pψn explicitly by sum over states, and
+            # Q δψn by solving the Sternheimer equation
+            # (H-εn) Q δψn = -Q δV ψn
+            # where Q = sum_n |ψn><ψn|
+            fnk == 0 && continue
+
+            ## TODO
+        end
+    end
+
+    if model.temperature > 0
+        ldos = LDOS(εF, basis, eigenvalues, ψ)
+        dos = DOS(εF, basis, eigenvalues)
+        δρ .+= ldos .* dot(ldos, δV) .* dVol ./ dos
+    end
+
+    δρ
 end
--- test/chi0.jl
+++ test/chi0.jl
@@ -4,24 +4,27 @@ using LinearAlgebra: norm
 
 include("testcases.jl")
 
-@testset "Computing χ0" begin
+# @testset "Computing χ0" begin
     Ecut=3
     fft_size = [10, 10, 1]
     tol=1e-10
     ε = 1e-8
     kgrid = [3, 1, 1]
     testcase = silicon
-    n_bands = 10
+    n_bands = 12
+    using Random
+    Random.seed!(0)
 
     spec = ElementPsp(testcase.atnum, psp=load_psp(testcase.psp))
-    for temperature in (0, 0.03)
+    # for temperature in (0, 0.03)
+temperature = .03
         model = model_LDA(testcase.lattice, [spec => testcase.positions], temperature=temperature)
         basis = PlaneWaveBasis(model, Ecut; kgrid=kgrid, fft_size=fft_size, enable_bzmesh_symmetry=false)
 
         ρ0 = guess_density(basis)
-        energies, ham = energy_hamiltonian(basis, nothing, nothing; ρ=ρ0)
-        ρ1 = DFTK.next_density(ham, tol=tol, eigensolver=diag_full, n_bands=n_bands).ρ
-        χ0 = compute_χ0(ham)
+        energies, ham0 = energy_hamiltonian(basis, nothing, nothing; ρ=ρ0)
+        ρ1 = DFTK.next_density(ham0, tol=tol, eigensolver=diag_full, n_bands=n_bands).ρ
+        χ0 = compute_χ0(ham0)
 
         # Now we make the same model, but add an artificial external potential ε * dV
         dV = randn(eltype(basis), basis.fft_size)
@@ -33,6 +36,19 @@ include("testcases.jl")
         diff = (ρ2.real - ρ1.real)/ε
 
         predicted_diff = real(reshape(χ0*vec(dV), basis.fft_size))
-        @test norm(diff - predicted_diff) < sqrt(ε)
-    end
-end
+        # @test norm(diff - predicted_diff) < sqrt(ε)
+
+        if temperature > 0
+            # Test the diagonal_only option
+            χ0_diag = compute_χ0(ham0; diagonal_only=true)
+            diff_diag_1 = real(reshape(χ0_diag*vec(dV), basis.fft_size))
+
+            EVs = [eigen(Hermitian(Array(Hk))) for Hk in ham0.blocks]
+            Es = [EV.values for EV in EVs]
+            Vs = [EV.vectors for EV in EVs]
+            occ, εF = find_occupation(basis, Es)
+            diff_diag_2 = apply_chi0(ham0, dV, Vs, occ, εF, Es; diagonal_only=true)
+            @test norm(diff_diag_1 - diff_diag_2) < sqrt(ε)
+        end
+    # end
+# end
