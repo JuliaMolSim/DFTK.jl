d583ebd16c7c9a71b2c8350e6468fbad08d1b3b9
Antoine Levitt <antoine.levitt@gmail.com>
2020-04-25 18:52:44 +0200

    add test

--- test/forces.jl
+++ test/forces.jl
@@ -8,12 +8,17 @@ include("testcases.jl")
         Si = ElementPsp(silicon.atnum, psp=load_psp(silicon.psp))
         atoms = [Si => pos]
         model = model_DFT(silicon.lattice, atoms, :lda_xc_teter93)
-        basis = PlaneWaveBasis(model, Ecut, kgrid=[2, 1, 2])
+        basis = PlaneWaveBasis(model, Ecut, kgrid=[2, 2, 2])
 
         is_converged = DFTK.ScfConvergenceDensity(1e-10)
         scfres = self_consistent_field(basis; is_converged=is_converged)
         sum(values(scfres.energies)), forces(scfres)
     end
+
+    # symmetrical positions, forces should be 0
+    pos0 = [(ones(3)) / 8, -ones(3) / 8]
+    _, F1 = energy(pos0)
+    @test norm(F1) < 1e-4
 
     pos1 = [([1.01, 1.02, 1.03]) / 8, -ones(3) / 8]  # displace a bit from equilibrium
     disp = rand(3)
@@ -25,7 +30,7 @@ include("testcases.jl")
 
     diff_findiff = -(E2 - E1) / ε
     diff_forces = dot(F1[1][1], disp)
-    @test abs(diff_findiff - diff_forces) < 2e-6
+    @test abs(diff_findiff - diff_forces) < 1e-4
 end
 
 @testset "Forces on metal (using free energy)" begin
