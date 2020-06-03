c2cd13fcd0dc612f30b935df6dfd266e7ede7cdb
Michael F. Herbst <info@michael-herbst.com>
2020-06-03 08:42:59 +0200

    Extend make_logo.sh

--- docs/logo/make_logo.sh
+++ docs/logo/make_logo.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 convert -density 150 -depth 10 -quality 95 DFTK_2to1.pdf DFTK_2000x1000.png
+convert -density "7.5" -depth 10 -quality 95 DFTK_2to1.pdf DFTK_100x50.png
 convert -density 60 -depth 10 -quality 95 DFTK_3to1.pdf DFTK_750x250.png
-# convert -density 300 -depth 10 -quality 95 DFTK.pdf DFTK.png
+convert -density 12 -depth 10 -quality 95 DFTK_3to1.pdf DFTK_150x50.png
+convert -density 300 -depth 10 -quality 95 DFTK.pdf DFTK.png
