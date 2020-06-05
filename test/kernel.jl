3bc64df1f0f0e5d07ea2064acd4ebf7871b1fe58
Michael F. Herbst <info@michael-herbst.com>
2020-06-05 19:14:07 +0200

    Proper GGA kernel test

--- test/kernel.jl
+++ test/kernel.jl
@@ -3,6 +3,8 @@ using Test
 
 include("testcases.jl")
 
+# TODO Test Hartree kernel
+
 @testset "LDA XC kernel" begin
     Ecut=3
     ε = 1e-8
@@ -37,30 +39,31 @@ end
 
 
 @testset "GGA XC kernel" begin
-    Ecut=3
+    Ecut=4
     kgrid = [3, 3, 3]
     testcase = silicon
+    tol=1e-10
     ε = 1e-6
 
     spec = ElementPsp(testcase.atnum, psp=load_psp(testcase.psp))
     model = model_DFT(testcase.lattice, [spec => testcase.positions],
                       [:gga_x_pbe, :gga_c_pbe])
     basis = PlaneWaveBasis(model, Ecut; kgrid=kgrid)
-    ρ = guess_density(basis)
+
+    # Do 1 SCF step
+    ρ0 = guess_density(basis)
+    energies, ham0 = energy_hamiltonian(basis, nothing, nothing; ρ=ρ0)
+    ρ1 = DFTK.next_density(ham0, tol=tol, eigensolver=diag_full, n_bands=6).ρ
 
     xc_terms = [t for t in basis.terms if t isa DFTK.TermXc]
     @assert length(xc_terms) == 1
     xc = xc_terms[1]
 
-    dρ = 0.01randn(size(ρ.real))
-    pot0 = ene_ops(xc, nothing, nothing; ρ=ρ).ops[1].potential
-    ρ_pert = from_real(basis, ρ.real .+ ε * dρ)
-    pot1 = ene_ops(xc, nothing, nothing; ρ=ρ_pert).ops[1].potential
+    pot0 = ene_ops(xc, nothing, nothing; ρ=ρ1).ops[1].potential
+    ρ1_pert = from_real(basis, ρ1.real .+ ε * ρ0.real)
+    pot1 = ene_ops(xc, nothing, nothing; ρ=ρ1_pert).ops[1].potential
 
-    response = DFTK.apply_kernel(xc, from_real(basis, dρ), ρ=ρ)
-    @test pot0 + ε .* response ≈ pot1 rtol=1e-6
+    response = DFTK.apply_kernel(xc, from_real(basis, ρ0.real), ρ=ρ1)
+
+    @test ((pot1 - pot0) ./ ε) ≈ response atol=5e-6
 end
-
-
-
-# TODO Test Hartree kernel
