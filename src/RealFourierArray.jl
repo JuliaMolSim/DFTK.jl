f7a639070d45b6f6900d2be76d5f6f97f4152282
Michael F. Herbst <info@michael-herbst.com>
2020-06-16 16:47:08 +0200

    Add length as well

--- src/RealFourierArray.jl
+++ src/RealFourierArray.jl
@@ -3,7 +3,7 @@
 
 import Base.getproperty, Base.setproperty!, Base.propertynames
 import Base.*, Base.+, Base./
-import Base.eltype, Base.size
+import Base.eltype, Base.size, Base.length
 
 ## TODO these functions are minimally optimized for the moment
 ## TODO this always allocates both real and fourier parts, which might be a bit wasteful
@@ -30,6 +30,7 @@ end
 Base.eltype(::RealFourierArray{Treal, T}) where {Treal, T} = T
 Base.size(array::RealFourierArray) = size(array._real)
 Base.size(array::RealFourierArray, i) = size(array._real, i)
+Base.length(array::RealFourierArray) = length(array._real)
 
 # Constructors
 RealFourierArray(basis, real, fourier) = RealFourierArray(basis, real, fourier, 2)
