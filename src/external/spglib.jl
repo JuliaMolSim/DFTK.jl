15a8bb5fc1fc20466293861c41471b85fad6bd3b
Unknown <louisponet@gmail.com>
2020-06-22 12:51:28 +0200

    more code review, spglib row vs column not resolved yet

--- src/external/spglib.jl
+++ src/external/spglib.jl
@@ -34,7 +34,7 @@ end
 spglib_atoms(atoms) = first(spglib_atommapping(atoms))
 
 
-@timing function spglib_get_symmetry(lattice, atoms; tol_symmetry=1e-5, max_symops=384)
+@timing function spglib_get_symmetry(lattice, atoms; tol_symmetry=1e-5)
     lattice = Matrix{Float64}(lattice)  # spglib operates in double precision
 
     if isempty(atoms)
@@ -44,26 +44,29 @@ spglib_atoms(atoms) = first(spglib_atommapping(atoms))
     end
 
     # Ask spglib for symmetry operations and for irreducible mesh
-    spg_rotations    = Array{Cint}(undef, 3, 3, max_symops)
-    spg_translations = Array{Cdouble}(undef, 3, max_symops)
     spg_positions, spg_numbers = spglib_atoms(atoms)
-    
-    spg_num_ops = ccall((:spg_get_symmetry, SPGLIB), Cint,
-       (Ptr{Cint}, Ptr{Cdouble}, Cint, Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cint}, Cint, Cdouble),
-       spg_rotations, spg_translations, Cint(max_symops), lattice, spg_positions, spg_numbers,
-       Cint(length(spg_numbers)), tol_symmetry)
-       
-    # Note: spglib_jll returns transposed rotation matrix vs the python spglib interface
 
+    spg_n_ops = ccall((:spg_get_multiplicity, SPGLIB), Cint,
+        (Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cint}, Cint, Cdouble),
+        lattice, spg_positions, spg_numbers, Cint(length(spg_numbers)), tol_symmetry)
+    
+    spg_rotations    = Array{Cint}(undef, 3, 3, spg_n_ops)
+    spg_translations = Array{Cdouble}(undef, 3, spg_n_ops)
+    
+    ccall((:spg_get_symmetry, SPGLIB), Cint,
+         (Ptr{Cint}, Ptr{Cdouble}, Cint, Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cint}, Cint, Cdouble),
+         spg_rotations, spg_translations, spg_n_ops, lattice, spg_positions, spg_numbers,
+         Cint(length(spg_numbers)), tol_symmetry)
+  
     # If spglib does not find symmetries give an error
-    if spg_num_ops == 0
+    if spg_n_ops == 0
         err_message = spglib_get_error_message()
         error("spglib failed to get the symmetries. Check your lattice, use a " *
               "uniform BZ mesh or disable symmetries. Spglib reported : " * err_message)
     end
 
-    Stildes = [Mat3{Int}(spg_rotations[:, :, i])' for i in 1:spg_num_ops]
-    τtildes = [rationalize.(Vec3{Float64}(spg_translations[:, i]), tol=tol_symmetry) for i in 1:spg_num_ops]
+    Stildes = [Mat3{Int}(spg_rotations[:, :, i])' for i in 1:spg_n_ops]
+    τtildes = [rationalize.(Vec3{Float64}(spg_translations[:, i]), tol=tol_symmetry) for i in 1:spg_n_ops]
     
     # Checks: (A Stilde A^{-1}) is unitary
     for Stilde in Stildes
@@ -96,7 +99,6 @@ end
 
 function spglib_standardize_cell(lattice::AbstractArray{T}, atoms; correct_symmetry=true,
                                  primitive=false, tol_symmetry=1e-5) where {T}
-                                 
     # Convert lattice and atoms to spglib and keep the mapping between our atoms
     spg_lattice = Matrix{Float64}(lattice) 
     # and spglibs atoms
@@ -109,9 +111,6 @@ function spglib_standardize_cell(lattice::AbstractArray{T}, atoms; correct_symme
       spg_lattice, spg_positions, spg_numbers, length(spg_numbers), Cint(primitive),
       Cint(!correct_symmetry), tol_symmetry)
 
-    # Note: In the python interface of spglib the lattice vectors
-    #       are given in rows, but DFTK uses columns
-    #       For future reference: The C interface spglib also uses columns.
     newatoms = [(atommapping[iatom]
                  => T.(spg_positions[findall(isequal(iatom), spg_numbers), :]))
                 for iatom in unique(spg_numbers)]
@@ -121,15 +120,13 @@ end
 function spglib_get_stabilized_reciprocal_mesh(kgrid_size, rotations;
                                                is_shift=Vec3(0, 0, 0),
                                                is_time_reversal=false,
-                                               qpoints=[Vec3(0.0, 0.0, 0.0)],
+                                               qpoints::Vector{Vec3{Float64}}=[Vec3(0.0, 0.0, 0.0)],
                                                isdense=false)
-
     rotations = eltype(rotations) == Int32 ? rotations : Cint.(rotations)
     nkpt = prod(kgrid_size)
     mapping = Vector{Cint}(undef, nkpt)
     grid_address = Matrix{Cint}(undef, 3, nkpt)
-    qpoints = eltype(qpoints) == Real ? [qpoints] : qpoints # Note: Done similarly to the python spglib
-    
+   
     numrot = size(rotations, 3)
     num_kpts = ccall((:spg_get_stabilized_reciprocal_mesh, SPGLIB), Cint,
       (Ptr{Cint}, Ptr{Cint}, Ptr{Cint}, Ptr{Cint}, Cint, Cint, Ptr{Cint}, Cint, Ptr{Cdouble}),
