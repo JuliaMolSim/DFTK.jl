c684a2aa7edf12700e417ed6808142a44cb35386
Michael F. Herbst <info@michael-herbst.com>
2020-06-30 07:52:21 +0200

    Polish SCF printing and automatic diagtol selection (#271)
    * Change column order in SCF printing
    * Enforce lower tolerance in second SCF iteration
    * Show plot of surface

--- src/scf/self_consistent_field.jl
+++ src/scf/self_consistent_field.jl
@@ -33,15 +33,15 @@ function ScfDefaultCallback()
     function callback(info)
         if info.n_iter == 1
             E_label = haskey(info.energies, "Entropy") ? "Free energy" : "Energy"
-            @printf "n     %-12s      ρout-ρin   Eₙ₋₁-Eₙ    Diag\n" E_label
-            @printf "---   ---------------   --------   --------   ----\n"
+            @printf "n     %-12s      Eₙ-Eₙ₋₁     ρout-ρin   Diag\n" E_label
+            @printf "---   ---------------   ---------   --------   ----\n"
         end
         E = info.energies === nothing ? Inf : info.energies.total
         prev_E = prev_energies === nothing ? Inf : prev_energies.total
-        res = norm(info.ρout.fourier - info.ρin.fourier)
-        ΔE = prev_E == Inf ? "      NaN" : @sprintf "% 3.2e" prev_E - E
+        Δρ = norm(info.ρout.fourier - info.ρin.fourier)
+        ΔE = prev_E == Inf ? "      NaN" : @sprintf "% 3.2e" E - prev_E
         diagiter = sum(info.diagonalization.iterations) / length(info.diagonalization.iterations)
-        @printf "%-3d   %-15.12f   %2.2e  %s   %3.1f \n" info.n_iter E res ΔE diagiter
+        @printf "% 3d   %-15.12f   %s   %2.2e   % 3.1f \n" info.n_iter E ΔE Δρ diagiter
         prev_energies = info.energies
     end
     callback
@@ -83,12 +83,14 @@ and never increases.
 function ScfDiagtol(;ratio_ρdiff=0.2, diagtol_min=nothing, diagtol_max=0.03)
     function determine_diagtol(info)
         isnothing(diagtol_min) && (diagtol_min = 100eps(real(eltype(info.ρin))))
-        info.n_iter ≤ 1 && return diagtol_max
+        info.n_iter ≤ 0 && return diagtol_max
+        info.n_iter == 1 && (diagtol_max /= 5)  # Enforce more accurate Bloch wave
 
         diagtol = norm(info.ρnext.fourier - info.ρin.fourier) * ratio_ρdiff
         diagtol = min(diagtol_max, diagtol)  # Don't overshoot
         diagtol = max(diagtol_min, diagtol)  # Don't undershoot
         @assert isfinite(diagtol)
+
 
         # Adjust maximum to ensure diagtol may only shrink during an SCF
         diagtol_max = min(diagtol, diagtol_max)
