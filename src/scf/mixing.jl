a21720d9b3e10a4bcc1e3ad46e90692becfd046c
Michael F. Herbst <info@michael-herbst.com>
2020-06-30 21:20:24 +0200

    Implement HybridMixing (#272)

--- src/scf/mixing.jl
+++ src/scf/mixing.jl
@@ -1,5 +1,6 @@
 using LinearMaps
 using IterativeSolvers
+import Base: @kwdef
 
 # Mixing rules: (ρin, ρout) => ρnext, where ρout is produced by diagonalizing the
 # Hamiltonian at ρin These define the basic fix-point iteration, that are then combined with
@@ -12,19 +13,18 @@ using IterativeSolvers
 # The interface is `mix(m; basis, ρin, ρout, kwargs...) -> ρnext`
 
 @doc raw"""
-Kerker mixing: ``J^{-1} ≈ \frac{α G^2}{(k_F^2 + G^2}``
+Kerker mixing: ``J^{-1} ≈ \frac{α G^2}{k_F^2 + G^2}``
 where ``k_F`` is the Thomas-Fermi wave vector.
 
 Notes:
   - Abinit calls ``1/k_F`` the dielectric screening length (parameter *dielng*)
 """
-struct KerkerMixing
-    α::Real
-    kF::Real
+@kwdef struct KerkerMixing
+    # Default parameters suggested by Kresse, Furthmüller 1996 (α=0.8, kF=1.5 Ǎ^{-1})
+    # DOI 10.1103/PhysRevB.54.11169
+    α::Real = 0.8
+    kF::Real = 0.8
 end
-# Default parameters suggested by Kresse, Furthmüller 1996 (α=0.8, kF=1.5 Ǎ^{-1})
-# DOI 10.1103/PhysRevB.54.11169
-KerkerMixing(;α=0.8, kF=0.8) = KerkerMixing(α, kF)
 function mix(mixing::KerkerMixing, basis, ρin::RealFourierArray, ρout::RealFourierArray; kwargs...)
     T = eltype(basis)
     Gsq = [sum(abs2, basis.model.recip_lattice * G) for G in G_vectors(basis)]
@@ -39,10 +39,9 @@ end
 @doc raw"""
 Simple mixing: ``J^{-1} ≈ α``
 """
-struct SimpleMixing
-    α::Real
+@kwdef struct SimpleMixing
+    α::Real = 0.8
 end
-SimpleMixing(;α=1) = SimpleMixing(α)
 function mix(mixing::SimpleMixing, basis, ρin::RealFourierArray, ρout::RealFourierArray; kwargs...)
     T = eltype(basis)
     if mixing.α == 1
@@ -54,20 +53,19 @@ end
 
 @doc raw"""
 We use a simplification of the Resta model DOI 10.1103/physrevb.16.2717 and set
-``χ_0(q) = \frac{C G^2}{4π (1 - C G^2 / k_F^2)}
-where ``C = 1 - ε_r`` with ``ε_r`` being the macroscopic relative permittivity.
+``χ_0(q) = \frac{C_0 G^2}{4π (1 - C_0 G^2 / k_F^2)}
+where ``C_0 = 1 - ε_r`` with ``ε_r`` being the macroscopic relative permittivity.
 We neglect ``f_\text{xc}``, such that
-``J^{-1} ≈ α \frac{k_F^2 - C G^2}{ε_r k_F^2 - C G^2}``
+``J^{-1} ≈ α \frac{k_F^2 - C_0 G^2}{ε_r k_F^2 - C_0 G^2}``
 
 By default it assumes a relative permittivity of 10 (similar to Silicon).
 `εr == 1` is equal to `SimpleMixing` and `εr == Inf` to `KerkerMixing`.
 """
-struct RestaMixing
-    α::Real
-    εr::Real
-    kF::Real
+@kwdef struct RestaMixing
+    α::Real = 0.8
+    εr::Real = 0.8
+    kF::Real = 10
 end
-RestaMixing(;α=0.8, kF=0.8, εr=10) = RestaMixing(α, εr, kF)
 function mix(mixing::RestaMixing, basis, ρin::RealFourierArray, ρout::RealFourierArray; kwargs...)
     T = eltype(basis)
     εr = T(mixing.εr)
@@ -77,11 +75,86 @@ function mix(mixing::RestaMixing, basis, ρin::RealFourierArray, ρout::RealFour
 
     ρin = ρin.fourier
     ρout = ρout.fourier
-    C = 1 - εr
+    C0 = 1 - εr
     Gsq = [sum(abs2, basis.model.recip_lattice * G) for G in G_vectors(basis)]
 
-    ρnext = @. ρin + T(mixing.α) * (ρout - ρin) * (kF^2 - C * Gsq) / (εr * kF^2 - C * Gsq)
+    ρnext = @. ρin + T(mixing.α) * (ρout - ρin) * (kF^2 - C0 * Gsq) / (εr * kF^2 - C0 * Gsq)
     # take the correct DC component from ρout; otherwise the DC component never gets updated
     ρnext[1] = ρout[1]
     from_fourier(basis, ρnext)
+end
+
+
+@doc raw"""
+The model for the susceptibility is
+```math
+χ_0(r, r') = (-LDOS(εF, r) δ(r, r') + LDOS(εF, r) LDOS(εF, r') / DOS(εF))
+           + \sqrt{L(x)} IFFT \frac{C_0 G^2}{4π (1 - C_0 G^2 / k_F^2)} FFT \sqrt{L(x)}
+```
+where ``C_0 = 1 - ε_r`` and the same convention for parameters is used as in `RestaMixing`.
+Additionally there is the real-space localisation function `L(r)`.
+"""
+@kwdef struct HybridMixing
+    α::Real = 0.8
+    εr::Real = 10
+    kF::Real = 0.8
+    localisation::Function = identity  # `L(r)` with `r` in fractional real-space coordinates
+    RPA::Bool = true       # Use RPA, i.e. only apply the Hartree and not the XC Kernel
+    verbose::Bool = false  # Run the GMRES verbosely
+end
+
+function mix(mixing::HybridMixing, basis, ρin::RealFourierArray, ρout::RealFourierArray;
+             εF, eigenvalues, ψ, kwargs...)
+    T = eltype(basis)
+    εr = T(mixing.εr)
+    kF = T(mixing.kF)
+    C0 = 1 - εr
+    Gsq = [sum(abs2, basis.model.recip_lattice * G) for G in G_vectors(basis)]
+    dVol = basis.model.unit_cell_volume / prod(basis.fft_size)
+
+    apply_sqrtL = identity
+    if mixing.localisation != identity
+        sqrtL = sqrt.(mixing.localisation.(r_vectors(basis)))
+        apply_sqrtL = x -> from_real(basis, sqrtL .* x.real)
+    end
+
+    # Compute the LDOS if required
+    ldos = nothing
+    if basis.model.temperature > 0
+        ldos = LDOS(εF, basis, eigenvalues, ψ)
+    end
+
+    # Solve J Δρ = ΔF with J = (1 - χ0 vc) and χ_0 given as in the docstring of the class
+    ΔF = ρout.real - ρin.real
+    devec(x) = reshape(x, size(ρin))
+    function Jop(x)
+        δρ = devec(x)
+        Jδρ = copy(δρ)
+
+        # Apply Kernel (just vc for RPA and (vc + fxc) if not RPA)
+        δV = apply_kernel(basis, from_real(basis, δρ), ρ=ρin, RPA=mixing.RPA)
+        δV.real .-= sum(δV.real) / length(δV.real)  # set DC to zero
+
+        # Apply Resta term of χ0
+        loc_δV = apply_sqrtL(δV).fourier
+        resta_loc_δV =  @. C0 * kF^2 * Gsq / 4T(π) / (kF^2 - C0 * Gsq) * loc_δV
+        Jδρ .-= apply_sqrtL(from_fourier(basis, resta_loc_δV)).real
+
+        # Apply LDOS term of χ0
+        if ldos !== nothing
+            Jδρ .-= (-ldos .* δV.real
+                     .+ sum(ldos .* δV.real) .* dVol .* ldos ./ (sum(ldos) .* dVol))
+        end
+
+        # Zero DC component and return
+        vec(Jδρ .-= sum(Jδρ) / length(Jδρ))
+    end
+    J = LinearMap(Jop, length(ρin))
+    x = gmres(J, ΔF, verbose=mixing.verbose)
+    Δρ = devec(x)
+    ρnext = real(@. ρin.real + T(mixing.α) * Δρ)
+
+    # Set DC from ρout
+    ρnext .+= (sum(ρout.real) - sum(ρnext)) / length(ρnext)
+    from_real(basis, ρnext)
 end
