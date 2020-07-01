040ae9286e435d6ac4fbae44aa455f1ab97043d5
Antoine Levitt <antoine.levitt@gmail.com>
2020-07-01 17:14:00 +0200

    refactoring

--- src/fft.jl
+++ src/fft.jl
@@ -1,5 +1,24 @@
 import FFTW
 
+# returns the lengths of the bounding rectangle in reciprocal space
+# that encloses the sphere of radius Gmax
+function bounding_rectangle(lattice::AbstractMatrix{T}, Gmax; tol=1e-8) where {T}
+    # If |B G| ≤ Gmax, then
+    # |Gi| ≤ |e_i^T B^-1 B G| ≤ |B^-T e_i| Gmax = |A_i| Gmax
+    # with B the reciprocal lattice matrix and A_i the i-th column of the lattice matrix
+    Glims_ = [norm(lattice[:, i]) / 2T(π) * Gmax for i in 1:3]
+
+    # Round up, unless exactly zero (in which case keep it zero in
+    # order to just have one G vector for 1D or 2D systems)
+    Glims = [0, 0, 0]
+    for i = 1:3
+        if Glims_[i] != 0
+            Glims[i] = ceil(Int, Glims_[i] .- tol)
+        end
+    end
+    Glims
+end
+
 @doc raw"""
 Determine the minimal grid size for the cubic basis set to be able to
 represent product of orbitals (with the default `supersampling=2`).
@@ -7,29 +26,21 @@ represent product of orbitals (with the default `supersampling=2`).
 Optionally optimize the grid afterwards for the FFT procedure by
 ensuring factorization into small primes.
 
-The function will determine the smallest cube containing the wave vectors
+The function will determine the smallest parallelepiped containing the wave vectors
  ``|G|^2/2 \leq E_\text{cut} ⋅ \text{supersampling}^2``.
 For an exact representation of the density resulting from wave functions
 represented in the spherical basis sets, `supersampling` should be at least `2`.
 """
 function determine_grid_size(lattice::AbstractMatrix{T}, Ecut; supersampling=2, tol=1e-8,
-                             ensure_smallprimes=true) where T
-    cutoff_Gsq = 2 * supersampling^2 * Ecut
-    Gmax = [norm(lattice[:, i]) / 2T(π) * sqrt(cutoff_Gsq) for i in 1:3]
-
-    # Round up, unless exactly zero (in which case keep it zero in
-    # order to just have one G vector for 1D or 2D systems)
-    for i = 1:3
-        if Gmax[i] != 0
-            Gmax[i] = ceil.(Int, Gmax[i] .- tol)
-        end
-    end
+                             ensure_smallprimes=false) where T
+    Gmax = supersampling * sqrt(2 * Ecut)
+    Glims = bounding_rectangle(lattice, Gmax; tol=tol)
 
     # Optimize FFT grid size: Make sure the number factorises in small primes only
     if ensure_smallprimes
-        Vec3([nextprod([2, 3, 5], 2gs + 1) for gs in Gmax])
+        Vec3([nextprod([2, 3, 5], 2gs + 1) for gs in Glims])
     else
-        Vec3([2gs + 1 for gs in Gmax])
+        Vec3([2gs + 1 for gs in Glims])
     end
 end
 function determine_grid_size(model::Model, Ecut; kwargs...)
@@ -38,8 +49,8 @@ end
 
 # For Float32 there are issues with aligned FFTW plans, so we
 # fall back to unaligned FFTW plans (which are generally discouraged).
-_fftw_flags(T::Float32) = FFTW.MEASURE | FFTW.UNALIGNED
-_fftw_flags(T::Float64) = FFTW.MEASURE
+_fftw_flags(T::Type{Float32}) = FFTW.MEASURE | FFTW.UNALIGNED
+_fftw_flags(T::Type{Float64}) = FFTW.MEASURE
 
 """
 Plan a FFT of type `T` and size `fft_size`, spending some time on finding an
