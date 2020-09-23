8d2a87933e47dd48ae54f8488e265f07fcf6e7c4
Michael F. Herbst <info@michael-herbst.com>
2020-09-23 14:51:42 +0200

    Typesetting

--- src/terms/anyonic.jl
+++ src/terms/anyonic.jl
@@ -2,10 +2,10 @@
 # This term does not contain the kinetic energy, which must be added separately
 # /!\ They have no 1/2 factor in front of the kinetic energy,
 #     so for consistency the added kinetic energy must have a scaling of 2
-# Energy = <u, ((-i∇+βA)^2 + V) u>
+# Energy = <u, ((-i∇ + βA)^2 + V) u>
 # where ∇∧A = 2π ρ, ∇⋅A = 0 => A = x^⟂/|x|² ∗ ρ
-# H = (-i∇+βA)² + V - 2β x⟂/|x|² ∗ (βAρ + J)
-#   = -Δ + 2β (-i∇)⋅A + β²|A|^2 - 2β x⟂/|x|² ∗ (βAρ + J)
+# H = (-i∇ + βA)² + V - 2β x^⟂/|x|² ∗ (βAρ + J)
+#   = -Δ + 2β (-i∇)⋅A + β²|A|^2 - 2β x^⟂/|x|² ∗ (βAρ + J)
 # where only the first three terms "count for the energy", and where
 # J = 1/(2i) (u* ∇u - u ∇u*)
 
@@ -60,12 +60,12 @@ function ene_ops(term::TermAnyonic, ψ, occ; ρ, kwargs...)
                   RealSpaceMultiplication(basis, basis.kpoints[1],
                                           β^2 .* (abs2.(Areal[1]) .+ abs2.(Areal[2])))]
 
-    # Now compute effective local potential - 2β x⟂/|x|² ∗ (βAρ + J)
+    # Now compute effective local potential - 2β x^⟂/|x|² ∗ (βAρ + J)
     J = compute_current(basis, ψ, occ)
     eff_current = [J[α].real .+
                    β .* ρ.real .* Areal[α] for α = 1:2]
     eff_current_fourier = [from_real(basis, eff_current[α]).fourier for α = 1:2]
-    # eff_pot = - 2β x⟂/|x|² ∗ eff_current
+    # eff_pot = - 2β x^⟂/|x|² ∗ eff_current
     # => ∇∧eff_pot = -4πβ eff_current
     # => eff_pot(G1, G2) = -4πβ i eff_current(G1, G2) * [-G2;G1;0]/(G1^2 + G2^2)
     eff_pot_fourier = zeros(complex(T), basis.fft_size)
