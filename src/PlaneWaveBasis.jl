30f9a08143cd73ff24f5ddccffb0ef1fc7ebdde3
Eric Berquist <eric.berquist@gmail.com>
2020-10-17 20:47:58 -0400

    Remove unbound type parameter

--- src/PlaneWaveBasis.jl
+++ src/PlaneWaveBasis.jl
@@ -141,7 +141,7 @@ build_kpoints(basis::PlaneWaveBasis, kcoords) =
 @timing function PlaneWaveBasis(model::Model{T}, Ecut::Number,
                                 kcoords::AbstractVector, ksymops, symmetries=nothing;
                                 fft_size=nothing, variational=true,
-                                optimize_fft_size=false, supersampling=2) where {T <: Real, D}
+                                optimize_fft_size=false, supersampling=2) where {T <: Real}
     # TODO this constructor is too complicated, we should simplify it
     # if possible (esp. the variational part)
     mpi_ensure_initialized()
