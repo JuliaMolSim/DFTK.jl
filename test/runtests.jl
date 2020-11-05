6052bcf19f8a7ea61f7a7ba3b16b287abba6560f
Michael F. Herbst <info@michael-herbst.com>
2020-11-05 14:57:03 +0100

    Bugfix in kpoint-splitting over MPI processes

--- test/runtests.jl
+++ test/runtests.jl
@@ -53,6 +53,7 @@ Random.seed!(0)
     end
 
     if "all" in TAGS
+        include("split_evenly.jl")
         include("determine_fft_size.jl")
         include("fourier_transforms.jl")
         include("PlaneWaveBasis.jl")
