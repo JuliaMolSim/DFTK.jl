ecf244995f7aa0b804e7022d2ccfc9cf18efb8f4
Eric Berquist <eric.berquist@gmail.com>
2020-11-18 01:04:55 -0500

    Remove misleading unit additions in force tests

--- test/forces.jl
+++ test/forces.jl
@@ -3,13 +3,11 @@ import DFTK: mpi_mean!
 using Test
 using Random
 using MPI
-using Unitful
-using UnitfulAtomic
 include("testcases.jl")
 
 @testset "Forces on semiconductor (using total energy)" begin
     function energy_forces(pos)
-        Ecut = 5 * u"hartree"
+        Ecut = 5
         Si = ElementPsp(silicon.atnum, psp=load_psp(silicon.psp))
         atoms = [Si => pos]
         model = model_DFT(silicon.lattice, atoms, :lda_xc_teter93)
@@ -46,7 +44,7 @@ end
 
 @testset "Forces on metal (using free energy)" begin
     function silicon_energy_forces(pos; smearing=Smearing.FermiDirac())
-        Ecut = 4 * u"hartree"
+        Ecut = 4
         Si = ElementPsp(silicon.atnum, psp=load_psp(silicon.psp))
         atoms = [Si => pos]
         model = model_DFT(silicon.lattice, atoms, :lda_xc_teter93;
