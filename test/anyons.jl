da61f7fd4673d3de5bfed26f0bd132247f449c87
Antoine Levitt <antoine.levitt@gmail.com>
2020-11-27 14:31:22 +0100

    Review
    Co-authored-by: Michael F. Herbst <info@michael-herbst.com>

--- test/anyons.jl
+++ test/anyons.jl
@@ -9,8 +9,10 @@ using Test
     ε = 1e-8
     M = 2.31
     σ = 1.81
-    dAdx = (DFTK.magnetic_field_produced_by_ρref(x+ε, y, M, σ) - DFTK.magnetic_field_produced_by_ρref(x, y, M, σ))/ε
-    dAdy = (DFTK.magnetic_field_produced_by_ρref(x, y+ε, M, σ) - DFTK.magnetic_field_produced_by_ρref(x, y, M, σ))/ε
+    dAdx = (  DFTK.magnetic_field_produced_by_ρref(x + ε, y, M, σ)
+            - DFTK.magnetic_field_produced_by_ρref(x,     y, M, σ)) / ε
+    dAdy = (  DFTK.magnetic_field_produced_by_ρref(x, y + ε, M, σ)
+            - DFTK.magnetic_field_produced_by_ρref(x, y,     M, σ)) / ε
     curlA = dAdx[2] - dAdy[1]
     divA = dAdx[1] + dAdy[2]
     @test norm(curlA - 2π*DFTK.ρref_real(x, y, M, σ)) < 1e-4
