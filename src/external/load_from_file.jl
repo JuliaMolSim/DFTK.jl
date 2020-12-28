97e4e37d5c317e999d3aa593003dbc59897fb957
Michael F. Herbst <info@michael-herbst.com>
2020-12-28 18:04:50 +0100

    Make NCDatatests optional

--- src/external/load_from_file.jl
+++ src/external/load_from_file.jl
@@ -5,17 +5,16 @@
 using PyCall
 
 
-function load_lattice(file::AbstractString)
+function load_lattice(T, file::AbstractString)
     ase = pyimport_e("ase")
     ispynull(ase) && error("Install ASE to load data from exteral files")
-    load_lattice(pyimport("ase.io").read(file))
+    load_lattice(T, pyimport("ase.io").read(file))
 end
 
-
-function load_atoms(file::AbstractString)
+function load_atoms(T, file::AbstractString)
     ase = pyimport_e("ase")
     ispynull(ase) && error("Install ASE to load data from exteral files")
-    load_atoms(pyimport("ase.io").read(file))
+    load_atoms(T, pyimport("ase.io").read(file))
 end
 
 function load_magnetic_moments(file::AbstractString)
@@ -23,3 +22,19 @@ function load_magnetic_moments(file::AbstractString)
     ispynull(ase) && error("Install ASE to load data from exteral files")
     load_magnetic_moments(pyimport("ase.io").read(file))
 end
+
+"""
+Return a DFTK-compatible `lattice` object loaded from an ASE `Atoms`, a pymatgen `Structure`
+or a compatible file (e.g. xyz file, cif file etc.)
+"""
+load_lattice(args...; kwargs...) = load_lattice(Float64, args...; kwargs...)
+
+"""
+Return a DFTK-compatible `atoms` object loaded from an ASE `Atoms`, a pymatgen `Structure`
+or a compatible file (e.g. xyz file, cif file etc.)
+"""
+load_atoms(  args...; kwargs...) = load_atoms(  Float64, args...; kwargs...)
+
+load_model(  args...; kwargs...) = load_model(  Float64, args...; kwargs...)
+load_basis(  args...; kwargs...) = load_basis(  Float64, args...; kwargs...)
+load_density(args...; kwargs...) = load_density(Float64, args...; kwargs...)
