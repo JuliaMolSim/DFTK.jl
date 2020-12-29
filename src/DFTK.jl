93890931992ad256706cc6cb23ab3823d1a51d15
Michael F. Herbst <info@michael-herbst.com>
2020-12-29 09:44:58 +0100

    Make NCDatatests optional (#396)

--- src/DFTK.jl
+++ src/DFTK.jl
@@ -136,14 +136,12 @@ include("pseudo/list_psp.jl")
 
 export pymatgen_structure
 export ase_atoms
-export EtsfFolder
 export load_lattice
 export load_basis
 export load_model
 export load_density
 export load_atoms
 export load_magnetic_moments
-include("external/etsf_nanoquanta.jl")
 include("external/abinit.jl")
 include("external/load_from_python.jl")
 include("external/load_from_file.jl")
@@ -186,6 +184,10 @@ function __init__()
     end
     @require Plots="91a5bcdd-55d7-5caf-9e0b-520d859cae80" include("plotting.jl")
     @require JLD2="033835bb-8acc-5ee8-8aae-3f567f8a3819"  include("jld2io.jl")
+
+    @require NCDatasets="85f8d34a-cbdd-5861-8df4-14fed0d494ab" begin
+        include("external/etsf_nanoquanta.jl")
+    end
 end
 
 end # module DFTK
