2d4faaa45cb3f1deaf10c76cf833cf54fe3a61c8
Antoine Levitt <antoine.levitt@gmail.com>
2021-01-04 16:13:28 +0100

    review

--- src/pseudo/NormConservingPsp.jl
+++ src/pseudo/NormConservingPsp.jl
@@ -1,4 +1,6 @@
+# Abstract interface for a norm-conserving pseudopotential. See PspHgh for an example.
 abstract type NormConservingPsp end
+
 # Subtypes must implement the following:
 
 #### Fields:
@@ -11,35 +13,38 @@ abstract type NormConservingPsp end
 
 #### Methods:
 """
-    eval_psp_projector_real(psp::PspHgh, i, l, q::Real)
+    eval_psp_projector_real(psp::PspHgh, i, l, r::Real)
 
 Evaluate the radial part of the `i`-th projector for angular momentum `l`
 in real-space at the vector with modulus `r`.
 """
-eval_psp_projector_real(psp::NormConservingPsp, i, l, r::T) where {T <: Real} =
+eval_psp_projector_real(psp::NormConservingPsp, i, l, r::Real) =
     error("Not implemented")
-eval_psp_projector_real(psp::NormConservingPsp, i, l, r::AbstractVector) = eval_psp_projector_real(psp, i, l, norm(r))
+eval_psp_projector_real(psp::NormConservingPsp, i, l, r::AbstractVector) =
+    eval_psp_projector_real(psp, i, l, norm(r))
 
 """
-    eval_psp_projector_fourier(psp::PspHgh, i, l, q::Number)
+    eval_psp_projector_fourier(psp::PspHgh, i, l, q::Real)
 
 Evaluate the radial part of the `i`-th projector for angular momentum `l`
 at the reciprocal vector with modulus `q`:
 p(q) = ∫_R^3 p_{il}(r) e^{-iqr} dr
      = 4π ∫_{R+} r^2 p_{il}(r) j_l(q r) dr
 """
-eval_psp_projector_fourier(psp::NormConservingPsp, i, l, q::T) where {T <: Real} =
+eval_psp_projector_fourier(psp::NormConservingPsp, i, l, q::Real) =
     error("Not implemented")
-eval_psp_projector_fourier(psp::NormConservingPsp, q::AbstractVector) = eval_psp_projector_fourier(psp, norm(q))
+eval_psp_projector_fourier(psp::NormConservingPsp, q::AbstractVector) =
+    eval_psp_projector_fourier(psp, norm(q))
 
 """
     eval_psp_local_real(psp, r)
 
 Evaluate the local part of the pseudopotential in real space.
 """
-eval_psp_local_real(psp::NormConservingPsp, r::T) where {T <: Real} =
+eval_psp_local_real(psp::NormConservingPsp, r::Real) =
     error("Not implemented")
-eval_psp_local_real(psp::NormConservingPsp, r::AbstractVector) = eval_psp_local_real(psp, norm(r))
+eval_psp_local_real(psp::NormConservingPsp, r::AbstractVector) =
+    eval_psp_local_real(psp, norm(r))
 
 """
     eval_psp_local_fourier(psp, q)
@@ -48,9 +53,10 @@ Evaluate the local part of the pseudopotential in reciprocal space:
 V(q) = ∫_R^3 Vloc(r) e^{-iqr} dr
      = 4π ∫_{R+} sin(qr)/q r e^{-iqr} dr
 """
-eval_psp_local_fourier(psp::NormConservingPsp, q::T) where {T <: Real} = 
+eval_psp_local_fourier(psp::NormConservingPsp, q) =
     error("Not implemented")
-eval_psp_local_fourier(psp::NormConservingPsp, q::AbstractVector) = eval_psp_local_fourier(psp, norm(q))
+eval_psp_local_fourier(psp::NormConservingPsp, q::AbstractVector) =
+    eval_psp_local_fourier(psp, norm(q))
 
 """
     eval_psp_energy_correction([T=Float64,] psp, n_electrons)
@@ -65,7 +71,8 @@ To obtain the latter, the caller needs to divide by the unit cell volume.
 """
 eval_psp_energy_correction(T, psp::NormConservingPsp, n_electrons) = zero(T)
 # by default, no correction
-
+eval_psp_energy_correction(psp::NormConservingPsp, n_electrons) =
+    eval_psp_energy_correction(Float64, psp, n_electrons)
 
 
 import Base.Broadcast.broadcastable
@@ -76,13 +83,10 @@ function projector_indices(psp::NormConservingPsp)
      for m = -l:l)
 end
 
-eval_psp_energy_correction(psp::NormConservingPsp, n_electrons) =
-    eval_psp_energy_correction(Float64, psp, n_electrons)
-
 # Number of projection vectors per atom
 function count_n_proj(psp::NormConservingPsp)
     psp.lmax < 0 ? 0 : sum(size(psp.h[l + 1], 1) * (2l + 1) for l in 0:psp.lmax)::Int
 end
 function count_n_proj(atoms)
-    sum(count_n_proj(psp)*length(positions) for (psp, positions) in atoms)::Int
+    sum(count_n_proj(psp) * length(positions) for (psp, positions) in atoms)::Int
 end
