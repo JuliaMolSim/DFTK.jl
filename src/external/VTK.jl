e5682b5b4e17ea42cbc5d375e49c4cae2b7ecb4d
DSantra92 <santradibbo@gmail.com>
2021-01-06 01:51:03 +0530

    stashed changes + reviewed changes

--- src/external/VTK.jl
+++ src/external/VTK.jl
@@ -1,49 +1,55 @@
-# Uses WriteVTK.jl to convert scfres structure to VTk file format
 import WriteVTK: vtk_grid, vtk_save
+
+# Uses WriteVTK.jl to convert scfres structure to VTk file format
 
 """
     save_scfres(VTKFileName, scfres)
 
 The function takes in the VTK filename and the scfres structure and stores into a VTK file.
 Grid Values:
-ρ_real -> real values of scfers.ρ
+ρ -> stores values of density in real space
 ψreal_i_j -> Real values of Bloch waves in real space, where i and j are Kpoint and EigenVector indexes respectively
-ρspin -> Real of ρspin are stored if ρspin in present
+ψimag_i_j -> Imaginary values of Bloch waves in real space, where i and j are Kpoint and EigenVector indexes respectively
+ρspin -> Real value of ρspin are stored if ρspin in present
 MetaData:
-Energy and occupation.
+Energy , EigenValues, Fermi Level and occupation.
 """
-function save_scfres(VTKFileName::AbstractString, scfres)
+function save_scfres(filename::AbstractString, scfres::NamedTuple)
     # Initialzing the VTK Grid
     basis = scfres.basis
-    basis_r_co = r_vectors_cart(basis)
-    gridsize = size(basis_r_co)
-    grid = zeros(3, gridsize...)
-    for (idcs, r) in zip(CartesianIndices(gridsize), basis_r_co)
+    grid = zeros(3, basis.fft_size...)
+    for (idcs, r) in zip(CartesianIndices(basis.fft_size), r_vectors_cart(basis))
         grid[:, Tuple(idcs)...] = r
     end
-    vtkfile = vtk_grid(VTKFileName, grid)
+    vtkfile = vtk_grid(filename, grid)
     
     # Storing the Bloch Waves in Real space
     for i in 1:length(basis.kpoints)
-        for j in 1:7
-            vtkfile["ψreal_$(i)_$(j)"] = real.(G_to_r(basis, basis.kpoints[i], scfres.ψ[i][:,j]))
+        for j in 1:size(scfres.ψ[1])[2]
+            vtkfile["ψ_real_$(i)_$(j)"] = real.(G_to_r(basis, basis.kpoints[i], scfres.ψ[i][:,j]))
+            vtkfile["ψ_imag_$(i)_$(j)"] = imag.(G_to_r(basis, basis.kpoints[i], scfres.ψ[i][:,j]))
         end
     end
 
-    # Storing the Real component of density
-    vtkfile["ρ_real"] = scfres.ρ.real
+    # Storing the  density in real space
+    vtkfile["ρ"] = scfres.ρ.real
     
     # Storing ρspin if it is present.
     isnothing(scfres.ρspin)  || (vtkfile["ρspin_real"] = scfres.ρspin.real)
     
     # Storing the energy components
-    for i in keys(scfres.energies)
-        vtkfile["Energy_$i"] = scfres.energies[i]
+    for key in keys(scfres.energies)
+        vtkfile["energy_$key"] = scfres.energies[key]
     end
+    
+    # Storing the Fermi level 
+    vtkfile["fermi_level"] =  scfres.εF
+
+    # Storing the EigenValues as a matrix
+    vtkfile["eigen_values"] = hcat(scfres.eigenvalues...)
 
     # Storing the Occupation as a matrix
-    occupation_mat = hcat(scfres.occupation...)
-    vtkfile["Occupation"] = occupation_mat
+    vtkfile["occupation"] = hcat(scfres.occupation...)
 
-    out = vtk_save(vtkfile)
+    out = vtk_save(vtkfile)[1]
 end
