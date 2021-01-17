964e7058eed2aa5d87bd63192689d9a3aaf6b6a9
Deeptendu Santra <santradibbo@gmail.com>
2021-01-17 12:26:18 +0530

    tests

--- test/save_load_scfres.jl
+++ test/save_load_scfres.jl
@@ -1,5 +1,6 @@
 using DFTK
-
+using WriteVTK
+using JLD2
 include("testcases.jl")
 
 Si = ElementPsp(14, psp=load_psp(silicon.psp))
@@ -10,15 +11,8 @@ Ecut = 7
 basis = PlaneWaveBasis(model, Ecut; kgrid = kgrid)
 scfres = self_consistent_field(basis)
 
-@test_throws ErrorException save_scfres("MyVTKfile.vts", scfres)
-@test_throws ErrorException save_scfres("MyVTKfile.jld", scfres)
 @test_throws ErrorException save_scfres("MyVTKfile.random", scfres)
 @test_throws ErrorException save_scfres("MyVTKfile", scfres)
-
-# Import statements are not all included in front to test for 
-# error statements in dispatch
-using WriteVTK
-using JLD2
 
 mktempdir() do tmpdir
     cd(tmpdir)
