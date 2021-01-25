c46782cd8def6f0881198d7a17c1069f12b0d5d0
Deeptendu Santra <santradibbo@gmail.com>
2021-01-25 08:26:07 +0530

    mpi-tests 1/2

--- test/save_load_scfres.jl
+++ test/save_load_scfres.jl
@@ -3,22 +3,24 @@ using WriteVTK
 using JLD2
 include("testcases.jl")
 
-Si = ElementPsp(14, psp=load_psp(silicon.psp))
-atoms = [Si => silicon.positions]
-model = model_LDA(silicon.lattice, atoms)
-kgrid = [1, 1, 1]
-Ecut = 7
-basis = PlaneWaveBasis(model, Ecut; kgrid=kgrid)
-scfres = self_consistent_field(basis)
+if mpi_nprocs() == 1
+    Si = ElementPsp(14, psp=load_psp(silicon.psp))
+    atoms = [Si => silicon.positions]
+    model = model_LDA(silicon.lattice, atoms)
+    kgrid = [1, 1, 1]
+    Ecut = 7
+    basis = PlaneWaveBasis(model, Ecut; kgrid=kgrid)
+    scfres = self_consistent_field(basis)
 
-@test_throws ErrorException save_scfres("MyVTKfile.random", scfres)
-@test_throws ErrorException save_scfres("MyVTKfile", scfres)
+    @test_throws ErrorException save_scfres("MyVTKfile.random", scfres)
+    @test_throws ErrorException save_scfres("MyVTKfile", scfres)
 
-mktempdir() do tmpdir
-    vtkfile = joinpath(tmpdir, "MyVTKfile.vts")
-    @test save_scfres(vtkfile, scfres) == vtkfile
-    @test isfile(vtkfile) == true
-    jldfile = joinpath(tmpdir, "MyJLDfile.jld2")
-    save_scfres(jldfile, scfres)
-    @test isfile(jldfile) == true
+    mktempdir() do tmpdir
+        vtkfile = joinpath(tmpdir, "MyVTKfile.vts")
+        @test save_scfres(vtkfile, scfres) == vtkfile
+        @test isfile(vtkfile) == true
+        jldfile = joinpath(tmpdir, "MyJLDfile.jld2")
+        save_scfres(jldfile, scfres)
+        @test isfile(jldfile) == true
+    end
 end
