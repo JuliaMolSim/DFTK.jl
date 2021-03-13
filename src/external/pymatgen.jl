3db4c99fe8fa306b1c239364d49854ecd6cda4bb
Michael F. Herbst <info@michael-herbst.com>
2021-03-13 12:46:03 +0100

    Pyimport fixes

--- src/external/pymatgen.jl
+++ src/external/pymatgen.jl
@@ -4,15 +4,14 @@
 function pymatgen_lattice(lattice::AbstractArray)
     # Notice: Pymatgen uses rows as lattice vectors, so we unpeel
     # our lattice column by column. The default unit in pymatgen is Ångström
-    mg = pyimport("pymatgen")
+    Lattice = pyimport("pymatgen.core.lattice").Lattice
     bohr_to_A = 1 / austrip(1u"Å")
-    mg.Lattice([Array(bohr_to_A .* lattice[:, i]) for i in 1:3])
+    Lattice([Array(bohr_to_A .* lattice[:, i]) for i in 1:3])
 end
 pymatgen_lattice(model::Model) = pymatgen_lattice(model.lattice)
 
 
 function pymatgen_structure(model_or_lattice, atoms)
-    mg = pyimport("pymatgen")
     pylattice = pymatgen_lattice(model_or_lattice)
 
     n_species = sum(length(pos) for (spec, pos) in atoms)
@@ -28,17 +27,19 @@ function pymatgen_structure(model_or_lattice, atoms)
     end
     @assert ispec == n_species + 1
 
-    mg.Structure(pylattice, pyspecies, pypositions)
+    Structure = pyimport("pymatgen.core.structure").Structure
+    Structure(pylattice, pyspecies, pypositions)
 end
 pymatgen_structure(model::Model) = pymatgen_structure(model, model.atoms)
 
 
 function load_lattice_pymatgen(T, pyobj::PyObject)
-    mg = pyimport("pymatgen")
+    Structure = pyimport("pymatgen.core.structure").Structure
+    Lattice   = pyimport("pymatgen.core.lattice").Lattice
 
-    if pyisinstance(pyobj, mg.Structure)
+    if pyisinstance(pyobj, Structure)
         load_lattice_pymatgen(T, pyobj.lattice)
-    elseif pyisinstance(pyobj, mg.Lattice)
+    elseif pyisinstance(pyobj, Lattice)
         lattice = Matrix{T}(undef, 3, 3)
         for i in 1:3, j in 1:3
             lattice[i, j] = austrip(get(get(pyobj.matrix, j-1), i-1) * u"Å")
@@ -55,7 +56,7 @@ Load a DFTK-compatible atoms representation from a supported pymatgen object.
 All atoms are using a Coulomb model.
 """
 function load_atoms_pymatgen(T, pyobj::PyObject)
-    @assert pyisinstance(pyobj, pyimport("pymatgen").Structure)
+    @assert pyisinstance(pyobj, pyimport("pymatgen.core.structure").Structure)
     map(unique(pyobj.species)) do spec
         coords = [s.frac_coords for s in pyobj.sites if s.specie == spec]
         ElementCoulomb(spec.number) => coords
