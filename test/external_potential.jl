f4ab38548340284f1144a86b01e66b1e2bbe099d
Michael F. Herbst <info@michael-herbst.com>
2021-04-30 17:06:41 +0200

    Disable on MPI

--- test/external_potential.jl
+++ test/external_potential.jl
@@ -1,6 +1,7 @@
 using DFTK
 using Test
 
+if mpi_nprocs() == 1  # Direct minimisation does not yet support MPI
 @testset "Externel potential from Fourier coefficients" begin
     lattice = [[10 0 0.]; [0 0 0]; [0 0 0]]
 
@@ -18,4 +19,5 @@ using Test
     scfres_dm = direct_minimization(basis, tol=1e-10)
     scfres_scf = self_consistent_field(basis, tol=1e-10)
     @test abs(scfres_scf.energies.total - scfres_dm.energies.total) < 1e-6
+end
 end
