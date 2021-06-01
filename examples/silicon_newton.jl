db42c34abd3fbbb938132d65b845e4329f447f1b
Gaspard Kemlin <gaspard.kemlin@enpc.fr>
2021-06-01 12:22:23 +0200

    change starting point

--- examples/silicon_newton.jl
+++ examples/silicon_newton.jl
@@ -10,25 +10,17 @@ atoms = [Si => [ones(3)/8, -ones(3)/8]]
 
 model = model_LDA(lattice, atoms)
 kgrid = [2, 2, 2]  # k-point grid (Regular Monkhorst-Pack grid)
-Ecut = 5           # kinetic energy cutoff in Hartree
+Ecut = 30           # kinetic energy cutoff in Hartree
 basis = PlaneWaveBasis(model, Ecut; kgrid=kgrid)
 tol = 1e-12
 
 scfres_scf = self_consistent_field(basis, tol=tol,
                                    is_converged=DFTK.ScfConvergenceDensity(tol))
+scfres_test = self_consistent_field(basis, tol=tol, maxiter=5,
+                                   is_converged=DFTK.ScfConvergenceDensity(tol))
 
 ## start no too far from the solution to ensure convergence of newton algorithm
-ψ0 = deepcopy(scfres_scf.ψ)
-for ik = 1:length(ψ0)
-    ψ0k = ψ0[ik]
-    for i in 1:size(ψ0k, 2)
-        ψ0k[:,i] += randn(size(ψ0k[:,i]))*1e-2
-    end
-end
-for ik = 1:length(ψ0)
-    ψ0[ik] = Matrix(qr(ψ0[ik]).Q)
-end
-scfres = newton(basis, ψ0=ψ0, tol=tol, max_iter=20)
+scfres = newton(basis, ψ0=scfres_test.ψ, tol=tol, max_iter=20)
 
 ## plot densities from both algorithms
 rvecs = collect(r_vectors(basis))[:, 1, 1]  # slice along the x axis
