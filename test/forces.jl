1e56a6beba842782bd4f8699f97b6837f04ae853
Michael F. Herbst <info@michael-herbst.com>
2021-06-03 17:48:46 +0200

    Extra forces test for magnetic ground state (#437)

--- test/forces.jl
+++ test/forces.jl
@@ -76,3 +76,37 @@ end
         @test abs(diff_findiff - diff_forces) < tol
     end
 end
+
+
+@testset "Forces on spin-polarised case" begin
+    function oxygen_energy_forces(pos)
+        Ecut = 4
+        O = ElementPsp(o2molecule.atnum, psp=load_psp("hgh/pbe/O-q6.hgh"))
+        magnetic_moments = [O => [1.0, 1.0]]
+        model = model_PBE(diagm([7.0, 7.0, 7.0]), [O => pos],
+                          temperature=0.02, smearing=smearing=Smearing.Gaussian(),
+                          magnetic_moments=magnetic_moments)
+        basis = PlaneWaveBasis(model, Ecut; kgrid=[1, 1, 1])
+
+        scfres = self_consistent_field(basis;
+                                       is_converged=DFTK.ScfConvergenceDensity(1e-7),
+                                       ρ=guess_density(basis, magnetic_moments),
+                                       α=0.7,
+                                      )
+        scfres.energies.total, compute_forces(scfres)
+    end
+
+    pos1 = [[0, 0, 0.1155], [0.01, -2e-3, -0.2]]
+    disp = rand(3)
+    mpi_mean!(disp, MPI.COMM_WORLD)  # must be identical on all processes
+    ε = 1e-6
+    pos2 = [pos1[1] + ε * disp, pos1[2]]
+
+    E1, F1 = oxygen_energy_forces(pos1)
+    E2, _  = oxygen_energy_forces(pos2)
+
+    diff_findiff = -(E2 - E1) / ε
+    diff_forces  = dot(F1[1][1], disp)
+
+    @test abs(diff_findiff - diff_forces) < 1e-4
+end
