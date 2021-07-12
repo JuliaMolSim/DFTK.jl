ac3844c49e2fb44f96c42bbc399ee305cda42971
Antoine Levitt <antoine.levitt@gmail.com>
2021-07-12 16:35:01 +0200

    Avoid mutation in ρ_from_total_and_spin

--- src/densities.jl
+++ src/densities.jl
@@ -138,13 +138,12 @@ total_density(ρ) = dropdims(sum(ρ; dims=4); dims=4)
 end
 
 function ρ_from_total_and_spin(ρtot, ρspin=nothing)
-    n_spin = ρspin === nothing ? 1 : 2
-    ρ = similar(ρtot, size(ρtot)..., n_spin)
-    if n_spin == 1
-        ρ .= ρtot
+    if ρspin === nothing
+         # returns a copy for consistency with the other case
+        copy(reshape(ρtot, (size(ρtot)..., n_spin)))
     else
-        ρ[:, :, :, 1] .= (ρtot .+ ρspin) ./ 2
-        ρ[:, :, :, 2] .= (ρtot .- ρspin) ./ 2
+        # Val to ensure inferability
+        cat((ρtot .+ ρspin) ./ 2,
+            (ρtot .- ρspin) ./ 2; dims=Val(4))
     end
-    ρ
 end
