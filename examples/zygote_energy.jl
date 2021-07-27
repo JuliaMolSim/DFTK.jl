d1226b5294bdcadcd79827242d29cc870a136b98
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2021-07-27 12:30:29 +0200

    upade r_to_G, G_to_r rrule for new normalization

--- examples/zygote_energy.jl
+++ examples/zygote_energy.jl
@@ -9,22 +9,23 @@ lattice = a / 2 * [[0 1 1.];
 Si = ElementPsp(:Si, psp=load_psp(:Si, functional="lda"))
 atoms = [Si => [ones(3)/8, -ones(3)/8]]
 # model = model_DFT(lattice, atoms, [:lda_x, :lda_c_vwn]) # xc not working yet (mutating)
-# terms = [
-#     Kinetic(),
-#     AtomicLocal(),
-#     AtomicNonlocal(),
-#     Ewald(),
-#     PspCorrection(),
-#     Entropy(),
-#     Hartree()
-# ]
-# model = Model(lattice; atoms=atoms, terms=terms, symmetries=false)
-model = model_DFT(lattice, atoms, [], symmetries=false)
+terms = [
+    Kinetic(),
+    AtomicLocal(),
+    # AtomicNonlocal(),
+    # Ewald(),
+    # PspCorrection(),
+    # Entropy(),
+    # Hartree()
+]
+model = Model(lattice; atoms=atoms, terms=terms, symmetries=false)
+# model = model_DFT(lattice, atoms, [], symmetries=false)
 kgrid = [1, 1, 1]
 Ecut = 7
 basis = PlaneWaveBasis(model, Ecut; kgrid=kgrid)
 
-scfres = self_consistent_field(basis, is_converged=DFTK.ScfConvergenceDensity(1e-13))
+# scfres = self_consistent_field(basis, is_converged=DFTK.ScfConvergenceDensity(1e-13))
+scfres = self_consistent_field(basis, is_converged=DFTK.ScfConvergenceDensity(1e6))
 ψ = scfres.ψ
 occupation = scfres.occupation
 
@@ -45,27 +46,29 @@ total_energy(scfres.ρ)
 
 # Approach 2: Try direct access to term energies
 
-function total_energy2(ρ)
+function HF_energy(basis, ψ, occupation, ρ)
+    # TODO ρ = compute_density(basis, ψ, occupation)
     energies = [DFTK.ene_ops(term, ψ, occupation; ρ=ρ).E for term in basis.terms]
     sum(energies)
 end
-total_energy2(scfres.ρ)
-g1 = Zygote.gradient(total_energy2, scfres.ρ)[1] # works
+HF_energy(ρ) = HF_energy(basis, ψ, occupation, ρ) # only for debug purposes, TODO delete
+HF_energy(scfres.ρ)
+g1 = Zygote.gradient(HF_energy, scfres.ρ)[1] # works
 
 # check against finite differences
-g2 = FiniteDiff.finite_difference_gradient(total_energy2, scfres.ρ)
+g2 = FiniteDiff.finite_difference_gradient(HF_energy, scfres.ρ)
 sum(abs, g1 - g2)  # 3.7460628693848023e-7
 
 using BenchmarkTools
-@btime total_energy2(scfres.ρ)  # 215.649 μs (278 allocations: 772.42 KiB)
-@btime Zygote.gradient(total_energy2, scfres.ρ)[1];  # 2.044 ms (3413 allocations: 3.03 MiB)
-@btime FiniteDiff.finite_difference_gradient(total_energy2, scfres.ρ);  # 4.559 s (4463509 allocations: 11.78 GiB)
+@btime HF_energy(scfres.ρ)  # 215.649 μs (278 allocations: 772.42 KiB)
+@btime Zygote.gradient(HF_energy, scfres.ρ)[1];  # 2.044 ms (3413 allocations: 3.03 MiB)
+@btime FiniteDiff.finite_difference_gradient(HF_energy, scfres.ρ);  # 4.559 s (4463509 allocations: 11.78 GiB)
 
 # also try E w.r.t. ψ
-total_energy_psi(ψ) = sum([DFTK.ene_ops(term, ψ, occupation; ρ=scfres.ρ).E for term in basis.terms])
-Zygote.gradient(total_energy_psi, ψ)
-@btime total_energy_psi(ψ);  # 192.225 μs (291 allocations: 776.19 KiB)
-@btime Zygote.gradient(total_energy_psi, ψ);  # 1.823 ms (3464 allocations: 3.11 MiB)
+HF_energy_psi(ψ) = HF_energy(basis, ψ, occupation, scfres.ρ)
+Zygote.gradient(HF_energy_psi, ψ)
+@btime HF_energy_psi(ψ);  # 192.225 μs (291 allocations: 776.19 KiB)
+@btime Zygote.gradient(HF_energy_psi, ψ);  # 1.823 ms (3464 allocations: 3.11 MiB)
 
 
 # E w.r.t. basis
@@ -78,15 +81,40 @@ Zygote.gradient(basis -> DFTK.ene_ops(basis.terms[4], ψ, occupation)[1], basis)
 Zygote.gradient(basis -> DFTK.ene_ops(basis.terms[5], ψ, occupation)[1], basis)
 Zygote.gradient(basis -> DFTK.ene_ops(basis.terms[6], ψ, occupation; ρ=scfres.ρ)[1], basis)
 Zygote.gradient(basis -> DFTK.ene_ops(basis.terms[7], ψ, occupation; ρ=scfres.ρ)[1], basis)
-total_energy_basis(basis) = sum(DFTK.ene_ops(term, ψ, occupation; ρ=scfres.ρ).E for term in basis.terms)
-total_energy_basis(basis) # -4.807121625456233
-Zygote.gradient(total_energy_basis, basis) # seems to work
+HF_energy_basis(basis) = HF_energy(basis, ψ, occupation, scfres.ρ)
+HF_energy_basis(basis) # -4.807121625456233
+g = Zygote.gradient(HF_energy_basis, basis)[1];
+dump(g; maxdepth=2)
 # TODO verify result
+# look at forces
 
+
+# model w.r.t. lattice parameter
+Si = ElementPsp(:Si, psp=load_psp(:Si, functional="lda"))
+function make_model(a)
+    lattice = a / 2 * [[0. 1. 1.];
+                       [1. 0. 1.];
+                       [1. 1. 0.]]
+    atoms = [Si => [ones(3)/8, -ones(3)/8]]
+    # model = model_DFT(lattice, atoms, [], symmetries=false)
+    Model(lattice; atoms=atoms)
+end
+make_model(a)
+Zygote.gradient(a -> make_model(a).recip_cell_volume, a) # (-0.2686157095138732,)
+FiniteDiff.finite_difference_derivative(a -> make_model(a).recip_cell_volume, a) # -0.2686157095506202
+
+function make_basis(model::Model)
+    kgrid = [1, 1, 1]
+    Ecut = 7
+    PlaneWaveBasis(model, Ecut; kgrid=kgrid)
+end
+make_basis(make_model(a)).opFFT.scale # 0.0020540029287345234
+Zygote.gradient(a -> make_basis(make_model(a)).opFFT.scale, a) # (nothing,)
+FiniteDiff.finite_difference_derivative(a -> make_basis(make_model(a)).opFFT.scale, a)# 0.00030029282584396495
 
 # basis w.r.t. lattice parameter
 
-Si = ElementPsp(:Si, psp=load_psp(:Si, functional="lda"))
+
 function make_basis(a)
     lattice = a / 2 * [[0. 1. 1.];
                        [1. 0. 1.];
--- src/workarounds/chainrules.jl
+++ src/workarounds/chainrules.jl
@@ -5,9 +5,9 @@ import AbstractFFTs
 function ChainRulesCore.rrule(::typeof(r_to_G), basis::PlaneWaveBasis, f_real::AbstractArray)
     f_fourier = r_to_G(basis, f_real)
     function r_to_G_pullback(Δf_fourier)
-        ∂f_real = real(basis.opBFFT_unnormalized * Δf_fourier) * (sqrt(basis.model.unit_cell_volume) / length(basis.opFFT_unnormalized))
-        ∂scale = sum(Δf_fourier .* f_fourier)
-        ∂basis = Tangent{typeof(basis)}(;opFFT=Tangent{typeof(basis.opFFT)}(;scale=∂scale))
+        ∂f_real = r_to_G_normalization * real(basis.opBFFT * Δf_fourier)
+        ∂normalization = sum(Δf_fourier .* f_fourier)
+        ∂basis = Tangent{typeof(basis)}(;r_to_G_normalization=∂normalization)
         return NoTangent(), ∂basis, ∂f_real
     end
     return f_fourier, r_to_G_pullback
@@ -17,36 +17,45 @@ function ChainRulesCore.rrule(::typeof(G_to_r), basis::PlaneWaveBasis, f_fourier
     f_real = G_to_r(basis, f_fourier; kwargs...)
     function G_to_r_pullback(Δf_real)
         ∂f_fourier = (basis.opFFT * Δf_real) * (length(basis.opFFT) / (basis.model.unit_cell_volume))
-        ∂scale = sum(Δf_fourier .* f_fourier)
-        ∂basis = Tangent{typeof(basis)}(;opIFFT=Tangent{typeof(basis.opIFFT)}(;scale=∂scale))
+        ∂normalization = sum(Δf_real .* f_real)
+        ∂basis = Tangent{typeof(basis)}(;G_to_r_normalization=∂normalization)
         return NoTangent(), ∂basis, ∂f_fourier
     end
     return f_real, G_to_r_pullback
 end
 
-function ChainRulesCore.rrule(::typeof(*), P::AbstractFFTs.ScaledPlan, x)
-    y = P * x
-    function mul_pullback(Δ)
-      N = prod(size(x)[[P.p.region...]])
-      ∂x = N * (P \ Δ)
-      ∂P = Tangent{typeof(P)}(;scale=sum(Δ .* y))
-      return NoTangent(), ∂P, ∂x 
-    end
-    return y, mul_pullback
-end
+# function ChainRulesCore.rrule(::typeof(*), P::AbstractFFTs.ScaledPlan, x)
+#     y = P * x
+#     function mul_pullback(Δ)
+#       N = prod(size(x)[[P.p.region...]])
+#       ∂x = N * (P \ Δ)
+#       ∂P = Tangent{typeof(P)}(;scale=sum(Δ .* y))
+#       return NoTangent(), ∂P, ∂x 
+#     end
+#     return y, mul_pullback
+# end
 
-# explicit Zygote.adjoint definitions are here bc Zygote.adjoint rules 
-# take precedence over ChainRulesCore.rrule, even if rrule is more specific.
-# Currently, Zygote defines an adjoint for *(P::AbstractFFTs.Plan, xs)
-# that fails for ScaledPlan (which has no field `region`).
-# TODO fix upstream and delete here
-@adjoint function *(P::AbstractFFTs.ScaledPlan, xs)
-    ys = P * xs
-    return ys, function(Δ)
-        N = prod(size(xs)[[P.p.region...]])
-        return ((;scale=sum(Δ .* ys)), N * (P \ Δ))
-    end
-end
+# # explicit Zygote.adjoint definitions are here bc Zygote.adjoint rules 
+# # take precedence over ChainRulesCore.rrule, even if rrule is more specific.
+# # Currently, Zygote defines an adjoint for *(P::AbstractFFTs.Plan, xs)
+# # that fails for ScaledPlan (which has no field `region`).
+# # TODO fix upstream and delete here
+# @adjoint function *(P::AbstractFFTs.ScaledPlan, xs)
+#     @warn "custom adjoint *(P::AbstractFFTs.ScaledPlan, xs)"
+#     ys = P * xs
+#     return ys, function(Δ)
+#         N = prod(size(xs)[[P.p.region...]])
+#         return ((;scale=sum(Δ .* ys)), N * (P \ Δ)) # or dot(Δ, ys) (complex conj) ?
+#     end
+# end
+# @adjoint function \(P::AbstractFFTs.ScaledPlan, xs)
+#     @warn "custom adjoint \\(P::AbstractFFTs.ScaledPlan, xs)"
+#     ys = P \ xs
+#     return ys, function(Δ)
+#         N = prod(size(xs)[[P.p.region...]])
+#         return ((;scale=-sum(Δ .* ys)/P.scale^2), (P * Δ) / N) # TODO check scale part (complex deriv?)
+#     end
+# end
 
 # workaround rrules for mpi: treat as noop
 function ChainRulesCore.rrule(::typeof(mpi_sum), arr, comm)
