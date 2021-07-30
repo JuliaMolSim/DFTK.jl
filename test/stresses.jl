1ccda157becb2ba2f7c923007ab60b75e084e4d9
Jae-Mo Lihm <jaemolim96@gmail.com>
2021-07-30 17:51:46 +0900

    Fix stresses test for MPI

--- test/stresses.jl
+++ test/stresses.jl
@@ -2,6 +2,7 @@ using Test
 using DFTK
 using ForwardDiff
 import FiniteDiff
+using MPI
 include("testcases.jl")
 
 # Hellmann-Feynman stress
@@ -42,7 +43,7 @@ include("testcases.jl")
     stresses = compute_stresses(scfres)
     @test isapprox(stresses, compute_stresses(scfres_nosym), atol=1e-10)
 
-    dir = randn(3, 3)
+    dir = MPI.bcast(randn(3, 3), 0, MPI.COMM_WORLD)
 
     dE_stresses = dot(dir, stresses) * scfres.basis.model.unit_cell_volume
     ref_recompute = FiniteDiff.finite_difference_derivative(0.0) do ε
