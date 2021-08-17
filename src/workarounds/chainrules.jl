521e8105c6511b46a89616c154d58ebf4d739218
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2021-08-17 15:41:07 +0200

    remove unused ScaledPlan Zygote adjoints

--- src/workarounds/chainrules.jl
+++ src/workarounds/chainrules.jl
@@ -51,38 +51,6 @@ function ChainRulesCore.rrule(::typeof(G_to_r), basis::PlaneWaveBasis, kpt::Kpoi
     return f_real, G_to_r_pullback
 end
 
-# function ChainRulesCore.rrule(::typeof(*), P::AbstractFFTs.ScaledPlan, x)
-#     y = P * x
-#     function mul_pullback(Δ)
-#       N = prod(size(x)[[P.p.region...]])
-#       ∂x = N * (P \ Δ)
-#       ∂P = Tangent{typeof(P)}(;scale=sum(Δ .* y))
-#       return NoTangent(), ∂P, ∂x 
-#     end
-#     return y, mul_pullback
-# end
-
-# # explicit Zygote.adjoint definitions are here bc Zygote.adjoint rules 
-# # take precedence over ChainRulesCore.rrule, even if rrule is more specific.
-# # Currently, Zygote defines an adjoint for *(P::AbstractFFTs.Plan, xs)
-# # that fails for ScaledPlan (which has no field `region`).
-# # TODO fix upstream and delete here
-# @adjoint function *(P::AbstractFFTs.ScaledPlan, xs)
-#     @warn "custom adjoint *(P::AbstractFFTs.ScaledPlan, xs)"
-#     ys = P * xs
-#     return ys, function(Δ)
-#         N = prod(size(xs)[[P.p.region...]])
-#         return ((;scale=sum(Δ .* ys)), N * (P \ Δ)) # or dot(Δ, ys) (complex conj) ?
-#     end
-# end
-# @adjoint function \(P::AbstractFFTs.ScaledPlan, xs)
-#     @warn "custom adjoint \\(P::AbstractFFTs.ScaledPlan, xs)"
-#     ys = P \ xs
-#     return ys, function(Δ)
-#         N = prod(size(xs)[[P.p.region...]])
-#         return ((;scale=-sum(Δ .* ys)/P.scale^2), (P * Δ) / N) # TODO check scale part (complex deriv?)
-#     end
-# end
 
 # workaround rrules for mpi: treat as noop
 function ChainRulesCore.rrule(::typeof(mpi_sum), arr, comm)
