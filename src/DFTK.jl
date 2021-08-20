a1c8621b05e87d6eaba50cc49c975292f8fcd35e
LaurentVidal95 <laurent.vida@gmail.com>
2021-08-20 12:11:01 +0200

    First commit on new PR.

--- src/DFTK.jl
+++ src/DFTK.jl
@@ -154,11 +154,13 @@ export load_model
 export load_density
 export load_atoms
 export load_magnetic_moments
+export run_wannier90
 include("external/abinit.jl")
 include("external/load_from_python.jl")
 include("external/load_from_file.jl")
 include("external/ase.jl")
 include("external/pymatgen.jl")
+include("external/wannier90.jl")
 
 export compute_bands
 export high_symmetry_kpath
