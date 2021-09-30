cb6f92631b4da20e1af9b9447e48f8e95b3ab35b
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2021-09-30 12:35:09 +0200

    add test for self-adjointness of apply_Ω,  apply_K

--- test/newton.jl
+++ test/newton.jl
@@ -1,6 +1,7 @@
 using Test
 using DFTK
-import DFTK: compute_projected_gradient, solve_ΩplusK, filled_occupation
+import DFTK: solve_ΩplusK, apply_Ω, apply_K
+import DFTK: filled_occupation, compute_projected_gradient
 
 include("testcases.jl")
 
@@ -12,23 +13,45 @@ include("testcases.jl")
     basis = PlaneWaveBasis(model, Ecut, silicon.kcoords, silicon.ksymops; fft_size=fft_size)
     scfres_start = self_consistent_field(basis, maxiter=1)
 
-    @testset "self-adjointness of solve_ΩplusK" begin
-        ψ = DFTK.select_occupied_orbitals(basis, scfres_start.ψ)
+    ψ = DFTK.select_occupied_orbitals(basis, scfres_start.ψ)
 
-        occupation = scfres_start.occupation
-        filled_occ = filled_occupation(model)
-        n_spin = model.n_spin_components
-        n_bands = div(model.n_electrons, n_spin * filled_occ, RoundUp)
-        @assert n_bands == size(ψ[1], 2)
-        # number of kpoints and occupation
-        Nk = length(basis.kpoints)
-        occupation = [filled_occ * ones(n_bands) for ik = 1:Nk]
+    occupation = scfres_start.occupation
+    filled_occ = filled_occupation(model)
+    n_spin = model.n_spin_components
+    n_bands = div(model.n_electrons, n_spin * filled_occ, RoundUp)
+    @assert n_bands == size(ψ[1], 2)
+    # number of kpoints and occupation
+    Nk = length(basis.kpoints)
+    occupation = [filled_occ * ones(n_bands) for ik = 1:Nk]
 
-        rhs = compute_projected_gradient(basis, ψ, occupation)
-        ϕ = rhs + ψ
+    ρ = compute_density(basis, ψ, occupation)
+
+    rhs = compute_projected_gradient(basis, ψ, occupation)
+    ϕ = rhs + ψ
+
+    @testset "self-adjointness of solve_ΩplusK" begin 
         @test isapprox(
             real(dot(ϕ, solve_ΩplusK(basis, ψ, rhs, occupation))),
             real(dot(solve_ΩplusK(basis, ψ, ϕ, occupation), rhs)),
+            atol=1e-7
+        )
+    end
+
+    @testset "self-adjointness of apply_Ω" begin
+        _, H = energy_hamiltonian(basis, ψ, occupation; ρ=ρ)
+        # Rayleigh-coefficients
+        Λ = [ψk'Hψk for (ψk, Hψk) in zip(ψ, H * ψ)]
+        @test isapprox(
+            real(dot(ϕ, apply_Ω(rhs, ψ, H, Λ))),
+            real(dot(apply_Ω(ϕ, ψ, H, Λ), rhs)),
+            atol=1e-7
+        )
+    end
+
+    @testset "self-adjointness of apply_K" begin
+        @test isapprox(
+            real(dot(ϕ, apply_K(basis, rhs, ψ, ρ, occupation))),
+            real(dot(apply_K(basis, ϕ, ψ, ρ, occupation), rhs)),
             atol=1e-7
         )
     end
