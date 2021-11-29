ea917b1849677e43f98b17e11a2147a3bf28bc73
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2021-11-30 00:15:53 +0100

    set Ecut=20 for debugging

--- examples/zygote_polarizability.jl
+++ examples/zygote_polarizability.jl
@@ -8,7 +8,7 @@ using Zygote
 ## Again we take the example of a Helium atom.
 He = ElementPsp(:He, psp=load_psp("hgh/lda/He-q2"))
 atoms = [He => [[1/2; 1/2; 1/2]]]  # Helium at the center of the box
-function make_basis(ε::T; a=10., Ecut=5) where T
+function make_basis(ε::T; a=10., Ecut=20) where T
     lattice=T(a) * I(3)  # lattice is a cube of ``a`` Bohrs
     # model = model_DFT(lattice, atoms, [:lda_x, :lda_c_vwn];
     #                   extra_terms=[ExternalFromReal(r -> -ε * (r[1] - a/2))],
@@ -48,7 +48,7 @@ polarizability_fd = let
     ε = 0.01
     (compute_dipole(ε) - compute_dipole(0.0)) / ε
 end
-# 1.3625772529105378
+# 0.6740128910348837
 
 Zygote.gradient(compute_dipole, 0.0) # TODO
-# (1.3622621878114305 - 2.5134097875985433e-17im,)
+# (0.673949426051509 - 2.0713011785935874e-17im,)
