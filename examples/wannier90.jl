eccb8a466216962cde23e0711e3082fc9d218859
Michael F. Herbst <info@michael-herbst.com>
2021-12-02 21:15:51 +0100

    Switch wannier example to graphene

--- examples/wannier90.jl
+++ examples/wannier90.jl
@@ -11,21 +11,28 @@
 #     Bugs are likely and we welcome issues in case you find any!
 #
 # This example shows how to obtain the MLWFs corresponding
-# to the first eight bands of silicon. Since the bands 5 to 8 are entangled,
-# 12 bands are first computed to obtain 8 MLWFs by a disantanglement procedure.
+# to the first five bands of graphene. Since the bands 2 to 11 are entangled,
+# 15 bands are first computed to obtain 4 MLWFs by a disantanglement procedure.
 
 using DFTK
+using Unitful
+using UnitfulAtomic
 
-a = 10.26
-lattice = a / 2 * [[0 1 1.];
-                   [1 0 1.];
-                   [1 1 0.]]
-Si = ElementPsp(:Si, psp=load_psp("hgh/pbe/Si-q4"))
-atoms = [ Si => [zeros(3), 0.25*[-1, 3, -1]] ]  # Non-symmetric silicon crystal
-model = model_PBE(lattice, atoms)
-basis = PlaneWaveBasis(model; Ecut=15, kgrid=[4, 4, 4])
+d = 10u"Å"
+a = 2.641u"Å"  # Graphene Lattice constant
+lattice = [a  -a/2    0;
+           0  √3*a/2  0;
+           0     0    d]
 
-scfres = self_consistent_field(basis, tol=1e-12, n_bands=12);
+C = ElementPsp(:C, psp=load_psp("hgh/pbe/c-q4"))
+atoms  = [C => [[0.0, 0.0, 0.0], [1//3, 2//3, 0.0]]]
+model  = model_PBE(lattice, atoms)
+basis  = PlaneWaveBasis(model; Ecut=15, kgrid=[5, 5, 1])
+scfres = self_consistent_field(basis; n_bands=15, tol=1e-8)
+
+# Plot bandstructure of the system
+
+plot_bandstructure(scfres; kline_density=10)
 
 # Now we use the `run_wannier90` routine to generate all files needed by
 # wannier90 and to perform the wannierization procedure.
@@ -35,19 +42,26 @@ scfres = self_consistent_field(basis, tol=1e-12, n_bands=12);
 # "wannier90/wannier.wout", etc.). A different file prefix can be given with the
 # keyword argument `fileprefix` as shown below.
 #
-# We now solve for 8 MLWF using wannier90:
+# We now solve for 4 MLWF using wannier90:
 
 using wannier90_jll  # Needed to make run_wannier90 available
 run_wannier90(scfres;
-              fileprefix="wannier/Si",
-              n_wannier=8,
-              num_print_cycles=50,
-              num_iter=500,
-              dis_win_max=17.185257,
-              dis_froz_max=6.8318033,
-              dis_num_iter=120,
+              fileprefix="wannier/graphene",
+              n_wannier=4,
+              num_print_cycles=25,
+              num_iter=200,
+              #
+              dis_win_max=19.0,
+              dis_froz_max=0.1,
+              dis_num_iter=300,
               dis_mix_ratio=1.0,
-              wannier_plot=true);
+              #
+              wannier_plot=true,
+              wannier_plot_format="cube",
+              wannier_plot_supercell=5,
+              write_xyz=true,
+              translate_home_cell=true,
+             );
 
 # As can be observed standard optional arguments for the disentanglement
 # can be passed directly to `run_wannier90` as keyword arguments.
