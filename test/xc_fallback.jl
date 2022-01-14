984f6c02e3ad67d9da6625300c55ea8be49becee
Antoine Levitt <antoine.levitt@gmail.com>
2022-01-14 11:00:06 +0100

    adapt tests

--- test/xc_fallback.jl
+++ test/xc_fallback.jl
@@ -1,41 +1,24 @@
 using Test
-using DFTK: lda_x!, lda_c_vwn!
 using Libxc
+using DFTK
 
-@testset "Fallback lda_x!" begin
-    func = Libxc.Functional(:lda_x)
+@testset "Fallback LDA" begin
+    for func_name in (:lda_x, :lda_c_vwn)
+        func = Libxc.Functional(func_name)
 
-    # Create reference
-    ρ = abs.(randn(100))
-    Eref = similar(ρ)
-    Vref = similar(ρ)
-    Libxc.evaluate!(func, rho=ρ, zk=Eref, vrho=Vref)
+        # Create reference
+        ρ = abs.(randn(100))
+        Eref = similar(ρ)
+        Vref = similar(ρ)
+        Libxc.evaluate!(func, rho=ρ, zk=Eref, vrho=Vref)
 
-    # Compute in DFTK in elevated precision
-    ρbig = Array{BigFloat}(ρ)
-    E=similar(ρbig)
-    V=similar(ρbig)
-    lda_x!(ρbig, E=E, Vρ=V)
+        # Compute in DFTK in elevated precision
+        ρbig = Array{BigFloat}(ρ)
+        E=similar(ρbig)
+        V=similar(ρbig)
+        Libxc.evaluate!(func, rho=ρbig, zk=E, vrho=V)
 
-    @test E ≈ Eref atol=5e-15
-    @test V ≈ Vref atol=5e-15
-end
-
-@testset "Fallback lda_c_vwn!" begin
-    func = Libxc.Functional(:lda_c_vwn)
-
-    # Create reference
-    ρ = abs.(randn(100))
-    Eref = similar(ρ)
-    Vref = similar(ρ)
-    Libxc.evaluate!(func, rho=ρ, zk=Eref, vrho=Vref)
-
-    # Compute in DFTK in elevated precision
-    ρbig = Array{BigFloat}(ρ)
-    E=similar(ρbig)
-    V=similar(ρbig)
-    lda_c_vwn!(ρbig, E=E, Vρ=V)
-
-    @test E ≈ Eref atol=5e-15
-    @test V ≈ Vref atol=5e-15
+        @test E ≈ Eref atol=5e-15
+        @test V ≈ Vref atol=5e-15
+    end
 end
