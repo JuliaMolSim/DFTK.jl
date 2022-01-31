b4fc07a14fffa6074c4b1c2cabc71b68189daea7
Antoine Levitt <antoine.levitt@gmail.com>
2022-01-31 16:49:47 +0100

    symop group functions

--- src/common/symop.jl
+++ src/common/symop.jl
@@ -12,3 +12,22 @@ end
 
 Base.:(==)(op1::SymOp, op2::SymOp) = op1.S == op2.S && isapprox(op1.τ, op2.τ; atol=1e-8)
 Base.one(::Type{SymOp}) = SymOp(Mat3{Int}(I), Vec3(zeros(3)))
+
+function Base.:*(op1, op2)
+    S = op1.S * op2.S
+    τ = op1.τ + op1.S' \ op2.τ
+    SymOp(S, τ)
+end
+
+import Base.inv
+Base.inv(op) = SymOp(inv(op.S), -op.S'*op.τ)
+
+function check_group(symops::Vector)
+    for s in symops
+        inv(s) ∈ symops || error("check_group: symop $s with inverse $(inv(s)) is not in the group")
+        for s2 in symops
+            (s*s2 ∈ symops && s2*s ∈ symops) || error("check_group: product is not stable")
+        end
+    end
+    symops
+end
