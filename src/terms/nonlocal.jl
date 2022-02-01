006439909a8f20b41da85375d4313936440a9429
towara <towara@stce.rwth-aachen.de>
2022-02-01 19:31:35 +0100

    fix nonlocal

--- src/terms/nonlocal.jl
+++ src/terms/nonlocal.jl
@@ -102,25 +102,8 @@ end
 # The ordering of the projector indices is (A,l,m,i), where A is running over all
 # atoms, l, m are AM quantum numbers and i is running over all projectors for a
 # given l. The matrix is block-diagonal with non-zeros only if A, l and m agree.
+
 function build_projection_coefficients_(basis::PlaneWaveBasis{T}, atoms) where {T}
-    # TODO In the current version the proj_coeffs still has a lot of zeros.
-    #      One could improve this by storing the blocks as a list or in a
-    #      BlockDiagonal data structure
-    n_proj = count_n_proj(atoms)
-    proj_coeffs = zeros(T, n_proj, n_proj)
-
-    count = 0
-    for (psp, positions) in atoms, r in positions
-        n_proj_psp = count_n_proj(psp)
-        proj_coeffs[count+1:count+n_proj_psp, count+1:count+n_proj_psp] = build_projection_coefficients_(psp)
-        count += n_proj_psp
-    end # psp, r
-    @assert count == n_proj
-
-    proj_coeffs
-end
-
-function _build_projection_coefficients_(basis::PlaneWaveBasis{T}, atoms) where {T}
     # TODO In the current version the proj_coeffs still has a lot of zeros.
     #      One could improve this by storing the blocks as a list or in a
     #      BlockDiagonal data structure
@@ -138,20 +121,8 @@ end
 # The ordering of the projector indices is (l,m,i), where l, m are the
 # AM quantum numbers and i is running over all projectors for a given l.
 # The matrix is block-diagonal with non-zeros only if l and m agree.
-function build_projection_coefficients_(psp::NormConservingPsp)
-    n_proj = count_n_proj(psp)
-    proj_coeffs = zeros(n_proj, n_proj)
-    count = 0
-    for l in 0:psp.lmax, m in -l:l
-        n_proj_l = size(psp.h[l + 1], 1)  # Number of i's
-        range = count .+ (1:n_proj_l)
-        proj_coeffs[range, range] = psp.h[l + 1]
-        count += n_proj_l
-    end # l, m
-    proj_coeffs
-end
 
-function _build_projection_coefficients_(psp::NormConservingPsp)
+function build_projection_coefficients_(psp::NormConservingPsp)
     #@warn "AtomicNonlocal build_projection_coefficients_(psp::NormConservingPsp)"
     blocks = [[psp.h[l + 1] for m in -l:l] for l in 0:psp.lmax]
     proj_coeffs = reduce((a,b)->cat(a,b,dims=(1,2)), reduce(vcat,blocks))
@@ -173,24 +144,11 @@ where pihat(q) = ∫_R^3 pi(r) e^{-iqr} dr
 
 We store 1/√Ω pihat(k+G) in proj_vectors.
 """
-function build_projection_coefficients_(psp::NormConservingPsp)
-    n_proj = count_n_proj(psp)
-    proj_coeffs = zeros(n_proj, n_proj)
-    count = 0
-    for l in 0:psp.lmax, m in -l:l
-        n_proj_l = size(psp.h[l + 1], 1)  # Number of i's
-        range = count .+ (1:n_proj_l)
-        proj_coeffs[range, range] = psp.h[l + 1]
-        count += n_proj_l
-    end # l, m
-    proj_coeffs
-end
-
 function _build_projection_vectors_(basis::PlaneWaveBasis{T}, atoms, kpt::Kpoint) where {T}
     unit_cell_volume = basis.model.unit_cell_volume
     n_proj = count_n_proj(atoms)
     n_G    = length(G_vectors(basis, kpt))
-    proj_vectors = zeros(Complex{T}, n_G, n_proj)
+    proj_vectors = zeros(Complex{T}, 0, 0)
 
     # Compute the columns of proj_vectors = 1/√Ω pihat(k+G)
     # Since the pi are translates of each others, pihat(k+G) decouples as
@@ -205,10 +163,9 @@ function _build_projection_vectors_(basis::PlaneWaveBasis{T}, atoms, kpt::Kpoint
         for r in positions
             # k+G in this formula can also be G, this only changes an unimportant phase factor
             structure_factors = map(q -> cis(-2T(π) * dot(q, r)), Gplusk_vectors(basis, kpt))
-            @views for iproj = 1:count_n_proj(psp)
-                proj_vectors[:, offset+iproj] .= (
-                    structure_factors .* form_factors[:, iproj] ./ sqrt(unit_cell_volume)
-                )
+            for iproj = 1:count_n_proj(psp)
+                col = structure_factors .* form_factors[:, iproj] ./ sqrt(unit_cell_volume)
+                proj_vectors = length(proj_vectors)>0 ? hcat(proj_vectors,col) : col
             end
             offset += count_n_proj(psp)
         end
@@ -238,9 +195,9 @@ function build_projection_vectors_(basis::PlaneWaveBasis{T}, atoms, kpt::Kpoint)
             form_factors = build_form_factors(psp, Gplusk_vectors_cart(basis, kpt))
             # Combine with structure factors
             # k+G in this formula can also be G, this only changes an unimportant phase factor
-            cols = [structure_factors(r) .* form_factors[:, iproj] ./ sqrt(unit_cell_volume) for iproj = 1:count_n_proj(psp), r in positions]
+            cols = [[structure_factors(r) .* form_factors[:, iproj] ./ sqrt(unit_cell_volume) for r in positions] for iproj = 1:count_n_proj(psp)]
             
-            return reduce(hcat, cols)
+            return reduce(hcat, reduce(vcat, cols))
         end
         proj_vectors = reduce(hcat, [build_columns(psp, positions) for (psp, positions) in atoms])
     end
@@ -266,7 +223,30 @@ function build_form_factors(psp, qs)
         form_factors_local
     end
     # for avoid nested for loop for Zygote compat
-    form_factors = reduce(hcat, reduce(vcat, [[build_factor(l,m) for m in -l:l] for l in 0:psp.lmax])) 
+    form_factors = reduce(vcat, [[build_factor(l,m) for m in -l:l] for l in 0:psp.lmax]) # build
+    form_factors = reduce(hcat, form_factors) # concat horizontally
 
+    form_factors
+end
+
+function _build_form_factors(psp, qs)
+    qnorms = norm.(qs)
+    T = real(eltype(qnorms))
+    # Compute position-independent form factors
+    #form_factors = zeros(Complex{T}, length(qs), count_n_proj(psp))
+    form_factors = zeros(Complex{T}, 0, 0)
+    count = 1
+    for l in 0:psp.lmax, m in -l:l
+        prefac_lm = im^l .* ylm_real.(l, m, qs)
+        n_proj_l = size(psp.h[l + 1], 1)
+
+        for iproj in 1:n_proj_l
+            radial_il = eval_psp_projector_fourier.(psp, iproj, l, qnorms)
+            form_factor_i = prefac_lm .* radial_il
+            form_factors = length(form_factors) > 0 ? hcat(form_factors, form_factor_i) : form_factor_i
+            count += 1
+        end
+    end
+    @assert count == count_n_proj(psp) + 1
     form_factors
 end
