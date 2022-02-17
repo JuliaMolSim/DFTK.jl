d2feee34090b5609cc270dbeabbd224e099b65a3
Antoine Levitt <antoine.levitt@gmail.com>
2022-02-17 21:33:14 +0100

    Simplify the erfc cutoff in ewald (#597)
    Co-authored-by: Michael F. Herbst <info@michael-herbst.com>

--- src/terms/ewald.jl
+++ src/terms/ewald.jl
@@ -89,23 +89,10 @@ function energy_ewald(lattice, recip_lattice, charges, positions; η=nothing, fo
         forces_recip = copy(forces)
     end
 
-    #
-    # Numerical cutoffs
-    #
-    # The largest argument to the exp(-x) function to obtain a numerically
-    # meaningful contribution. The +5 is for safety.
-    max_exponent = -log(eps(T)) + 5
-
-    # The largest argument to the erfc function for various precisions.
-    # To get an idea:
-    #   erfc(5) ≈ 1e-12,  erfc(8) ≈ 1e-29,  erfc(10) ≈ 2e-45,  erfc(14) ≈ 3e-87
-    max_erfc_arg = 100
-    try
-        max_erfc_arg = Dict(Float32 => 5, Float64 => 8, BigFloat => 14)[T]
-    catch KeyError
-        # Fallback for not yet implemented cutoffs
-        max_erfc_arg = something(findfirst(arg -> 100 * erfc(arg) < eps(T), 1:100), 100)
-    end
+    # Numerical cutoffs to obtain meaningful contributions. These are very conservative.
+    # The largest argument to the exp(-x) function
+    max_exp_arg = -log(eps(T)) + 5  # add some wiggle room
+    max_erfc_arg = sqrt(max_exp_arg)  # erfc(x) ~= exp(-x^2)/(sqrt(π)x) for large x
 
     #
     # Reciprocal space sum
@@ -134,7 +121,7 @@ function energy_ewald(lattice, recip_lattice, charges, positions; η=nothing, fo
             # Check if the Gaussian exponent is small enough
             # for this term to contribute to the reciprocal sum
             exponent = Gsq / 4η^2
-            if exponent > max_exponent
+            if exponent > max_exp_arg
                 continue
             end
 
