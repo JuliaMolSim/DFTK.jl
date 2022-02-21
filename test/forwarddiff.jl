2a699a38c9a79eac3d31b7ca7d4d09ed86aa764e
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2022-02-21 10:55:00 +0100

    add basic ForwardDiff.gradient tests

--- test/forwarddiff.jl
+++ test/forwarddiff.jl
@@ -4,9 +4,10 @@ using Test
 include("testcases.jl")
 
 @testset "Force derivatives using finite differences" begin
-    function compute_force(ε::T) where T
+    function compute_force(ε1, ε2)
+        T = promote_type(typeof(ε1), typeof(ε2))
         Si  = ElementPsp(silicon.atnum, psp=load_psp(silicon.psp))
-        pos = [[1.01, 1.02, 1.03] / 8, -ones(3) / 8 + ε * [1., 0, 0]]
+        pos = [[1.01, 1.02, 1.03] / 8, -ones(3) / 8 + ε1 * [1., 0, 0] + ε2 * [0, 1., 0]]
         atoms = [Si => pos]
         # TODO symmetries = true gives issues for now ... debug after refactoring done.
         model = model_DFT(Matrix{T}(silicon.lattice), atoms, [:lda_x, :lda_c_pw], symmetries=false)
@@ -17,9 +18,25 @@ include("testcases.jl")
         compute_forces_cart(scfres)[1]
     end
 
-    derivative_fd = let ε = 1e-5
-        (compute_force(ε) - compute_force(0.0)) / ε
+    F = compute_force(0.0)
+    derivative_ε1_fd = let ε1 = 1e-5
+        (compute_force(ε1, 0.0) - F) / ε1
     end
-    derivative = ForwardDiff.derivative(compute_force, 0.0)
-    @test maximum(maximum, derivative - derivative_fd) < 1e-4
+    derivative_ε1 = ForwardDiff.derivative(ε1 -> compute_force(ε1, 0.0), 0.0)
+    @test norm(derivative_ε1 - derivative_ε1_fd) < 1e-4
+
+    derivative_ε2_fd = let ε2 = 1e-5
+        (compute_force(0.0, ε2) - F) / ε2
+    end
+    derivative_ε2 = ForwardDiff.derivative(ε2 -> compute_force(0.0, ε2), 0.0)
+    @test norm(derivative_ε2 - derivative_ε2_fd) < 1e-4
+
+    @testset "Multiple partials" begin
+        grad = ForwardDiff.gradient(v -> compute_force(v...)[1][1], [0.0, 0.0])
+        @test abs(grad[1] - derivative_ε1[1][1]) < 1e-4
+        @test abs(grad[2] - derivative_ε2[1][1]) < 1e-4
+
+        # TODO
+        # jac = ForwardDiff.jacobian(v -> compute_force(v...), [0.0, 0.0])
+    end
 end
