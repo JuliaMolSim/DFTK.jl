0bf7131527546f7f0793239a141663c14815b8df
Étienne Polack <polack+github@hollved.net>
2022-02-21 16:34:50 +0100

    Using pseudo-inverse for one- and two-dimensional cells

--- src/postprocess/forces.jl
+++ src/postprocess/forces.jl
@@ -28,7 +28,7 @@ which has the same structure as the `atoms` object passed to the underlying [`Mo
 function compute_forces_cart(basis::PlaneWaveBasis, ψ, occ; kwargs...)
     lattice = basis.model.lattice
     forces = compute_forces(basis::PlaneWaveBasis, ψ, occ; kwargs...)
-    [[lattice \ f for f in forces_for_element] for forces_for_element in forces]
+    [[pinv(lattice)*f for f in forces_for_element] for forces_for_element in forces]
 end
 
 function compute_forces(scfres)
