6ab4fdbf6d131f0a774544e608c314c5856b54b1
Michael F. Herbst <info@michael-herbst.com>
2022-04-02 17:01:50 +0200

    Script for debugging

---
+++ examples/zygote-debug.jl
@@ -0,0 +1,54 @@
+using DFTK
+using Zygote
+setup_threading(n_blas=1)
+
+# Specify structure for silicon lattice
+a = 10.26
+lattice = a / 2 * [[0 1 1.];
+                   [1 0 1.];
+                   [1 1 0.]]
+Si = ElementPsp(:Si, psp=load_psp("hgh/lda/Si-q4"))
+atoms     = [Si,        Si        ]
+positions = [ones(3)/8, -ones(3)/8]# + 0.1rand(3)]
+
+terms = [
+    Kinetic(),
+    AtomicLocal(),
+    AtomicNonlocal(),
+    Ewald(),
+    PspCorrection(),
+    Hartree(),
+]
+model = Model(lattice, atoms, positions; terms, symmetries=false)
+basis = PlaneWaveBasis(model; Ecut=15, kgrid=(1, 1, 1), kshift=(0, 0, 0))
+
+# Test primal
+let terms = [Kinetic(), AtomicLocal(), Ewald(), PspCorrection(), Hartree(),]
+    model = Model(lattice, atoms, positions; terms, symmetries=false, temperature=1e-3)
+    basis = PlaneWaveBasis(model; Ecut=15, kgrid=(1, 1, 1), kshift=(0, 0, 0))
+    energy = self_consistent_field(basis; tol=1e-14).energies.total
+    reference = -19.629878507271652  # From master
+    diff = abs(energy - reference)
+    if diff > 1e-8
+        @error reference energy diff
+    end
+end
+let terms = terms
+    model = Model(lattice, atoms, positions; terms, symmetries=false, temperature=1e-3)
+    basis = PlaneWaveBasis(model; Ecut=15, kgrid=(1, 1, 1), kshift=(0, 0, 0))
+    energy = self_consistent_field(basis; tol=1e-14).energies.total
+    reference = -4.821586293957623  # From master
+    diff = abs(energy - reference)
+    if diff > 1e-8
+        @error reference energy diff
+    end
+end
+
+function forces_from_basis(basis)
+    is_converged = DFTK.ScfConvergenceDensity(1e-8)
+    scfres = self_consistent_field(basis; is_converged)
+    # compute_forces(scfres)[1][1]
+    scfres.energies.total
+end
+
+Zygote.gradient(forces_from_basis, basis)
