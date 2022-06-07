c1b69e89294e1c2f92e927b2e5069003aa939814
Étienne Polack <polack@hollved.net>
2022-06-07 15:11:19 +0200

    Revert "Cartesian comment"
    This reverts commit b9f1432cb51ead3a641ee8ad1f1319bd314fb9d9.

--- src/terms/local.jl
+++ src/terms/local.jl
@@ -95,7 +95,7 @@ end
     ρ_fourier = r_to_G(basis, total_density(ρ))
 
     # energy = sum of form_factor(G) * struct_factor(G) * rho(G)
-    # where struct_factor(G) = e^{-i G·r}, where r and *G* are in Cartesian coordinates
+    # where struct_factor(G) = e^{-i G·r}
     forces = [zero(Vec3{T}) for _ in 1:length(model.positions)]
     for group in model.atom_groups
         element = model.atoms[first(group)]
