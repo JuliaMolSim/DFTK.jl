24c83feb47efc8d2a22f4d5b2d37fd39b917c0fa
towara <towara@stce.rwth-aachen.de>
2022-06-30 23:15:59 +0200

    rename polarizability example

--- examples/zygote_polarizability.jl
+++ examples/zygote_polarizability.jl
@@ -1,31 +1,52 @@
-# # Polarizability using Zygote
+# # Polarizability using automatic differentiation
+#
+# For a more classical approach and more details about computing polarizabilities,
+# see [Polarizability by linear response](@ref).
 
 using DFTK
 using LinearAlgebra
+using DFTK: FallbackFunctional, LibxcFunctional
 using Zygote
+using ChainRulesCore
 
 ## Construct PlaneWaveBasis given a particular electric field strength
 ## Again we take the example of a Helium atom.
-He = ElementPsp(:He, psp=load_psp("hgh/lda/He-q2"))
-atoms = [He]  # Helium at the center of the box
-positions = [[1/2; 1/2; 1/2]]
-function make_basis(ε::T; a=10., Ecut=5) where T  # too small Ecut, only for efficient debugging
-    lattice=T(a) * Mat3(I(3))  # lattice is a cube of ``a`` Bohrs
-    # model = model_DFT(lattice, atoms, [:lda_x, :lda_c_vwn];
-    #                   extra_terms=[ExternalFromReal(r -> -ε * (r[1] - a/2))],
-    #                   symmetries=false)
+#function make_basis(ε::T; a=10., Ecut=30) where T
+function make_basis(ε::T; a=10., Ecut=5) where T
+    lattice=T(a) * I(3)  # lattice is a cube of ``a`` Bohrs
+    ## Helium at the center of the box
+    #atoms     = [ElementPsp(:He, psp=load_psp("hgh/lda/He-q2"))]
+    #positions = [[1/2, 1/2, 1/2]]
 
-    # TODO: make missing terms differentiable
+    #model = model_DFT(lattice, atoms, positions, [:lda_x, :lda_c_vwn];
+    #                  extra_terms=[ExternalFromReal(r -> -ε * (r[1] - a/2))],
+    #                  symmetries=false)
+
+    model = model_DFT(lattice, atoms, positions, [FallbackFunctional{:lda}(:lda_x), FallbackFunctional{:lda}(:lda_c_vwn)];
+                      extra_terms=[ExternalFromReal(r -> -ε * (r[1] - a/2))],
+                      symmetries=false)
+
+    PlaneWaveBasis(model; Ecut, kgrid=[1, 1, 1])  # No k-point sampling on isolated system
+end
+
+function make_basis_model(ε::T; a=10., Ecut=5) where T
+    lattice=T(a) * I(3)  # lattice is a cube of ``a`` Bohrs
+    He = nothing
+    ChainRulesCore.@ignore_derivatives begin
+        He = ElementPsp(:He, psp=load_psp("hgh/lda/He-q2"))
+    end
+    atoms = [He]
+    positions = [[1/2; 1/2; 1/2]] # Helium at the center of the box
+
     terms = [
         Kinetic(),
         AtomicLocal(),
-        # AtomicNonlocal(),  # more tricky, but eventually needed for reasonable physics
+        AtomicNonlocal(),
         Ewald(),
-        PspCorrection(),   # eventually interesting (psp parameters)
-        Entropy(),         # TODO check numerics with higher temperature and scf higher n_bands kwarg
+        PspCorrection(),
         Hartree(),
         ExternalFromReal(r -> -ε * (r[1] - a/2)),
-        # XC
+        Xc([FallbackFunctional{:lda}(:lda_x), FallbackFunctional{:lda}(:lda_c_vwn)])
     ]
     model = Model(lattice, atoms, positions; terms, symmetries=false)
     PlaneWaveBasis(model; Ecut, kgrid=[1, 1, 1])  # No k-point sampling on isolated system
@@ -33,7 +54,7 @@ end
 
 ## dipole moment of a given density (assuming the current geometry)
 function dipole(basis, ρ)
-    # @assert isdiag(basis.model.lattice)
+    @assert isdiag(basis.model.lattice)
     a  = basis.model.lattice[1, 1]
     rr = [a * (r[1] - 1/2) for r in r_vectors(basis)]
     sum(rr .* ρ) * basis.dvol
@@ -41,30 +62,20 @@ end
 
 ## Function to compute the dipole for a given field strength
 function compute_dipole(ε; tol=1e-8, kwargs...)
-    scfres = self_consistent_field(make_basis(ε; kwargs...), tol=tol)
+    scfres = self_consistent_field(make_basis_model(ε; kwargs...), tol=tol)
+    #scfres = self_consistent_field(make_basis(ε; kwargs...), tol=tol)
     dipole(scfres.basis, scfres.ρ)
-end
+end;
 
+compute_dipole(0)
 
-f = compute_dipole(0.0)
+g = Zygote.gradient(compute_dipole, 0);
 
 # With this in place we can compute the polarizability from finite differences
 # (just like in the previous example):
-polarizability_fd = let
-    ε = 0.001
-    (compute_dipole(ε) - f) / ε
+fd = let
+    ε = 0.01
+    (compute_dipole(ε) - compute_dipole(0.0)) / ε
 end
-# 8.08068504649102    # 0.5 seconds (121.70 k allocations: 180.000 MiB)
 
-g = Zygote.gradient(compute_dipole, 0.0)
-# 8.084399146013764,
-# incl. compile time: 229 seconds
-# second call:          4 seconds (7.84 M allocations: 733.864 MiB, 25.31% gc time)
-
-
-println("f: ", f, " fd: ", polarizability_fd, " AD: ", g)
-
-# using Profile, PProf
-# Profile.clear()
-# @profile Zygote.gradient(compute_dipole, 0.0)
-# pprof()
+println("FD:", fd, "G:", g)
