3af80feb15e45dbc3574136f2d93e7df294ed0bf
Antoine Levitt <antoine.levitt@gmail.com>
2022-07-12 10:31:13 +0200

    review

--- examples/graphene.jl
+++ examples/graphene.jl
@@ -6,41 +6,40 @@ using DFTK
 using Unitful
 using UnitfulAtomic
 
-# Define the convergence parameters (these should be increased in production)
-L = 20 # height of the simulation box
+## Define the convergence parameters (these should be increased in production)
+L = 20  # height of the simulation box
 kgrid = [6, 6, 1]
 Ecut = 15
 temperature = 1e-3
 
-# Define the geometry and pseudopotential
-a = 4.66 # lattice constant
+## Define the geometry and pseudopotential
+a = 4.66  # lattice constant
 a1 = a*[1/2,-sqrt(3)/2, 0]
 a2 = a*[1/2, sqrt(3)/2, 0]
 a3 = L*[0  , 0        , 1]
 lattice = [a1 a2 a3]
-C1 = [1/3,-1/3,0.0] # in reduced coordinates
+C1 = [1/3,-1/3,0.0]  # in reduced coordinates
 C2 = -C1
 positions = [C1, C2]
 C = ElementPsp(:C, psp=load_psp("hgh/pbe/c-q4"))
 atoms = [C, C]
 
-# Run SCF
+## Run SCF
 model = model_PBE(lattice, atoms, positions; temperature)
 basis = PlaneWaveBasis(model; Ecut, kgrid)
 scfres = self_consistent_field(basis)
 
-# Choose the points of the band diagram, in reduced coordinates (in the (b1,b2) basis)
+## Choose the points of the band diagram, in reduced coordinates (in the (b1,b2) basis)
 Γ = [0, 0, 0]
-K1 = [ 1, 1, 0]/3
-K2 = [-1, 2, 0]/3
+K = [1, 1, 0]/3
 M = (K1 + K2)/2
-kpath_coords = [Γ, K1, M, Γ]
+kpath_coords = [Γ, K, M, Γ]
 kpath_names = ["Γ", "K", "M", "Γ"]
 
-# Build the path
+## Build the path
 kline_density = 20
 function build_path(k1, k2)
-    target_Δk = 1/kline_density # the actual Δk is |k2-k1|/npt
+    target_Δk = 1/kline_density  # the actual Δk is |k2-k1|/npt
     npt = ceil(Int, norm(model.recip_lattice * (k2-k1)) / target_Δk)
     [k1 + t * (k2-k1) for t in range(0, 1, length=npt)]
 end
@@ -50,6 +49,6 @@ for i = 1:length(kpath_coords)-1
 end
 klabels = Dict(kpath_names[i] => kpath_coords[i] for i=1:length(kpath_coords))
 
-# Plot the bands
+## Plot the bands
 band_data = compute_bands(basis, kcoords; scfres.ρ)
-display(DFTK.plot_band_data(band_data; scfres.εF, klabels, units=u"hartree"))
+DFTK.plot_band_data(band_data; scfres.εF, klabels)
