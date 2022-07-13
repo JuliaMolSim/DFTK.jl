e232c9e43c978ca84dc6d969013bdaa08d38b7c3
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2022-07-13 22:34:04 +0200

    rm old example code

--- examples/zygote_debug.jl
+++ examples/zygote_debug.jl
@@ -24,43 +24,6 @@ model = Model(lattice, atoms, positions; terms, symmetries=false)
 Ecut = 15
 basis = PlaneWaveBasis(model; Ecut, kgrid=(1, 1, 1), kshift=(0, 0, 0))
 
-
-# TODO
-scfres = self_consistent_field(basis; tol=1e-14)
-using ChainRulesCore
-config = Zygote.ZygoteRuleConfig()
-(; E, H), energy_hamiltonian_pullback =
-               rrule_via_ad(config, energy_hamiltonian, basis, scfres.ψ, scfres.occupation, scfres.ρ)
-∂energies = Tangent{Energies{Float64}}(energies = ChainRulesCore.Tangent{Pair{String, Float64}, NamedTuple{(:first, :second), Tuple{ChainRulesCore.ZeroTangent, Float64}}}[Tangent{Pair{String, Float64}}(first = ChainRulesCore.ZeroTangent(), second = 1.0), Tangent{Pair{String, Float64}}(first = ChainRulesCore.ZeroTangent(), second = 1.0), Tangent{Pair{String, Float64}}(first = ChainRulesCore.ZeroTangent(), second = 1.0), Tangent{Pair{String, Float64}}(first = ChainRulesCore.ZeroTangent(), second = 1.0), Tangent{Pair{String, Float64}}(first = ChainRulesCore.ZeroTangent(), second = 1.0), Tangent{Pair{String, Float64}}(first = ChainRulesCore.ZeroTangent(), second = 1.0)],)
-∂H = ZeroTangent()
-energy_hamiltonian_pullback((; E=∂energies, H=∂H))
-
-# Test primal
-let terms = [Kinetic(), AtomicLocal(), Ewald(), PspCorrection(), Hartree(),]
-    model = Model(lattice, atoms, positions; terms, symmetries=false, temperature=1e-3)
-    basis = PlaneWaveBasis(model; Ecut, kgrid=(1, 1, 1), kshift=(0, 0, 0))
-    # energy = self_consistent_field(basis; tol=1e-14).energies.total
-    scfres = self_consistent_field(basis; tol=1e-14)
-    energy = sum(values(scfres.energies))
-    reference = -19.629878507271652  # From master
-    diff = abs(energy - reference)
-    if diff > 2e-8
-        @error reference energy diff
-    end
-end
-let terms = terms
-    model = Model(lattice, atoms, positions; terms, symmetries=false, temperature=1e-3)
-    basis = PlaneWaveBasis(model; Ecut, kgrid=(1, 1, 1), kshift=(0, 0, 0))
-    # energy = self_consistent_field(basis; tol=1e-14).energies.total
-    scfres = self_consistent_field(basis; tol=1e-14)
-    energy = sum(values(scfres.energies))
-    reference = -4.821586293957623  # From master
-    diff = abs(energy - reference)
-    if diff > 2e-8
-        @error reference energy diff
-    end
-end
-
 function energy_from_basis(basis)
     is_converged = DFTK.ScfConvergenceDensity(1e-8)
     scfres = self_consistent_field(basis; is_converged)
@@ -84,6 +47,8 @@ function eigenvalues_from_basis(basis)
     sum(sum, scfres.eigenvalues)
 end
 Zygote.gradient(eigenvalues_from_basis, basis)
+
+# Comparison to FiniteDiff
 
 function eigenvalues_from_lattice(lattice)
     model = Model(lattice, atoms, positions; terms, symmetries=false)
