b6e99e9b0919192672c4ea6d572b96b0224e654b
Antoine Levitt <antoine.levitt@gmail.com>
2022-07-18 16:53:38 +0200

    formatting

--- examples/papers/cazalis.jl
+++ examples/papers/cazalis.jl
@@ -10,7 +10,7 @@ struct Hartree2D end
 struct Term2DHartree <: DFTK.TermNonlinear end
 (t::Hartree2D)(basis) = Term2DHartree()
 function DFTK.ene_ops(term::Term2DHartree, basis::PlaneWaveBasis{T},
-                 ψ, occ; ρ, kwargs...) where {T}
+                      ψ, occ; ρ, kwargs...) where {T}
     ## 2D Fourier transform of 3D Coulomb interaction 1/|x|
     poisson_green_coeffs = 2T(π) ./ [norm(G) for G in G_vectors_cart(basis)]
     poisson_green_coeffs[1] = 0  # DC component
@@ -23,7 +23,7 @@ function DFTK.ene_ops(term::Term2DHartree, basis::PlaneWaveBasis{T},
     (E=E, ops=ops)
 end
 
-## define el-nucl interaction
+## define electron-nuclei interaction
 struct Element2DCoulomb <: DFTK.Element end
 function DFTK.local_potential_fourier(el::Element2DCoulomb, q::Real)
     ## = ∫ V(r) exp(-ix⋅q) dx
@@ -35,14 +35,14 @@ function DFTK.local_potential_fourier(el::Element2DCoulomb, q::Real)
 end
 
 L = 1.0
-kgrid = [10, 10, 1] # increase this for production
-Ecut = 500 # increase this for production
+kgrid = [10, 10, 1]  # increase this for production
+Ecut = 500  # increase this for production
 height = 0
 
 ## Define the geometry and pseudopotential
-a1 = L*[1/2, -sqrt(3)/2, 0]
-a2 = L*[1/2,  sqrt(3)/2, 0]
-a3 = height*[0, 0, 1]
+a1 = L * [1/2, -sqrt(3)/2, 0]
+a2 = L * [1/2,  sqrt(3)/2, 0]
+a3 = height * [0, 0, 1]
 lattice = [a1 a2 a3]
 C1 = [1/3, -1/3, 0.0]  # in reduced coordinates
 C2 = -C1
@@ -52,10 +52,7 @@ c = Element2DCoulomb()
 atoms = [c, c]
 
 model = Model(lattice, atoms, positions; temperature=1e-4, smearing=Smearing.Gaussian(),
-              terms=[Kinetic(),
-                     Hartree2D(),
-                     AtomicLocal(),
-                     ],
+              terms=[Kinetic(), Hartree2D(), AtomicLocal()],
               n_electrons=2)
 basis = PlaneWaveBasis(model; Ecut, kgrid)
 
@@ -63,12 +60,12 @@ basis = PlaneWaveBasis(model; Ecut, kgrid)
 scfres = self_consistent_field(basis, tol=1e-10)
 
 ## Choose the points of the band diagram, in reduced coordinates (in the (b1,b2) basis)
-Γ = [0, 0, 0]
-K = [ 1, 1, 0]/3
-Kp= [-1, 2, 0]/3
-M = (K + Kp)/2
+Γ  = [0, 0, 0]
+K  = [ 1, 1, 0]/3
+Kp = [-1, 2, 0]/3
+M  = (K + Kp)/2
 kpath_coords = [Γ, K, M, Γ]
-kpath_names = ["Γ", "K", "M", "Γ"]
+kpath_names  = ["Γ", "K", "M", "Γ"]
 
 ## Build the path
 kline_density = 20
