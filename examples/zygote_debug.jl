ae2d0c2dd7d185985a949d81435330a73e7a8f96
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2022-07-21 17:26:52 +0200

    add debug prints

--- examples/zygote_debug.jl
+++ examples/zygote_debug.jl
@@ -51,16 +51,35 @@ Zygote.gradient(eigenvalues_from_basis, basis)
 # Comparison to FiniteDiff
 
 function basis_from_lattice(lattice)
+    terms = [
+        Kinetic(),
+        AtomicLocal(),
+        AtomicNonlocal(),
+        Ewald(),
+        PspCorrection(),
+        Hartree(),
+    ]
     model = Model(lattice, atoms, positions; terms, symmetries=false)
     PlaneWaveBasis(model; Ecut, kgrid=(1, 1, 1), kshift=(0, 0, 0))
 end
 
+energy_from_lattice(lattice) = energy_from_basis(basis_from_lattice(lattice))
+e = energy_from_lattice(lattice)
+e_zg, (grad_e_zg,) = Zygote.withgradient(energy_from_lattice, lattice)
+grad_e_fd = FiniteDiff.finite_difference_gradient(energy_from_lattice, lattice)
+println("energy error ", abs(e - e_zg))
+println("energy w.r.t. lattice error ", abs.(grad_e_fd - grad_e_zg))
+
 forces_from_lattice(lattice) = forces_from_basis(basis_from_lattice(lattice)) 
-forces_from_lattice(lattice)
-Zygote.gradient(forces_from_lattice, lattice) # TODO debug values
-FiniteDiff.finite_difference_gradient(forces_from_lattice, lattice)
+f = forces_from_lattice(lattice)
+f_zg, (grad_f_zg,) = Zygote.withgradient(forces_from_lattice, lattice) # TODO debug values
+grad_f_fd = FiniteDiff.finite_difference_gradient(forces_from_lattice, lattice)
+println("force error ", abs(f - f_zg))
+println("force w.r.t. lattice error ", abs.(grad_f_fd - grad_f_zg))
 
 eigenvalues_from_lattice(lattice) = eigenvalues_from_basis(basis_from_lattice(lattice))
-eigenvalues_from_lattice(lattice)
-Zygote.gradient(eigenvalues_from_lattice, lattice) # TODO debug values
-FiniteDiff.finite_difference_gradient(eigenvalues_from_lattice, lattice)
+ev = eigenvalues_from_lattice(lattice)
+ev_zg, (grad_ev_zg,) = Zygote.withgradient(eigenvalues_from_lattice, lattice) # TODO debug values
+grad_ev_fd = FiniteDiff.finite_difference_gradient(eigenvalues_from_lattice, lattice)
+println("eigenvalues error ", abs(ev - ev_zg))
+println("eigenvalues w.r.t. lattice error ", abs.(grad_ev_fd - grad_ev_zg))
