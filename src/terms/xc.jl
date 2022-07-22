74d10312efe20f00293fe98336dfec161c496a93
Étienne Polack <87805034+epolack@users.noreply.github.com>
2022-07-22 13:52:51 +0000

    Update xc.jl

--- src/terms/xc.jl
+++ src/terms/xc.jl
@@ -93,7 +93,8 @@ end
             mG² = [-sum(abs2, G) for G in G_vectors_cart(basis)]
             Vl  = reshape(terms.Vl, n_spin, basis.fft_size...)
             Vl_fourier = r_to_G(basis, Vl[s, :, :, :])
-            # not sure why, see https://github.com/JuliaMolSim/DFTK.jl/issues/694
+            # not sure why `assume_real` is needed here
+            # see https://github.com/JuliaMolSim/DFTK.jl/issues/694
             potential[:, :, :, s] .+= G_to_r(basis, mG² .* Vl_fourier;
                                              assume_real=Val(true))  # ΔVl
         end
