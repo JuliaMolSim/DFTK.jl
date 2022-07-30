ee6ae138ef028eddccdaac41e2dbda0833bbaae1
Gabriel Birnbaum <gbbinbum@gmail.com>
2022-07-30 13:01:29 -0700

    added fixed convergence study

--- examples/convergence_study.jl
+++ examples/convergence_study.jl
@@ -39,11 +39,10 @@ function compute_convergence(Ecut = 500, nkpts::Integer = 8, tol = 1e-2)
 end
 
 # And since nkpts only takes discrete values of `k` in `[k,k,k]`, we define a custom function to define its convergence for a given desired tolerance `tol`
-get_converged_k(energies; tol) = findfirst(abs(i - j) < tol for (i, j) in zip(energies, energies[1:end]))
-
+get_converged_k(energies; tol) = findfirst([abs(i - j) < tol for (i, j) in zip(energies, energies[2:end])])
 
 # Now we can use `get_converged_k()` and `compute_convergence()` to get the converging nkpts
-energies_w_diff_k = [compute_kgrid_convergence(k) for k in 1:7] # change to 10 to get full convergence like in plot
+energies_w_diff_k = [compute_convergence(Ecut, k) for k in 1:7] # change to 10 to get full convergence like in plot
 k_conv = get_converged_k(energies_w_diff_k; tol) # 5.0
 
 # and plot it:
@@ -63,7 +62,7 @@ E_cut_conv = Optim.minimizer(opt_res)
 E_cut_conv_H = auconvert(u"eV", E_cut_conv)
 
 # and finally plot Ecut:
-Ecut_convergence = [compute_Ecut_convergence(i) for i in 7:14] # change to 22 to get full convergence like in plot
+Ecut_convergence = [compute_convergence(ecut_i, nkpts) for ecut_i in 7:14] # change to 22 to get full convergence like in plot
 plot(Ecut_convergence, dpi = 300, lw = 3, xlabel = "Energy cutoff (Ha)", ylabel = "Energy/Atom", label = "Ecut")
 scatter!(Ecut_convergence, label = "Data points")
 
