a5845b25dc961e0fbb6c66851a574cb5c552cef5
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2022-08-07 12:59:25 +0200

    Pin down Zygote splatting kwargs bug

--- examples/zygote_debug.jl
+++ examples/zygote_debug.jl
@@ -110,7 +110,7 @@ function hellmann_feynman_energy(scfres, lattice)
 end
 # ForwardDiff.derivative(a -> hellmann_feynman_energy(scfres, make_lattice(a)), a)
 FiniteDiff.finite_difference_derivative(a -> hellmann_feynman_energy(scfres, make_lattice(a)), a)
-Zygote.gradient(a -> hellmann_feynman_energy(scfres, make_lattice(a)), a) # wrong
+Zygote.gradient(a -> hellmann_feynman_energy(scfres, make_lattice(a)), a) # correct
 
 f2(a) = sum(density_from_lattice(make_lattice(a)))
 FiniteDiff.finite_difference_derivative(f2, a)
@@ -118,20 +118,19 @@ ForwardDiff.derivative(f2, a)
 Zygote.gradient(f2, a) # correct
 
 f3(ρ) = energy_hamiltonian(basis, scfres.ψ, scfres.occupation; ρ).E.total
-# f3(ρ) = sum(values(energy_hamiltonian(basis, scfres.ψ, scfres.occupation; ρ).E))
 f3(ρ; k) = energy_hamiltonian(basis, scfres.ψ, scfres.occupation; ρ).E[k]
 for k in keys(scfres.energies)
     println(k)
     f4(h) = f3(scfres.ρ + h*scfres.ρ; k)
     g1 = FiniteDiff.finite_difference_derivative(f4, 0.0)
     g2 = ForwardDiff.derivative(f4, 0.0)
-    g3 = only(Zygote.gradient(f4, 0.0)) # wrong
+    g3 = only(Zygote.gradient(f4, 0.0)) # correct
     println(" FinDiff: ", g1, " ForwardDiff: ", g2, " Zygote: ", g3)
 end
 # Kinetic
 #  FinDiff: 0.0 ForwardDiff: 0.0 Zygote: 0.0
 # AtomicLocal
-#  FinDiff: -2.1687148211557647 ForwardDiff: -2.1687148211339324 Zygote: -4.337429642267865
+#  FinDiff: -2.1687148211557647 ForwardDiff: -2.1687148211339324 Zygote: -2.1687148211339324
 # AtomicNonlocal
 #  FinDiff: 0.0 ForwardDiff: 0.0 Zygote: 0.0
 # Ewald
@@ -140,6 +139,17 @@ end
 #  FinDiff: 0.0 ForwardDiff: 0.0 Zygote: 0.0
 # Hartree
 #  FinDiff: 1.2590922290980915 ForwardDiff: 1.259092229051408 Zygote: 1.259092229051417
-#
-# looks like the incorrectness lies in AtomicLocal! and it is indeed a factor of 2
-# TODO
+
+# Suspicion: This might be something with double-counting of kwargs
+f6(x; kwargs...) = energy_hamiltonian(basis, scfres.ψ, scfres.occupation; kwargs..., ρ=kwargs[:ρ]).E.total
+f7(ρ) = f6(0.0; ρ=ρ)
+FiniteDiff.finite_difference_derivative(h -> f7(scfres.ρ + h*scfres.ρ), 0.0)
+Zygote.gradient(h -> f7(scfres.ρ + h*scfres.ρ), 0.0) # incorrect by factor 2
+
+f1(; kwargs...) = kwargs[:x]
+f2(; kwargs...) = f1(; kwargs..., x=kwargs[:x])
+f3(x) = f2(; x)
+FiniteDiff.finite_difference_derivative(f3, 0.0) # 1.0
+ForwardDiff.derivative(f3, 0.0) # 1.0
+Zygote.gradient(f3, 0.0) # (2.0,) aha!
+# --> opened a new issue https://github.com/FluxML/Zygote.jl/issues/1284
