e12f35be94c0ef904c2b918db7454351884d75ac
Guillaume Vigne <guillaume.vigne007@gmail.com>
2022-09-01 20:06:31 +0200

    Prettier way to overload eigen for CuArrays

--- src/workarounds/gpu_arrays.jl
+++ src/workarounds/gpu_arrays.jl
@@ -1,16 +1,17 @@
 #TODO: remove this when it is implemented in GPUArrays
-import LinearAlgebra.dot, LinearAlgebra.eigen, LinearAlgebra.RealHermSymComplexHerm
+import LinearAlgebra.dot, LinearAlgebra.eigen
 using LinearAlgebra
 using GPUArrays
 using CUDA
 
 LinearAlgebra.dot(x::AbstractGPUArray, D::Diagonal,y::AbstractGPUArray) = x'*(D*y)
 
-function LinearAlgebra.eigen(A::RealHermSymComplexHerm{T,AT}) where {T,AT <: CuArray}
-    if eltype(A) <: Complex
-        vals, vects = CUDA.CUSOLVER.heevd!('V','U', A.data)
-    else
-        vals, vects = CUDA.CUSOLVER.syevd!('V','U',A.data)
-    end
+function LinearAlgebra.eigen(A::Hermitian{T,AT}) where {T <: Complex,AT <: CuArray}
+    vals, vects = CUDA.CUSOLVER.heevd!('V','U', A.data)
+    (vectors = vects, values = vals)
+end
+
+function LinearAlgebra.eigen(A::Hermitian{T,AT}) where {T <: Real,AT <: CuArray}
+    vals, vects = CUDA.CUSOLVER.syevd!('V','U', A.data)
     (vectors = vects, values = vals)
 end
