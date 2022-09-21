9fec8f45c68d0c7cd4b1a4496da23711465573ea
Gaspard Kemlin <gaspard.kemlin@enpc.fr>
2022-09-21 10:49:36 +0200

    typo

--- examples/cohen_bergstresser.jl
+++ examples/cohen_bergstresser.jl
@@ -23,7 +23,6 @@ basis = PlaneWaveBasis(model, Ecut=10.0, kgrid=(2, 2, 2));
 # We diagonalise at the Gamma point to find a Fermi level ...
 ham = Hamiltonian(basis)
 eigres = diagonalize_all_kblocks(DFTK.lobpcg_hyper, ham, 6)
-println(eigres.λ)
 εF = DFTK.compute_occupation(basis, eigres.λ; occupation_threshold=0).εF
 
 # ... and compute and plot 8 bands:
