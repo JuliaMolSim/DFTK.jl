d28391e6ffb366168fc43ea61e5cac9d04b252e3
GVigne <72138169+GVigne@users.noreply.github.com>
2022-09-28 15:05:37 +0200

    Make LOBPCG GPU-compatible (#711)
    Co-authored-by: Michael F. Herbst <info@michael-herbst.com>

---
+++ src/workarounds/gpu_arrays.jl
@@ -0,0 +1,19 @@
+# TODO: remove this when it is implemented in GPUArrays and CUDA
+import LinearAlgebra.dot, LinearAlgebra.eigen
+using LinearAlgebra
+using GPUArraysCore
+using CUDA
+
+# https://github.com/JuliaGPU/CUDA.jl/issues/1565
+LinearAlgebra.dot(x::AbstractGPUArray, D::Diagonal,y::AbstractGPUArray) = x'*(D*y)
+
+# https://github.com/JuliaGPU/CUDA.jl/issues/1572
+function LinearAlgebra.eigen(A::Hermitian{T,AT}) where {T <: Complex,AT <: CuArray}
+    vals, vects = CUDA.CUSOLVER.heevd!('V','U', A.data)
+    (vectors = vects, values = vals)
+end
+
+function LinearAlgebra.eigen(A::Hermitian{T,AT}) where {T <: Real,AT <: CuArray}
+    vals, vects = CUDA.CUSOLVER.syevd!('V','U', A.data)
+    (vectors = vects, values = vals)
+end
