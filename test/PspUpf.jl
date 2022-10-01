f862f81ccaa5a13bb1d1c04006ac62395a8633f3
Austin Zadoks <austin.zadoks@pm.me>
2022-10-01 12:02:45 +0200

    Add UPF-HGH consistency tests

--- test/PspUpf.jl
+++ test/PspUpf.jl
@@ -5,7 +5,7 @@ using SpecialFunctions: besselj
 using QuadGK
 
 @testset "Check reading 'Si.nc.z_4.oncvpsp3.dojo.v4-std'" begin
-    psp = load_psp("upf/pbe/Si.nc.z_4.oncvpsp3.dojo.v4-std.upf")
+    psp = load_psp("upf/pbe/si-nc-sr-standard-04.upf")
 
     @test psp.lmax == 2
     @test psp.Zion == 4
@@ -16,6 +16,75 @@ using QuadGK
     end
 
     @test psp.projs[1][1][1] ≈ -5.6328824383E-09
+end
+
+@testset "Real potentials are consistent with HGH" begin
+    for pspfile in ["si-q4", "b-q3", "he-q2", "fe-q16", "mo-q14", "tl-q3", "tl-q13"]
+        upf = load_psp("upf/pbe/$(pspfile).upf")
+        hgh = load_psp("hgh/pbe/$(pspfile).hgh")
+        for r in [upf.rgrid[1], upf.rgrid[end]]
+            reference_hgh = eval_psp_local_real(hgh, r)
+            @test reference_hgh ≈ eval_psp_local_real(upf, r) rtol=1e-8 atol=1e-8
+        end
+    end
+end
+
+@testset "Fourier potentials are consistent with HGH" begin
+    for pspfile in ["si-q4", "b-q3", "he-q2", "fe-q16", "mo-q14", "tl-q3", "tl-q13"]
+        upf = load_psp("upf/pbe/$(pspfile).upf")
+        hgh = load_psp("hgh/pbe/$(pspfile).hgh")
+        for q in [0.01, 0.1, 0.2, 0.5, 1., 2., 5., 10.]
+            reference_hgh = eval_psp_local_fourier(hgh, q)
+            @test reference_hgh ≈ eval_psp_local_fourier(upf, q) rtol=1e-5 atol=1e-5
+        end
+    end
+end
+
+@testset "Projectors are consistent with HGH in real and Fourier space" begin
+    for pspfile in ["si-q4", "b-q3", "he-q2", "fe-q16", "mo-q14", "tl-q3", "tl-q13"]
+        upf = load_psp("upf/pbe/$(pspfile).upf")
+        hgh = load_psp("hgh/pbe/$(pspfile).hgh")
+        for (l, i) in [(0, 1), (0, 2), (0, 3), (1, 1), (1, 2), (1, 3),
+                       (2, 1), (2, 2), (3, 1)]
+            l > upf.lmax - 1 && continue  # Overshooting available AM
+            i > length(upf.projs[l+1]) && continue  # Overshooting available projectors at AM
+            ircut = length(upf.projs[l+1][i])
+            for q in [0.01, 0.1, 0.2, 0.5, 1., 2., 5., 10.]
+                reference_hgh = eval_psp_projector_fourier(hgh, i, l, q)
+                @test reference_hgh ≈ eval_psp_projector_fourier(upf, i, l, q) / 2 atol=1e-7 rtol=1e-7
+            end
+            for r in [upf.rgrid[1], upf.rgrid[ircut]]
+                reference_hgh = eval_psp_projector_real(hgh, i, l, r)
+                @test reference_hgh ≈ eval_psp_projector_real(upf, i, l, r) / 2 atol=1e-7 rtol=1e-7
+            end
+        end
+    end
+end
+
+@testset "Energy correction is consistent with HGH" begin
+    for pspfile in ["si-q4", "b-q3", "he-q2", "fe-q16", "mo-q14", "tl-q3", "tl-q13"]
+        upf = load_psp("upf/pbe/$(pspfile).upf")
+        hgh = load_psp("hgh/pbe/$(pspfile).hgh")
+        n_electrons = 3
+        reference_hgh = eval_psp_energy_correction(hgh, n_electrons)
+        @test reference_hgh ≈ eval_psp_energy_correction(upf, n_electrons) rtol=1e-5 atol=1e-5
+    end
+end
+
+@testset "Potentials are consistent in real and Fourier space" begin
+    reg_param = 0.25  # divergent integral, needs regularization
+    function integrand(psp_local_real, q, r)
+        4π * psp_local_real(r) * exp(-reg_param * r) * sin(q*r) / q * r
+    end
+    for pspfile in ["si-nc-sr-standard-04.upf"]
+        psp = load_psp("upf/pbe/" * pspfile)
+        psp_local_real = linear_interpolation((psp.rgrid,), psp.vloc ./ 2)
+
+        for q in [0.01, 0.1, 0.2, 0.5, 1., 2., 5., 10.]
+            reference = quadgk(r -> integrand(psp_local_real, q, r), psp.rgrid[1], psp.rgrid[end])[1]
+            @test reference ≈ eval_psp_local_fourier(psp, q) rtol=1. atol=1.
+        end
+    end
 end
 
 @testset "Projectors are consistent in real and Fourier space" begin
@@ -31,31 +100,46 @@ end
         4π * x * eval_psp_projector_real(psp, i, l, x) * j(l, q*x)
     end
 
-    for pspfile in ["Si.nc.z_4.oncvpsp3.dojo.v4-std.upf"]
+    for pspfile in ["si-nc-sr-standard-04.upf"]
         psp = load_psp("upf/pbe/" * pspfile)
         for (l, i) in [(0, 1), (0, 2), (0, 3), (1, 1), (1, 2), (1, 3),
                        (2, 1), (2, 2), (3, 1)]
             l > psp.lmax - 1 && continue  # Overshooting available AM
             i > length(psp.projs[l+1]) && continue  # Overshooting available projectors at AM
+            ircut = length(psp.projs[l+1][i])
             for q in [0.01, 0.1, 0.2, 0.5, 1, 2, 5, 10]
-                reference = quadgk(r -> integrand(psp, i, l, q, r), psp.rgrid[1], psp.rgrid[end])[1]
-                @test reference ≈ eval_psp_projector_fourier(psp, i, l, q) atol=5e-15 rtol=1e-8
+                reference = quadgk(r -> integrand(psp, i, l, q, r), psp.rgrid[1], psp.rgrid[ircut])[1]
+                @test reference ≈ eval_psp_projector_fourier(psp, i, l, q) atol=1e-8 rtol=1e-8
             end
         end
     end
 end
 
-@testset "Potentials are consistent in real and Fourier space" begin
-    reg_param = 0.001  # divergent integral, needs regularization
-    function integrand(psp, q, r)
-        4π * eval_psp_local_real(psp, r) * exp(-reg_param * r) * sin(q*r) / q * r
+@testset "PSP energy correction is consistent with real-space potential" begin
+    reg_param = 1e-6  # divergent integral, needs regularization
+    q_small = 1e-8    # We are interested in q→0 term
+    function integrand(psp, n_electrons, r)
+        # Difference of potential of point-like atom (what is assumed in Ewald)
+        # versus actual structure of the pseudo potential
+        coulomb = -psp.Zion / r
+        diff = n_electrons * (eval_psp_local_real(psp, r) - coulomb)
+        4π * diff * exp(-reg_param * r) * sin(q_small*r) / q_small * r
     end
 
-    for pspfile in ["Si.nc.z_4.oncvpsp3.dojo.v4-std.upf"]
+    n_electrons = 3
+    for pspfile in ["si-nc-sr-standard-04.upf"]
         psp = load_psp("upf/pbe/" * pspfile)
-        for q in [0.01, 0.2, 1, 1.3]
-            reference = quadgk(r -> integrand(psp, q, r), psp.rgrid[1], psp.rgrid[end])[1]
-            @test reference ≈ eval_psp_local_fourier(psp, q) rtol=.1 atol = .1
-        end
+        reference = quadgk(r -> integrand(psp, n_electrons, r), psp.rgrid[1], psp.rgrid[end])[1]
+        @test reference ≈ eval_psp_energy_correction(psp, n_electrons) atol=1e-1
+    end
+end
+
+@testset "PSP energy correction is consistent with fourier-space potential" begin
+    q_small = 1e-3    # We are interested in q→0 term
+    for pspfile in ["si-nc-sr-standard-04.upf"]
+        psp = load_psp("upf/pbe/" * pspfile)
+        coulomb = -4π * (psp.Zion * 2) / q_small^2
+        reference = eval_psp_local_fourier(psp, q_small) - coulomb
+        @test reference ≈ eval_psp_energy_correction(psp, 1) atol=1e-3
     end
 end
