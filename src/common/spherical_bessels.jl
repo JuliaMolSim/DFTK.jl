f967b652c795caa86294ab402e9155d93a78069e
Austin Zadoks <austin.zadoks@pm.me>
2022-10-17 10:15:27 +0200

    Return statements

--- src/common/spherical_bessels.jl
+++ src/common/spherical_bessels.jl
@@ -7,11 +7,11 @@ Returns the spherical Bessel function of the first kind j_l(x). Consistent with
 https://en.wikipedia.org/wiki/Bessel_function#Spherical_Bessel_functions.
 """
 @fastmath function sphericalbesselj(l::Integer, x::T)::T where {T}
-    l == 0 && sin(x) / x
-    l == 1 && (sin(x) - cos(x) * x) / x^2
-    l == 2 && (sin(x) * (3 - x^2) + cos(x) * (-3x)) / x^3
-    l == 3 && (sin(x) * (15 - 6x^2) + cos(x) * (x^3 - 15x)) / x^4
-    l == 4 && (sin(x) * (105 - 45x^2 + x^4) + cos(x) * (10x^3 - 105x)) / x^5
-    l == 5 && (sin(x) * (945 - 420x^2 + 15x^4) + cos(x) * (-945x + 105x^3 - x^5)) / x^6
-    l > 5 && SpecialFunctions.sphericalbesselj(l, x)
+    l == 0 && return sin(x) / x
+    l == 1 && return (sin(x) - cos(x) * x) / x^2
+    l == 2 && return (sin(x) * (3 - x^2) + cos(x) * (-3x)) / x^3
+    l == 3 && return (sin(x) * (15 - 6x^2) + cos(x) * (x^3 - 15x)) / x^4
+    l == 4 && return (sin(x) * (105 - 45x^2 + x^4) + cos(x) * (10x^3 - 105x)) / x^5
+    l == 5 && return (sin(x) * (945 - 420x^2 + 15x^4) + cos(x) * (-945x + 105x^3 - x^5)) / x^6
+    l > 5 && return SpecialFunctions.sphericalbesselj(l, x)
 end
