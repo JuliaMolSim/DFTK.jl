b68e6b44093453caa00cc75075ec37dc1d53e28c
LaurentVidal95 <laurent.vida@gmail.com>
2022-10-19 15:45:38 +0200

    Added reference to the arXiv preprint (Cancès, Hassan, Vidal) for energy cutoff smearing.

--- src/terms/kinetic.jl
+++ src/terms/kinetic.jl
@@ -52,7 +52,7 @@ struct BlowupIdentity end
 
 
 """
-Blow-up function as proposed in [REF paper Cancès, Hassan, Vidal to be submitted]
+Blow-up function as proposed in [https://arxiv.org/abs/2210.00442]
 The blow-up order of the function is fixed to ensure C^2 regularity of the energies bands
 away from crossings and Lipschitz continuity at crossings.
 """
