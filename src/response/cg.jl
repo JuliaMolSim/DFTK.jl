2d4bd0da0455896d0894a3e9a3d4079e29645fe8
Michael F. Herbst <info@michael-herbst.com>
2022-10-19 13:52:33 +0200

    No printing means no callback :)

--- src/response/cg.jl
+++ src/response/cg.jl
@@ -1,7 +1,7 @@
 using LinearMaps
 
-function default_cg_callback(info; verbose=false)
-    verbose && @printf("%3d\t%1.2e\n", info.n_iter, info.residual_norm)
+function default_cg_print(info)
+    @printf("%3d\t%1.2e\n", info.n_iter, info.residual_norm)
 end
 
 """
@@ -9,7 +9,7 @@ Implementation of the conjugate gradient method which allows for preconditioning
 and projection operations along iterations.
 """
 function cg!(x::AbstractVector{T}, A::LinearMap{T}, b::AbstractVector{T};
-             precon=I, proj=identity, callback=default_cg_callback,
+             precon=I, proj=identity, callback=identity,
              tol=1e-10, maxiter=100, miniter=1) where {T}
 
     # initialisation
@@ -45,7 +45,7 @@ function cg!(x::AbstractVector{T}, A::LinearMap{T}, b::AbstractVector{T};
         residual_norm = norm(r)
 
         # output
-        info = (; A, b, n_iter, x, r, residual_norm)
+        info = (; A, b, n_iter, x, r, residual_norm, converged, stage=:iterate)
         callback(info)
         if (n_iter > miniter) && residual_norm <= tol
             converged = true
@@ -58,8 +58,9 @@ function cg!(x::AbstractVector{T}, A::LinearMap{T}, b::AbstractVector{T};
         β = γ / γ_prev
         p .= proj(c .+ β .* p)
     end
-
-    (; x, converged, tol, residual_norm, iterations=n_iter, maxiter)
+    info = (; x, converged, tol, residual_norm, iterations=n_iter, maxiter, stage=:finalize)
+    callback(info)
+    info
 end
 cg!(x::AbstractVector, A::AbstractMatrix, b::AbstractVector; kwargs...) = cg!(x, LinearMap(A), b; kwargs...)
 cg(A::LinearMap, b::AbstractVector; kwargs...) = cg!(zero(b), A, b; kwargs...)
