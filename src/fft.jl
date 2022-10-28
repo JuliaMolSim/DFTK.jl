a339a4dfd5a599b4b8b4060b942cb8ee0e07d421
Guillaume Vigne <guillaume.vigne007@gmail.com>
2022-10-28 12:08:58 +0200

    Fix CPU performance bug when using FFTs

--- src/fft.jl
+++ src/fft.jl
@@ -266,8 +266,7 @@ _fftw_flags(::Type{Float64}) = FFTW.MEASURE
 Plan a FFT of type `T` and size `fft_size`, spending some time on finding an
 optimal algorithm. (Inplace, out-of-place) x (forward, backward) FFT plans are returned.
 """
-# Removed the flags as CUDA's plan_fft doesn't need flags. If this is a performance issue, we should
-# check array_type's type then call either FFTW.plan_fft(tmp, flags = ...) or CUDA.plan_fft(tmp)
+# Default fallback, which will be used when doing GPU computations.
 function build_fft_plans(array_type::AbstractArray{T}, fft_size) where {T<:Union{Float32,Float64}}
     tmp = similar(array_type, Complex{T}, fft_size...)
     ipFFT = AbstractFFTs.plan_fft!(tmp)
@@ -276,6 +275,14 @@ function build_fft_plans(array_type::AbstractArray{T}, fft_size) where {T<:Union
     ipFFT, opFFT, inv(ipFFT).p, inv(opFFT).p
 end
 
+# Specific CPU version, using flags to be a bit faster.
+function build_fft_plans(array_type::Array{T}, fft_size) where {T<:Union{Float32,Float64}}
+    tmp = Array{Complex{T}}(undef, fft_size...)
+    ipFFT = FFTW.plan_fft!(tmp, flags=_fftw_flags(T))
+    opFFT = FFTW.plan_fft(tmp, flags=_fftw_flags(T))
+    # backward by inverting and stripping off normalizations
+    ipFFT, opFFT, inv(ipFFT).p, inv(opFFT).p
+end
 
 # TODO Some grid sizes are broken in the generic FFT implementation
 # in FourierTransforms, for more details see workarounds/fft_generic.jl
