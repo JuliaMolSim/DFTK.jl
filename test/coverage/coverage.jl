bbc61964e48ec5590ea86f4806422c630754abbe
Michael F. Herbst <info@michael-herbst.com>
2022-12-12 12:26:03 +0100

    Enable coverage

--- test/coverage/coverage.jl
+++ test/coverage/coverage.jl
@@ -2,8 +2,7 @@ using Coverage
 
 cd(joinpath(@__DIR__, "..", "..")) do
     processed = process_folder()
-    # TODO Does not yet work
-    # Codecov.submit_local(processed)
+    Codecov.submit_local(processed)
     covered_lines, total_lines = get_summary(processed)
     percentage = covered_lines / total_lines * 100
     println("($(percentage)%) covered")
