19e817eebdf54dc528cd9161a9ce6baacc9d94ed
Michael F. Herbst <info@michael-herbst.com>
2022-12-19 11:39:20 +0100

    Fixes

--- examples/silicon.jl
+++ examples/silicon.jl
@@ -11,8 +11,6 @@ Si = ElementPsp(:Si, psp=load_psp("hgh/lda/Si-q4"))
 atoms     = [Si, Si]
 positions = [ones(3)/8, -ones(3)/8]
 
-# model = model_LDA(lattice, atoms, positions, temperature=1e-2, smearing=Smearing.MethfesselPaxton(2))
-model = model_LDA(lattice, atoms, positions, temperature=1e-2u"Ry", smearing=Smearing.MarzariVanderbilt())
+model = model_LDA(lattice, atoms, positions)
 basis = PlaneWaveBasis(model; Ecut=15, kgrid=[4, 4, 4])
-scfres = self_consistent_field(basis, tol=1e-8, mixing=LdosMixing())
-nothing
+scfres = self_consistent_field(basis, tol=1e-8)
