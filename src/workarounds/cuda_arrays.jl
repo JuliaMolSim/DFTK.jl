4edf85630121b47266664fef111f9f8bbc3a823e
Michael F. Herbst <info@michael-herbst.com>
2023-01-27 20:56:56 +0100

    Remove error exception

--- src/workarounds/cuda_arrays.jl
+++ src/workarounds/cuda_arrays.jl
@@ -16,10 +16,6 @@ synchronize_device(::GPU{<:CUDA.CuArray}) = CUDA.synchronize()
 for fun in (:potential_terms, :kernel_terms)
     @eval function DftFunctionals.$fun(fun::DispatchFunctional,
                                        ρ::CUDA.CuMatrix{Float64}, args...)
-        # TODO Remove this
-        if !Libxc.has_cuda()
-            error("CUDA not available on $(Libxc.Libxc_GPU_jll.host_platform)")
-        end
         @assert Libxc.has_cuda()
         $fun(fun.inner, ρ, args...)
     end
