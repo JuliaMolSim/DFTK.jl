6f95ca47797a7e076d46a842a38eb7eb54a3ec91
Michael F. Herbst <info@michael-herbst.com>
2023-01-27 20:22:24 +0100

    Temporary

--- src/workarounds/cuda_arrays.jl
+++ src/workarounds/cuda_arrays.jl
@@ -16,7 +16,11 @@ synchronize_device(::GPU{<:CUDA.CuArray}) = CUDA.synchronize()
 for fun in (:potential_terms, :kernel_terms)
     @eval function DftFunctionals.$fun(fun::DispatchFunctional,
                                        ρ::CUDA.CuMatrix{Float64}, args...)
-        @assert DFTK.Libxc.has_cuda()
+        # TODO Remove this
+        if !Libxc.has_cuda()
+            error("CUDA not available on $(Libxc.Libxc_GPU_jll.host_platform)")
+        end
+        @assert Libxc.has_cuda()
         $fun(fun.inner, ρ, args...)
     end
 end
