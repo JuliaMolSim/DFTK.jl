8d823d3c7a1a6bac25035b01805532056207d3d1
Michael F. Herbst <info@michael-herbst.com>
2023-03-31 07:25:18 +0200

    PBE test

--- test/silicon_pbe.jl
+++ test/silicon_pbe.jl
@@ -44,6 +44,10 @@ end
     run_silicon_pbe(Float64, Ecut=7, test_tol=0.03, n_ignored=0, grid_size=17)
 end
 
+@testset "Silicon PBE (small, Float32)" begin
+    run_silicon_pbe(Float32, Ecut=7, test_tol=0.03, n_ignored=1, grid_size=17, scf_tol=1e-4)
+end
+
 if !isdefined(Main, :FAST_TESTS) || !FAST_TESTS
     @testset "Silicon PBE (large, Float64)" begin
         run_silicon_pbe(Float64, Ecut=25, test_tol=1e-5, n_ignored=0,
