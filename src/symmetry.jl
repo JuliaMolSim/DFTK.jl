96de186bfda02b88d53607434599bf3ff6dae96d
Étienne Polack <polack@hollved.net>
2023-05-04 10:13:44 +0200

    comments: longer length and math indent

--- src/symmetry.jl
+++ src/symmetry.jl
@@ -1,33 +1,30 @@
 # This file contains functions to handle the symetries.
 # The type SymOp is defined in Symop.jl
 
-# A symmetry (W, w) (or (S, τ)) induces a symmetry in the Brillouin zone
-# that the Hamiltonian at S k is unitary equivalent to that at k, which we exploit to
-# reduce computations. The relationship is
-# S = W'
-# τ = -W^-1 w
-# (valid both in reduced and cartesian coordinates). In our notation
-# the rotation matrix W and translation w are such that, for each atom of
-# type A at position a, W a + w is also an atom of type A.
+# A symmetry (W, w) (or (S, τ)) induces a symmetry in the Brillouin zone that the
+# Hamiltonian at S k is unitary equivalent to that at k, which we exploit to reduce
+# computations. The relationship is
+#   S = W'
+#   τ = -W^-1 w
+# (valid both in reduced and cartesian coordinates). In our notation the rotation matrix
+# W and translation w are such that, for each atom of type A at position a, W a + w is also
+# an atom of type A.
 
-# The full (reducible) Brillouin zone is implicitly represented by
-# a set of (irreducible) kpoints (see explanation in docs). Each
-# irreducible k-point k comes with a list of symmetry operations
-# (S, τ) (containing at least the trivial operation (I, 0)), where S
-# is a unitary matrix (/!\ in cartesian but not in reduced coordinates)
-# and τ a translation vector. The k-point is then used to represent
-# implicitly the information at all the kpoints Sk. The relationship
-# between the Hamiltonians is
-# H_{Sk} = U H_k U*, with
-# (Uu)(x) = u(W x + w)
+# The full (reducible) Brillouin zone is implicitly represented by a set of (irreducible)
+# kpoints (see explanation in docs). Each irreducible k-point k comes with a list of
+# symmetry operations (S, τ) (containing at least the trivial operation (I, 0)), where S is
+# a unitary matrix (/!\ in cartesian but not in reduced coordinates) and τ a translation
+# vector. The k-point is then used to represent implicitly the information at all the
+# kpoints Sk. The relationship between the Hamiltonians is
+#   H_{Sk} = U H_k U*, with
+#   (Uu)(x) = u(W x + w)
 # or in Fourier space
-# (Uu)(G) = e^{-i G τ} u(S^-1 G)
+#   (Uu)(G) = e^{-i G τ} u(S^-1 G)
 # In particular, we can choose the eigenvectors at Sk as u_{Sk} = U u_k
 
-# We represent then the BZ as a set of irreducible points `kpoints`,
-# and a set of weights `kweights` (summing to 1). The value of
-# observables is given by a weighted sum over the irreducible kpoints,
-# plus a symmetrization operation (which depends on the particular way
+# We represent then the BZ as a set of irreducible points `kpoints`, and a set of weights
+# `kweights` (summing to 1). The value of observables is given by a weighted sum over the
+# irreducible kpoints, plus a symmetrization operation (which depends on the particular way
 # the observable transforms under the symmetry).
 
 # There is by decreasing cardinality
