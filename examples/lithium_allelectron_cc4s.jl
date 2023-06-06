29074c7acf5624278e3b60cae33ad9d714aad4ed
Michael F. Herbst <info@michael-herbst.com>
2023-06-06 16:54:41 +0200

    add lithium example

--- examples/lithium_allelectron_cc4s.jl
+++ examples/lithium_allelectron_cc4s.jl
@@ -8,4 +8,4 @@ model = model_LDA(pyconvert(AbstractSystem, system); temperature=1e-3, smearing=
 basis = PlaneWaveBasis(model; Ecut=10, kgrid=[1, 1, 1])
 scfres = self_consistent_field(basis, tol=1e-8)
 
-DFTK.export_cc4s("silicon_cc4s.hdf5", scfres)
+DFTK.export_cc4s("lithium_cc4s.hdf5", scfres)
