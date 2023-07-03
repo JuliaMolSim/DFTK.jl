8c0cf763a50386f42f8b1c4472253abcd4e8e6fc
Michael F. Herbst <info@michael-herbst.com>
2023-07-03 23:19:20 +0200

    up

--- src/workarounds/cuda_arrays.jl
+++ src/workarounds/cuda_arrays.jl
@@ -1,9 +1,16 @@
 synchronize_device(::GPU{<:CUDA.CuArray}) = CUDA.synchronize()
 
-for fun in (:potential_terms, :kernel_terms)
-    @eval function DftFunctionals.$fun(fun::DispatchFunctional,
-                                       ρ::CUDA.CuMatrix{Float64}, args...)
-        @assert Libxc.has_cuda()
-        $fun(fun.inner, ρ, args...)
+@static if @load_preference("libxc_cuda", "true") == "true"
+    for fun in (:potential_terms, :kernel_terms)
+        @eval function DftFunctionals.$fun(fun::DispatchFunctional,
+                                           ρ::CUDA.CuMatrix{Float64}, args...)
+            @assert Libxc.has_cuda()
+            $fun(fun.inner, ρ, args...)
+        end
     end
+end
+
+# Note: This preference is temporary for now and can disappear at any time.
+function disable_libxc_cuda!()
+    @set_preferences!("libxc_cuda" => "false")
 end
