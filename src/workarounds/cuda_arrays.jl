bd1e457ce7960f9ce9228a6a9aa5761464865f91
Michael F. Herbst <info@michael-herbst.com>
2023-07-20 15:43:04 +0200

    Remove use_libxc_cuda preference

--- src/workarounds/cuda_arrays.jl
+++ src/workarounds/cuda_arrays.jl
@@ -1,11 +1,9 @@
 synchronize_device(::GPU{<:CUDA.CuArray}) = CUDA.synchronize()
 
-@static if @load_preference("use_libxc_cuda", "true") == "true"
-    for fun in (:potential_terms, :kernel_terms)
-        @eval function DftFunctionals.$fun(fun::DispatchFunctional,
-                                           ρ::CUDA.CuMatrix{Float64}, args...)
-            @assert Libxc.has_cuda()
-            $fun(fun.inner, ρ, args...)
-        end
+for fun in (:potential_terms, :kernel_terms)
+    @eval function DftFunctionals.$fun(fun::DispatchFunctional,
+                                       ρ::CUDA.CuMatrix{Float64}, args...)
+        @assert Libxc.has_cuda()
+        $fun(fun.inner, ρ, args...)
     end
 end
