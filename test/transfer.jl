1d5ff1d82ffe0d1a10565c9a63ca1068fc62d224
Andrea Azzali <andrea.azzali@sns.it>
2023-09-01 16:55:08 +0200

    Added test for density transfer

--- test/transfer.jl
+++ test/transfer.jl
@@ -1,6 +1,6 @@
 using Test
 using DFTK
-using DFTK: transfer_blochwave, compute_transfer_matrix
+using DFTK: transfer_blochwave, compute_transfer_matrix, transfer_density
 using LinearAlgebra
 
 include("testcases.jl")
@@ -43,4 +43,33 @@ include("testcases.jl")
     ψ_b = transfer_blochwave(ψ, basis, bigger_basis)
     ψ_bb = transfer_blochwave(ψ_b, bigger_basis, basis)
     @test norm(ψ-ψ_bb) < eps(eltype(basis))
+end
+
+@testset "Transfer of density" begin
+    model = Model(diagm(ones(3)))
+    kgrid = [1,1,1]
+    Ecut = 10
+
+    # Test grids that have both even and odd sizes.
+
+    # Transfer from small to big basis and back, check that this acts
+    # like identity.
+    basis      = PlaneWaveBasis(model; Ecut, kgrid, fft_size=(15,30,1))
+    basis_big  = PlaneWaveBasis(model; Ecut, kgrid, fft_size=(20,33,11))
+    ρ = random_density(basis,1)
+    ρ_b  = transfer_density(ρ, basis, basis_big)
+    ρ_bb = transfer_density(ρ_b, basis_big, basis)
+    @test norm(ρ_bb - ρ)/norm(ρ) < 10eps(eltype(basis))
+
+    # Transfer from big to small basis and back, check that this acts
+    # like identity on the Fourier components of the small basis.
+    basis      = PlaneWaveBasis(model; Ecut, kgrid, fft_size=(9,10,1))
+    basis_big  = PlaneWaveBasis(model; Ecut, kgrid, fft_size=(16,24,1))
+    ρ = random_density(basis_big,1)
+    ρ_s  = transfer_density(ρ, basis_big, basis)
+    ρ_ss = transfer_density(ρ_s, basis, basis_big)
+    err = norm([x-y for (x,y) in zip(fft(basis_big, ρ_ss)[:,1,1,1],
+                                     fft(basis_big,ρ)[:,1,1,1])
+                if abs(x)>10eps(eltype(basis_big))])
+    @test err/norm(ρ_ss) < 10eps(eltype(basis_big))
 end
