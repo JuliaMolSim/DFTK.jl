9cad528410ea2e92a948993e6aeca80ba28b5b72
Michael F. Herbst <info@michael-herbst.com>
2023-10-03 16:48:07 +0200

    Update self_consistent_field.jl

--- src/scf/self_consistent_field.jl
+++ src/scf/self_consistent_field.jl
@@ -78,7 +78,8 @@ Overview of parameters:
   the number of bands to compute. If you want to influence this algorithm or use a predefined
   number of bands in each SCF step, pass a [`FixedBands`](@ref) or [`AdaptiveBands`](@ref).
   Beware that with non-zero temperature, the convergence of the SCF algorithm may be limited
-  by the `default_occupation_threshold` parameter.
+  by the `default_occupation_threshold` parameter. For highly accurate calculations we thus
+  recommend increasing the `default_occupation_threshold` of the `AdaptiveBands`.
 - `callback`: Function called at each SCF iteration. Usually takes care of printing the
   intermediate state.
 """
@@ -94,7 +95,7 @@ Overview of parameters:
     solver=scf_anderson_solver(),
     eigensolver=lobpcg_hyper,
     determine_diagtol=ScfDiagtol(),
-    nbandsalg::NbandsAlgorithm=AdaptiveBands(basis.model; occupation_threshold=tol/10),
+    nbandsalg::NbandsAlgorithm=AdaptiveBands(basis.model),
     fermialg::AbstractFermiAlgorithm=default_fermialg(basis.model),
     callback=ScfDefaultCallback(; show_damping=false),
     compute_consistent_energies=true,
