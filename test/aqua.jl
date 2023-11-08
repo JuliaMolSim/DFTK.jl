eba1dd5848c3eec71c9d84fb8962b197e067eb72
Michael F. Herbst <info@michael-herbst.com>
2023-11-08 16:05:43 +0100

    Update test selection mechanism

--- test/aqua.jl
+++
@@ -1,5 +0,0 @@
-using DFTK
-using Aqua
-# TODO For now disable type piracy check, as we use that at places to patch up missing functionality.
-#      Should disable this on a more fine-grained scale.
-Aqua.test_all(DFTK, ambiguities=false, piracy=false, stale_deps=(ignore=[:Primes, ], ))
--- test/runtests.jl
+++ test/runtests.jl
@@ -10,6 +10,7 @@ if VERSION < v"1.8-"
     end
 end
 
+using Aqua
 using TestItemRunner
 using DFTK
 using Random
@@ -24,6 +25,12 @@ using Random
 # runs all tests plus the "example" tests.
 #
 
+# Setup threading in DFTK
+setup_threading(; n_blas=2)
+
+# Initialize seed
+Random.seed!(0)
+
 const DFTK_TEST_ARGS = let
     if "DFTK_TEST_ARGS" in keys(ENV)
         append!(split(ENV["DFTK_TEST_ARGS"], ","), ARGS)
@@ -32,51 +39,34 @@ const DFTK_TEST_ARGS = let
     end
 end
 
-const RUN_EXAMPLES = "example" in DFTK_TEST_ARGS
 # Tags supplied by the user.
 const TAGS = isempty(DFTK_TEST_ARGS) ? [:all] : Symbol.(DFTK_TEST_ARGS)
 
-# Work around some issues with test preferences
-let
-    test_project = first(Base.load_path())
-    preferences_file = joinpath(dirname(@__DIR__), "LocalPreferences.toml")
-    test_preferences_file = joinpath(dirname(test_project), "LocalPreferences.toml")
-    if isfile(preferences_file) && !isfile(test_preferences_file)
-        cp(preferences_file, test_preferences_file)
-    end
+# Tags excluded from the first run of "all" tests
+const EXCLUDED_FROM_ALL = Symbol[:example, :gpu]
+:fast ∈ TAGS     && push!(EXCLUDED_FROM_ALL, :slow)
+mpi_nprocs() > 1 && push!(EXCLUDED_FROM_ALL, :dont_test_mpi)
+Sys.iswindows()  && push!(EXCLUDED_FROM_ALL, :dont_test_windows)
+
+# Tags explicitly included
+const EXTRA_TAGS = filter(!in((:all, :fast)), TAGS)
+
+println("Running tests")
+if :all ∈ TAGS || :fast ∈ TAGS
+    println("    all except: $(join(EXCLUDED_FROM_ALL, ", "));")
+end
+if !isempty(EXTRA_TAGS)
+    println("    plus:       $(join(EXTRA_TAGS, ", "));")
 end
 
+if :all ∈ TAGS || :fast ∈ TAGS
+    is_excluded(ti) = any(in(ti.tags), EXCLUDED_FROM_ALL)
+    @run_package_tests filter=!is_excluded
 
-# Setup threading in DFTK
-setup_threading(; n_blas=2)
-
-# Initialize seed
-Random.seed!(0)
-
-exclude_tags = []
-:fast ∈ TAGS && push!(exclude_tags, :slow)
-:gpu ∉ TAGS  && push!(exclude_tags, :gpu)
-mpi_nprocs() > 1 && push!(exclude_tags, :dont_test_mpi)
-Sys.iswindows() && push!(exclude_tags, :unix)
-
-fast_string = :fast ∈ TAGS ? "fast" : ""
-println("   Running $(fast_string) tests")
-println("       User TAGS     = $(join(TAGS, ", ")).")
-println("       Excluded TAGS = $(join(exclude_tags, ", ")).")
-push!(exclude_tags, :example)  # we run the examples at the end if requested
-
-
-# Synthetic tests at the beginning, so it fails faster if something has gone badly wrong
-if :all ∈ TAGS || :core ∈ TAGS
-    @run_package_tests filter=ti->!(any(e -> e ∈ ti.tags, exclude_tags)) && (:core ∈ ti.tags)
-    push!(exclude_tags, :core)
-end
-@run_package_tests filter=ti->!(any(e -> e ∈ ti.tags, exclude_tags))
-
-if :all in TAGS && mpi_master()
-    include("aqua.jl")
+    # TODO For now disable type piracy check, as we use that at places to patch
+    #      up missing functionality. Should disable this on a more fine-grained scale.
+    Aqua.test_all(DFTK, ambiguities=false, piracy=false, stale_deps=(ignore=[:Primes, ], ))
 end
 
-if RUN_EXAMPLES
-    @run_package_tests filter=ti->(:example ∈ ti.tags)
-end
+is_explicitly_selected(ti) = any(in(ti.tags), EXTRA_TAGS)
+@run_package_tests filter=is_explicitly_selected
