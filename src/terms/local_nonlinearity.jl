c0fb8d67362583a3560fd2c64ae5d2dbbc3d49ed
Antoine Levitt <antoine.levitt@gmail.com>
2023-11-21 14:32:29 +0100

    Fix bug with spin and local_nonlinearity

--- src/terms/local_nonlinearity.jl
+++ src/terms/local_nonlinearity.jl
@@ -11,24 +11,27 @@ end
 
 function ene_ops(term::TermLocalNonlinearity, basis::PlaneWaveBasis{T}, ψ, occupation;
                  ρ, kwargs...) where {T}
+    ρ = total_density(ρ)
     fp(ρ) = ForwardDiff.derivative(term.f, ρ)
     E = sum(fρ -> convert_dual(T, fρ), term.f.(ρ)) * basis.dvol
     potential = convert_dual.(T, fp.(ρ))
 
     # In the case of collinear spin, the potential is spin-dependent
-    ops = [RealSpaceMultiplication(basis, kpt, potential[:, :, :, kpt.spin])
+    ops = [RealSpaceMultiplication(basis, kpt, potential)
            for kpt in basis.kpoints]
     (; E, ops)
 end
 
 
 function compute_kernel(term::TermLocalNonlinearity, ::AbstractBasis{T}; ρ, kwargs...) where {T}
+    ρ = total_density(ρ)
     fp(ρ) = ForwardDiff.derivative(term.f, ρ)
     fpp(ρ) = ForwardDiff.derivative(fp, ρ)
     Diagonal(vec(convert_dual.(T, fpp.(ρ))))
 end
 
 function apply_kernel(term::TermLocalNonlinearity, ::AbstractBasis{T}, δρ; ρ, kwargs...) where {T}
+    ρ = total_density(ρ)
     fp(ρ) = ForwardDiff.derivative(term.f, ρ)
     fpp(ρ) = ForwardDiff.derivative(fp, ρ)
     convert_dual.(T, fpp.(ρ) .* δρ)
