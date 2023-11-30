517c0bf271e32524de51d7ce2b528010c746e249
Michael F. Herbst <info@michael-herbst.com>
2023-11-30 10:00:44 +0100

    My suggestion for CI simplification

--- test/runtests_parser.jl
+++ test/runtests_parser.jl
@@ -1,28 +1,38 @@
 function parse_test_args()
-    test_args = ARGS
+    # Parsing code
+    args = Symbol.(ARGS)
     if "DFTK_TEST_ARGS" in keys(ENV) && isempty(ARGS)
-        test_args = split(ENV["DFTK_TEST_ARGS"], ",")
+        args = Symbol.(split(ENV["DFTK_TEST_ARGS"], ","))
     end
-    args = isempty(test_args) ? [:all] : Symbol.(test_args)
 
-    if :gpu in args
-        base_tag = :gpu
-        excluded = Symbol[:mpi]
-    elseif :mpi in args
-        base_tag = :mpi
-        excluded = Symbol[:dont_test_mpi, :gpu]
+    # Figure out the base tag
+    incl_excl = Dict(
+        :gpu => (; included=Symbol[], excluded=[:mpi]),
         # TODO :dont_test_mpi added for backwards compatibility ... drop later
-    else
+        :mpi => (; included=Symbol[], excluded=[:gpu, :dont_test_mpi]),
+        :all => (; included=Symbol[], excluded=[:gpu, :mpi]),
+    )
+    base_tag = filter!(in(keys(incl_excl)), args)
+    if isempty(base_tag)
         base_tag = :all
-        excluded = Symbol[:gpu, :mpi]
+    elseif length(base_tag) > 2
+        error("Cannot have more than one of $(join(string.(keys(incl_excl)), ", "))")
+    else
+        base_tag = base_tag[1]
     end
-    if :fast in args
-        push!(excluded, :slow)
-    end
+    (; excluded, included) = incl_excl[base_tag]
+
+    # Perform extra modifications
     if Sys.iswindows()
         push!(excluded, :dont_test_windows)
     end
+    for arg in filter(!in(keys(incl_excl)), args)
+        if startswith(string(arg), "no")
+            push!(excluded, Symbol(string(arg)[3:end]))  # Strip "no" and add
+        else
+            push!(included, arg)
+        end
+    end
 
-    included = filter(!in((:all, :fast, :gpu, :mpi)), args)
     (; base_tag, excluded, included)
 end
