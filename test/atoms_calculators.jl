a06df050e4a508576c7015d4fe1004a693cad7b7
CedricTravelletti <cedrictravelletti@gmail.com>
2023-12-20 10:35:49 +0100

    Modified AtomsCalculators forces to have nonzero forces.

--- test/atoms_calculators.jl
+++ test/atoms_calculators.jl
@@ -4,20 +4,22 @@
     using AtomsCalculators
     silicon = TestCases.silicon
 
+    # Perturb from equilibrium so forces are not 0.
+    silicon.positions[1] += [0.05, 0, 0]
     # Converto to AtomsBase system
     silicon = periodic_system(silicon.lattice, silicon.atoms, silicon.positions)
     
-    model_kwargs = (; functionals=[:lda_x, :lda_c_pw])
+    model_kwargs = (; temperature=1e-6, functionals=[:lda_x, :lda_c_pw])
     basis_kwargs = (; kgrid=[4, 4, 4], Ecut=5.0)
     scf_kwargs = (; tol=1e-7)
     calculator = DFTKCalculator(silicon; model_kwargs, basis_kwargs, scf_kwargs)
 
     energy = AtomsCalculators.potential_energy(silicon, calculator)
-    ref_energy = -7.8691334531
+    ref_energy = -7.73109
     @test isapprox(energy, ref_energy; rtol=1e-3)
 
     forces = AtomsCalculators.forces(silicon, calculator)
-    ref_forces = [[6.071013955320855e-10, -4.690795889974796e-10, -6.299121885664237e-10],
-                  [4.750013095864486e-10, -3.6292771194166813e-10, 3.5873004768426427e-10]]
-    @test isapprox(forces, ref_forces; atol=1e-5)
+    ref_forces = [[0.213133, -0.13936, -0.13931],
+                  [-0.213145, 0.13935, 0.13931]]
+    @test isapprox(forces, ref_forces; rtol=1e-3)
 end
