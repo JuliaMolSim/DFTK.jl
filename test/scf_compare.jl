d6d5459c5aeac8395f6c3370da894c984f210bbb
Michael F. Herbst <info@michael-herbst.com>
2023-12-28 13:29:10 +0100

    Test fix

--- test/scf_compare.jl
+++ test/scf_compare.jl
@@ -79,7 +79,7 @@ end
     # Run DM
     if mpi_nprocs() == 1  # Distributed implementation not yet available
         @testset "Direct minimization" begin
-            ρ_dm = direct_minimization(basis, ψ0; g_tol=tol).ρ
+            ρ_dm = direct_minimization(basis, ψ0; tol).ρ
             @test maximum(abs.(ρ_dm - ρ_def)) < 10tol
         end
     end
