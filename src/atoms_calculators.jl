b911379a56d92fa1e1c3be457b6909c4ab41fad1
CedricTravelletti <cedrictravelletti@gmail.com>
2024-01-11 10:03:57 +0100

    Minor cosmetic changes.

--- src/atoms_calculators.jl
+++ src/atoms_calculators.jl
@@ -4,7 +4,7 @@
 # This interface is inspired by the one used in Molly.jl, 
 # see https://github.com/JuliaMolSim/Molly.jl/blob/master/src/types.jl
 #
-# Important: By default, when the calculator is called with a `state`, the 
+# By default, when the calculator is called with a `state`, the 
 # symmetries of the state will be re-used in the current calculation (the basis 
 # is re-built, but symmetries are fixed and not re-computed). 
 #    
@@ -49,9 +49,9 @@ function DFTKCalculator(system; state=nothing, verbose=false,
 end
 
 function compute_scf!(system::AbstractSystem, calculator::DFTKCalculator, state::DFTKState)
+    # Note that we re-use the symmetries from the state, to avoid degenerate cases.
     model = model_DFT(system; calculator.params.model_kwargs...,
                       symmetries=state.scfres.basis.symmetries)
-    # Note that we re-use the symmetries from the state, to avoid degenerate cases.
     basis = PlaneWaveBasis(model; calculator.params.basis_kwargs...)
     
     # Note that we use the state's densities and orbitals, but change the basis 
