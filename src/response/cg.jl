5049ffb3c926b64978f575af6db867009869e311
Étienne Polack <87805034+epolack@users.noreply.github.com>
2024-01-24 14:45:59 +0000

    Factorisation of phonon test cases (#822)

--- src/response/cg.jl
+++ src/response/cg.jl
@@ -28,14 +28,21 @@ function cg!(x::AbstractVector{T}, A::LinearMap{T}, b::AbstractVector{T};
     # p is the descent direction
     p = copy(c)
     n_iter = 0
-    residual_norm = zero(real(T))
+    residual_norm = norm(r)
 
     # convergence history
     converged = false
 
     # preconditioned conjugate gradient
     while n_iter < maxiter
+        # output
+        info = (; A, b, n_iter, x, r, residual_norm, converged, stage=:iterate)
+        callback(info)
         n_iter += 1
+        if (n_iter ≥ miniter) && residual_norm ≤ tol
+            converged = true
+            break
+        end
         mul!(c, A, p)
         α = γ / dot(p, c)
 
@@ -43,14 +50,6 @@ function cg!(x::AbstractVector{T}, A::LinearMap{T}, b::AbstractVector{T};
         x .= proj(x .+ α .* p)
         r .= proj(r .- α .* c)
         residual_norm = norm(r)
-
-        # output
-        info = (; A, b, n_iter, x, r, residual_norm, converged, stage=:iterate)
-        callback(info)
-        if (n_iter > miniter) && residual_norm <= tol
-            converged = true
-            break
-        end
 
         # apply preconditioner and prepare next iteration
         ldiv!(c, precon, r)
