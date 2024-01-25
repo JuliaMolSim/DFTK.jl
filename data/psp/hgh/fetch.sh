dc8275c6a9688125db35b635acba53737c2b0370
Matthias Krack <matthias.krack@psi.ch>
2024-01-25 19:25:28 +0100

    Filter GTH-PP names which do not match the naming convention

--- data/psp/hgh/fetch.sh
+++ data/psp/hgh/fetch.sh
@@ -6,6 +6,7 @@ for xcf in pade pbe; do
    element=($(wget -nv ${URL}/GTH_POTENTIALS -O - | grep -i "${xcf}-q" | awk '{print $1}'))
    zpseudo=($(wget -nv ${URL}/GTH_POTENTIALS -O - | grep -i "${xcf}-q" | awk '{print $2}' | cut -d- -f3))
    for ((i=0; i < ${#element[@]}; i++)); do
+      [[ ${zpseudo[i]} =~ [^q[:digit:]] ]] && continue
       ppname="${element[i]}-${zpseudo[i]}"
       remote="${xcf}/${ppname}"
       target=$(echo ${remote/pade/lda}.hgh | tr 'A-Z' 'a-z')
