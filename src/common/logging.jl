8ddb5511e80a6791b244a3b52c66a189a517fab7
Étienne Polack <polack@hollved.net>
2024-02-09 03:10:30 +0100

    reducing `@info` verbosity
    still `[` in `boxstr`

--- src/common/logging.jl
+++ src/common/logging.jl
@@ -1,1 +1,8 @@
 using Logging
+
+function meta_formatter(level::LogLevel, args...)
+    color = Logging.default_logcolor(level)
+    Info == level && return color, "", ""
+    Logging.default_metafmt(level, args...)
+end
+global_logger(ConsoleLogger(stdout, Info; meta_formatter))
