60fb97f9afd85f94c330dff528a6ba7b8d661938
CedricTravelletti <cedrictravelletti@gmail.com>
2024-03-05 15:12:43 +0100

    Fixed units.

--- src/external/atoms_calculators.jl
+++ src/external/atoms_calculators.jl
@@ -4,6 +4,8 @@
 # see https://github.com/JuliaMolSim/Molly.jl/blob/master/src/types.jl
 using AtomsBase
 using AtomsCalculators
+using Unitful
+using UnitfulAtomic
 
 
 Base.@kwdef struct DFTKParameters
@@ -69,14 +71,14 @@ AtomsCalculators.@generate_interface function AtomsCalculators.potential_energy(
         system::AbstractSystem, calculator::DFTKCalculator; state = DFTKState(),
         kwargs...)
     compute_scf!(system, calculator, state)
-    calculator.state.scfres.energies.total
+    calculator.state.scfres.energies.total * u"hartree"
 end
 
 AtomsCalculators.@generate_interface function AtomsCalculators.forces(
         system::AbstractSystem, calculator::DFTKCalculator; state = DFTKState(),
         kwargs...)
     compute_scf!(system, calculator, state)
-    compute_forces_cart(calculator.state.scfres)
+    compute_forces_cart(calculator.state.scfres) * u"hartree/bohr"
 end
 
 AtomsCalculators.@generate_interface function AtomsCalculators.virial(
@@ -84,5 +86,5 @@ AtomsCalculators.@generate_interface function AtomsCalculators.virial(
         kwargs...)
     compute_scf!(system, calculator, state)
     - (compute_stresses_cart(calculator.state.scfres)
-     * calculator. state.scfres.basis.model.unit_cell_volume)
+     * calculator. state.scfres.basis.model.unit_cell_volume) * u"hartree"
 end
