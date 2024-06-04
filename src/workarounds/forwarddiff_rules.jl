6ca199520146ef12495d489006ce3e896fa5f3e2
Michael F. Herbst <info@michael-herbst.com>
2024-06-04 12:22:30 +0200

    Workarounds

--- src/workarounds/forwarddiff_rules.jl
+++ src/workarounds/forwarddiff_rules.jl
@@ -204,6 +204,35 @@ function self_consistent_field(basis_dual::PlaneWaveBasis{T};
        scfres.algorithm, scfres.runtime_ns)
 end
 
+function hankel(r::AbstractVector, r2_f::AbstractVector, l::Integer, p::TT) where {TT <: ForwardDiff.Dual}
+    # This custom rule uses two properties of the hankel transform:
+    #   d H[f] / dp = 4\pi \int_0^∞ r^2 f(r) j_l'(p⋅r)⋅r dr
+    # and that
+    #   j_l'(x) = l / x * j_l(x) - j_{l+1}(x)
+    # and tries to avoid allocations as much as possible, which hurt in this inner loop.
+    #
+    # One could implement this by custom rules in integration and spherical bessels, but
+    # the tricky bit is to exploit that one needs both the j_l'(p⋅r) and j_l(p⋅r) values
+    # but one does not want to precompute and allocate them into arrays
+    # TODO Investigate custom rules for bessels and integration
+
+    T  = ForwardDiff.valtype(TT)
+    pv = ForwardDiff.value(p)
+
+    jl = sphericalbesselj_fast.(l, pv .* r)
+    value = 4T(π) * simpson((i, r, r2_f, jl) -> r2_f[i] * jl[i], r, r2_f, jl)
+
+    if iszero(pv)
+        return TT(value, zero(T) * ForwardDiff.partials(p))
+    end
+
+    function derivative_value(i, r, r2_f, jl, pv)
+        (r2_f[i] * (l * jl[i] / pv - r .* sphericalbesselj_fast(l+1, pv * r)))
+    end
+    derivative = 4T(π) * simpson(derivative_value, r, r2_f, jl, pv)
+    TT(value, derivative * ForwardDiff.partials(p))
+end
+
 # other workarounds
 
 # problem: ForwardDiff of norm of SVector gives NaN derivative at zero
