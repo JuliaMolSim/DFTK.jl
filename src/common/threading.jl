c8735df1dcc160473df4c77a9cc8f00db0397a14
Michael F. Herbst <info@michael-herbst.com>
2024-06-05 00:09:47 +0200

    Thread IO updates

--- src/common/threading.jl
+++ src/common/threading.jl
@@ -10,13 +10,12 @@ By default, use 1 FFT thread, and `Threads.nthreads()` BLAS and DFTK threads
 Changing `n_DFTK` requires a restart of Julia.
 """
 function setup_threading(; n_fft=1, n_blas=Threads.nthreads(), n_DFTK=nothing)
-    if n_DFTK != nothing
+    if !isnothing(n_DFTK)
         set_DFTK_threads!(n_DFTK)
     end
     n_DFTK = @load_preference("DFTK_threads", Threads.nthreads())
     FFTW.set_num_threads(n_fft)
     BLAS.set_num_threads(n_blas)
-    njulia = Threads.nthreads()
     mpi_master() && @info "Threading setup: " Threads.nthreads() n_DFTK n_fft n_blas
 end
 
@@ -30,10 +29,12 @@ end
 
 function set_DFTK_threads!(n)
     if n > Threads.nthreads()
-        error("You're asking for $n_DFTK DFTK threads, but only ran julia with $(Threads.nthreads()) threads.")
+        error("You set your preference for DFTK threads using set_DFTK_threads!, " *
+              "but only ran julia with $(Threads.nthreads()) threads.")
     end
     if @load_preference("DFTK_threads", nothing) != n
-        @info "DFTK_threads preference changed. This is a permanent change, restart julia to see the effect."
+        @info("DFTK_threads preference changed. This is a permanent change, " *
+              "restart julia to see the effect.")
     end
     @set_preferences!("DFTK_threads" => n)
 end
@@ -46,7 +47,8 @@ function get_DFTK_threads()
     if isnothing(nthreads)
         nthreads = Threads.nthreads()
     elseif nthreads > Threads.nthreads()
-        error("You set your preference for $n_DFTK DFTK threads, but only ran julia with $(Threads.nthreads()) threads.")
+        error("You set your preference for DFTK threads using set_DFTK_threads!, " *
+              "but only ran julia with $(Threads.nthreads()) threads.")
     end
     nthreads
 end
@@ -68,7 +70,7 @@ end
 # private interface to be called
 function parallel_loop_over_range(fun, range, storages)
     nthreads = get_DFTK_threads()
-    storages != nothing && @assert length(storages) >= nthreads
+    !isnothing(storages) && @assert length(storages) >= nthreads
     chunk_length = cld(length(range), nthreads)
 
     # this tensorized if is ugly, but this is potentially
