89e6f4f2e307676f91eb39b3141191670c0ada50
Michael F. Herbst <info@michael-herbst.com>
2024-06-04 09:59:09 +0200

    Hankel custom rule undo

--- src/common/hankel.jl
+++ src/common/hankel.jl
@@ -16,26 +16,26 @@ function hankel(r::AbstractVector, r2_f::AbstractVector, l::Integer, p::T)::T wh
     end
 end
 
-# This custom rule makes sure we can use loop vectorisation in the innermost (simpson) loop
-# and that we avoid allocating a Vector{<:Dual} each time hankel( ) is called
-@timing function hankel(r::AbstractVector, r2_f::AbstractVector, l::Integer, p::TT) where {TT <: ForwardDiff.Dual}
-    # Note that
-    #   d H[f] / dp = 4\pi \int_0^∞ r^2 f(r) j_l'(p⋅r)⋅r dr
-    # and that
-    #   j_l'(x) = l / x * j_l(x) - j_{l+1}(x)
-
-    T  = ForwardDiff.valtype(TT)
-    pv = ForwardDiff.value(p)
-
-    jl = sphericalbesselj_fast.(l, pv .* r)
-    value = 4T(π) * simpson((i, r) -> r2_f[i] * jl[i], r)
-
-    if iszero(pv)
-        return TT(value, zero(T) * ForwardDiff.partials(p))
-    end
-
-    derivative_value = (i, r) -> (r2_f[i] * (l * jl[i] / pv
-                                             - r .* sphericalbesselj_fast(l+1, pv * r)))
-    derivative = 4T(π) * simpson(derivative_value, r)
-    TT(value, derivative * ForwardDiff.partials(p))
-end
+# # This custom rule makes sure we can use loop vectorisation in the innermost (simpson) loop
+# # and that we avoid allocating a Vector{<:Dual} each time hankel( ) is called
+# @timing function hankel(r::AbstractVector, r2_f::AbstractVector, l::Integer, p::TT) where {TT <: ForwardDiff.Dual}
+#     # Note that
+#     #   d H[f] / dp = 4\pi \int_0^∞ r^2 f(r) j_l'(p⋅r)⋅r dr
+#     # and that
+#     #   j_l'(x) = l / x * j_l(x) - j_{l+1}(x)
+# 
+#     T  = ForwardDiff.valtype(TT)
+#     pv = ForwardDiff.value(p)
+# 
+#     jl = sphericalbesselj_fast.(l, pv .* r)
+#     value = 4T(π) * simpson((i, r) -> r2_f[i] * jl[i], r)
+# 
+#     if iszero(pv)
+#         return TT(value, zero(T) * ForwardDiff.partials(p))
+#     end
+# 
+#     derivative_value = (i, r) -> (r2_f[i] * (l * jl[i] / pv
+#                                              - r .* sphericalbesselj_fast(l+1, pv * r)))
+#     derivative = 4T(π) * simpson(derivative_value, r)
+#     TT(value, derivative * ForwardDiff.partials(p))
+# end
