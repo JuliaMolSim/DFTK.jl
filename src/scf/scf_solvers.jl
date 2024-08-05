1d405e98d26dc7c8ec24e189d2aa3d4908309c9c
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2024-08-05 16:08:53 +0200

    Comment on internals for flags in `scf_solvers.jl`

--- src/scf/scf_solvers.jl
+++ src/scf/scf_solvers.jl
@@ -1,8 +1,18 @@
-# these provide fixed-point solvers that can be passed to self_consistent_field()
+# these provide fixed-point solvers that can be passed to `self_consistent_field`
 
 # the fp_solver function must accept being called like
-# fp_solver(f, x0, info0; maxiter), where f(x, info) is the fixed-point map. 
-# It must return an object supporting res.fixpoint and res.info
+# `fp_solver(f, x0, info0; maxiter)`, where `f` is the fixed-point map.
+#
+# The fixed-point map `f` is expected to be called as such:
+#    `(fx, info) = f(x, info)`
+# The`info` contains auxiliary state, including two boolean flags:
+#    `info.converged`: flagged inside `f` if the convergence criterion is achieved.
+#    `info.timedout`: flagged inside `f` if a timeout is achieved.
+# The fixed-point function `f` is just passive in the sense that it flags these,
+# but still can be called further. The decision-making is left to the solver,
+# with the default convention being that either of these flags leads to termination.
+#
+# The solver must return an object supporting res.fixpoint and res.info
 
 """
 Create a damped SCF solver updating the density as
