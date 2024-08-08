b24192c44a2839abcb5b226c61ab55cf6ea7628c
Michael F. Herbst <info@michael-herbst.com>
2024-08-08 15:19:06 +0200

    Less brittle test

--- test/anderson.jl
+++ test/anderson.jl
@@ -3,25 +3,34 @@ using DFTK
 using LinearAlgebra
 (; silicon, aluminium) = TestCases.all_testcases
 
-function test_addiis(testcase; temperature=0, Ecut=10, kgrid=[3, 3, 3])
+function test_addiis(testcase; temperature=0, Ecut=10, kgrid=[3, 3, 3], n_bands=8)
     model = model_LDA(testcase.lattice, testcase.atoms, testcase.positions; temperature)
     basis = PlaneWaveBasis(model; kgrid, Ecut)
-    tol   = 1e-6
+    tol   = 1e-10
+    ρ = guess_density(basis)
+    ψ = [DFTK.random_orbitals(basis, kpt, n_bands) for kpt in basis.kpoints]
+
+    solver = scf_anderson_solver(; errorfactor=Inf, maxcond=Inf, m=100)
+    scfres_simple = self_consistent_field(basis; ρ, ψ, tol, mixing=SimpleMixing(), solver)
 
     solver = scf_anderson_solver(; errorfactor=Inf, maxcond=1e6, m=100)
-    scfres_rdiis = self_consistent_field(basis; tol, mixing=SimpleMixing(), solver)
+    scfres_rdiis = self_consistent_field(basis; ρ, ψ, tol, mixing=SimpleMixing(), solver)
 
-    solver = scf_anderson_solver(; errorfactor=1e4, maxcond=Inf, m=100)
-    scfres_addiis = self_consistent_field(basis; tol, mixing=SimpleMixing(), solver)
+    solver = scf_anderson_solver(; errorfactor=1e5, maxcond=Inf, m=100)
+    scfres_addiis = self_consistent_field(basis; ρ, ψ, tol, mixing=SimpleMixing(), solver)
 
     @test norm(scfres_addiis.ρ - scfres_rdiis.ρ) * sqrt(basis.dvol) < 10tol
-    @test scfres_addiis.n_iter ≤ scfres_rdiis.n_iter
+
+    # Test that some measure to control Anderson conditioning helps.
+    @test scfres_rdiis.n_iter  ≤ scfres_simple.n_iter
+    @test scfres_addiis.n_iter ≤ scfres_simple.n_iter
+
+    # Iterations can fluctuate a bit, but test that adaptive Anderson is not substantially
+    # worse than condition-number truncated Anderson
+    @test scfres_addiis.n_iter < scfres_rdiis.n_iter + 2
 end
 
-@testset "Silicon, no temp" begin
-    test_addiis(silicon; temperature=0)
-end
 @testset "Aluminium, temp" begin
-    test_addiis(aluminium; temperature=0.03)
+    test_addiis(aluminium; temperature=0.01)
 end
 end
