25afc5aac369a49c5def75c1b78a6ae6863357d4
Augustin Bussy <augustin.bussy@cscs.ch>
2024-08-26 17:44:17 +0200

    MPI workarounds for aarch64

---
+++ src/common/aarch64_mpi.jl
@@ -0,0 +1,90 @@
+"""
+MPI reduction opertations with custom types (i.e. anything that has not a MPI datatype equivalent)
+are not available on aarch64. These are temprorary workarounds, where variables with custom types 
+are broken down to standard types before communication, and recast to the initial types after.
+This file was created by fixing all MPI errors encountered by running the tests on an ARM machine:
+all sensible MPI reduction routines are implemented for each custom type causing an error.
+"""
+
+# Julia's Bool type has no direct equivalent MPI datatype => need integer conversion
+function mpi_min(bool::Bool, comm::MPI.Comm)
+    int = Int(bool)
+    Bool(mpi_min(int, comm))
+end
+
+function mpi_max(bool::Bool, comm::MPI.Comm)
+    int = Int(bool)
+    Bool(mpi_max(int, comm))
+end
+
+# Vec3{T} must be cast to Vector{T} before MPI reduction
+function mpi_sum!(arr::Vector{Vec3{T}}, comm::MPI.Comm) where{T}
+    n = length(arr)
+    new_arr = Vector{T}([])
+    for i in 1:n
+        append!(new_arr, arr[i][1:3])
+    end
+    mpi_sum!(new_arr, comm)
+    for i in 1:n
+	    arr[i] = Vec3{T}(new_arr[3(i-1)+1:3(i-1)+3])
+    end
+    arr
+end
+
+# ForwardDiff.Dual{T, U, V} and arrays of it must be cast to Vector{U} as well
+# utility function to cast a Dual type to an array containing a value and the partial diffs
+function dual_array(dual::ForwardDiff.Dual{T, U, V}) where{T, U, V}
+    dual_array = [ForwardDiff.value(dual)]
+    append!(dual_array, collect(ForwardDiff.partials(dual)))
+    dual_array
+end
+
+# utility function that casts back an array to a Dual type, based on a template Dual
+function new_dual(dual_array, template)
+    DualType = typeof(template)
+    PartialsType = typeof(ForwardDiff.partials(template))
+    DualType(dual_array[1], PartialsType(Tuple(dual_array[2:end])))
+end
+
+# MPI reductions of single ForwardDiff.Dual types
+function mpi_sum(dual::ForwardDiff.Dual{T, U, V}, comm::MPI.Comm) where{T, U, V}
+    arr = dual_array(dual)
+    mpi_sum!(arr, comm)
+    new_dual(arr, dual)
+end
+
+function mpi_min(dual::ForwardDiff.Dual{T, U, V}, comm::MPI.Comm) where{T, U, V}
+    arr = dual_array(dual)
+    mpi_min!(arr, comm)
+    new_dual(arr, dual)
+end
+
+function mpi_max(dual::ForwardDiff.Dual{T, U, V}, comm::MPI.Comm) where{T, U, V}
+    arr = dual_array(dual)
+    mpi_max!(arr, comm)
+    new_dual(arr, dual)
+end
+
+function mpi_mean(dual::ForwardDiff.Dual{T, U, V}, comm::MPI.Comm) where{T, U, V}
+    arr = dual_array(dual)
+    mpi_mean!(arr, comm)
+    new_dual(arr, dual)
+end
+
+# MPI reductions of arrays of ForwardDiff.Dual types
+function mpi_sum!(dual::Array{ForwardDiff.Dual{T, U, V}, N}, comm::MPI.Comm) where{T, U, V, N}
+    array = Vector{U}([])
+    lengths = []
+    for i in 1:length(dual)
+        tmp = dual_array(dual[i])
+        append!(array, tmp)
+        append!(lengths, length(tmp))
+    end
+    mpi_sum!(array, comm)
+    offset = 0
+    for i in 1:length(dual)
+        dual[i] = new_dual(array[offset+1:offset+lengths[i]], dual[i])
+        offset += lengths[i]
+    end
+    dual
+end
