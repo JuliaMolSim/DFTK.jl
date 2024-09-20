536635012278ef226785c609c00658f31fbd4d80
xquan818 <840169780@qq.com>
2024-09-20 20:11:11 +0200

    reupload the projected density of states

--- ext/DFTKPlotsExt.jl
+++ ext/DFTKPlotsExt.jl
@@ -2,7 +2,7 @@ module DFTKPlotsExt
 using Brillouin: KPath
 using DFTK
 using DFTK: is_metal, data_for_plotting, spin_components, default_band_εrange
-import DFTK: plot_dos, plot_bandstructure
+import DFTK: plot_dos, plot_bandstructure, plot_ldos, plot_pdos
 using Plots
 using Unitful
 using UnitfulAtomic
@@ -107,5 +107,99 @@ function plot_dos(basis, eigenvalues; εF=nothing, unit=u"hartree",
     p
 end
 plot_dos(scfres; kwargs...) = plot_dos(scfres.basis, scfres.eigenvalues; scfres.εF, kwargs...)
+
+function plot_ldos(basis, eigenvalues, ψ; εF=nothing, unit=u"hartree",
+                   temperature=basis.model.temperature,
+                   smearing=basis.model.smearing,
+                   εrange=default_band_εrange(eigenvalues; εF), 
+                   n_points=1000, ldos_xyz=[:, 1, 1], kwargs...)
+    eshift = something(εF, 0.0)
+    εs = range(austrip.(εrange)..., length=n_points)
+
+    # Constant to convert from AU to the desired unit
+    to_unit = ustrip(auconvert(unit, 1.0))
+
+    # LDε has three dimensions (x, y, z)
+    # map on a single axis to plot the variation with εs
+    LDεs = dropdims.(compute_ldos.(εs, Ref(basis), Ref(eigenvalues), Ref(ψ); smearing, temperature); dims=4)
+    LDεs_slice = similar(LDεs[1], n_points, length(LDεs[1][ldos_xyz...]))
+    for (i, LDε) in enumerate(LDεs)
+        LDεs_slice[i, :] = LDε[ldos_xyz...]
+    end
+    p = heatmap(1:size(LDεs_slice, 2), (εs .- eshift) .* to_unit, LDεs_slice; kwargs...)
+    if !isnothing(εF)
+        Plots.hline!(p, [0.0], label="εF", color=:green, lw=1.5)
+    end
+
+    if isnothing(εF)
+        Plots.ylabel!(p, "eigenvalues  ($(string(unit)))")
+    else
+        Plots.ylabel!(p, "eigenvalues -ε_F  ($(string(unit)))")
+    end
+    p
+end
+plot_ldos(scfres; kwargs...) = plot_ldos(scfres.basis, scfres.eigenvalues, scfres.ψ; scfres.εF, kwargs...)
+
+function plot_pdos(basis, eigenvalues, ψ, psp_group;
+                   εF=nothing, unit=u"hartree",
+                   temperature=basis.model.temperature,
+                   smearing=basis.model.smearing,
+                   εrange=default_band_εrange(eigenvalues; εF), 
+                   n_points=1000, p=nothing, kwargs...)
+    eshift = something(εF, 0.0)
+    εs = range(austrip.(εrange)..., length=n_points)
+    
+    # Constant to convert from AU to the desired unit
+    to_unit = ustrip(auconvert(unit, 1.0))
+
+    # Calculate the projections of the first atom in the atom group
+    pdos = compute_pdos(εs, basis, eigenvalues, ψ, psp_group; 
+                        temperature, smearing)
+    
+    # Summing all angular components for each atom orbital
+    psp = basis.model.atoms[first(psp_group)].psp
+    n_pswfc = DFTK.count_n_pswfc_radial(psp)
+    pdos_label = Vector{String}(undef, n_pswfc)
+    pdos_orbital = Vector{Vector{eltype(pdos)}}(undef, n_pswfc)
+    count = 1
+    for l = 0:psp.lmax
+        il_n = DFTK.count_n_pswfc_radial(psp, l)
+        mfet_count = sum(x -> DFTK.count_n_pswfc(psp,x), 0:l-1; init=1) 
+        for il = 1:il_n
+            mfet = mfet_count .+ il_n .* (collect(1:2l+1) .- 1)
+            pdos_label[count] = psp.pswfc_labels[l+1][il]
+            pdos_orbital[count] = dropdims(sum(pdos[:, mfet], dims=2); dims=2)
+            count += 1
+            mfet_count += 1
+        end
+    end
+    pdos_label = (String(basis.model.atoms[first(psp_group)].symbol) * "-") .* pdos_label
+
+    # Plot pdos for each orbital
+    p = something(p, Plots.plot(; kwargs...))
+    for i = 1:n_pswfc
+        Plots.plot!(p, (εs .- eshift) .* to_unit, pdos_orbital[i];
+                    label=pdos_label[i])
+    end
+    p
+end
+
+function plot_pdos(scfres; kwargs...)
+    # Plot DOS
+    p = plot_dos(scfres; scfres.εF, kwargs...)
+
+    # TODO Require symmetrization with respect to kpoints and BZ symmetry 
+    #      (now achieved by unfolding all the quantities).
+    scfres_unfold = DFTK.unfold_bz(scfres)
+    psp_groups = [group for group in scfres_unfold.basis.model.atom_groups
+                  if scfres_unfold.basis.model.atoms[first(group)] isa ElementPsp]
+
+    # Plot PDOS
+    for group in psp_groups
+        plot_pdos(scfres_unfold.basis, scfres_unfold.eigenvalues,
+                  scfres_unfold.ψ, group; scfres.εF, p, kwargs...)
+    end
+    p
+end
 
 end
--- src/postprocess/dos.jl
+++ src/postprocess/dos.jl
@@ -7,6 +7,10 @@
 #
 # LDOS (local density of states)
 # LD(ε) = sum_n f_n' |ψn|^2 = sum_n δ(ε - ε_n) |ψn|^2
+#
+# PDOS (projected density of states)
+# PD(ε) = sum_n f_n' |<ψn,ϕlmi>|^2
+# ϕlmi = ∑_R ϕlmi(r-pos-R) is a periodic atomic wavefunction centered at pos
 
 """
 Total density of states at energy ε
@@ -64,6 +68,77 @@ function compute_ldos(scfres::NamedTuple; ε=scfres.εF, kwargs...)
 end
 
 """
+Projected density of states at energy ε for the first atom in the atom group.
+"""
+function compute_pdos(ε, basis, eigenvalues, ψ, psp_group; 
+                      smearing=basis.model.smearing,
+                      temperature=basis.model.temperature)
+    if (temperature == 0) || smearing isa Smearing.None
+        error("compute_pdos only supports finite temperature")
+    end
+    filled_occ = filled_occupation(basis.model)
+
+    # Calculate the projections of the first atom in the atom group
+    psp = basis.model.atoms[first(psp_group)].psp
+    position = basis.model.positions[first(psp_group)]
+    projs = compute_pdos_projs(basis, eigenvalues, ψ, psp, position)
+
+    D = zeros(typeof(ε[1]), length(ε), size(projs[1], 2))
+    for (i, iε) in enumerate(ε)
+        for (ik, projk) in enumerate(projs)
+            @views for (iband, εnk) in enumerate(eigenvalues[ik])
+                enred = (εnk - iε) / temperature
+                D[i, :] .-= (filled_occ .* basis.kweights[ik] .* projk[iband, :]
+                             ./ temperature
+                             .* Smearing.occupation_derivative(smearing, enred))       
+            end
+        end
+    end
+    D = mpi_sum(D, basis.comm_kpts)
+end
+
+function compute_pdos(scfres::NamedTuple; ε=scfres.εF, kwargs...)
+    # TODO Require symmetrization with respect to kpoints and BZ symmetry 
+    #      (now achieved by unfolding all the quantities).
+    scfres = unfold_bz(scfres)
+    psp_groups = [group for group in scfres.basis.model.atom_groups
+                  if scfres.basis.model.atoms[first(group)] isa ElementPsp]
+    [compute_pdos(ε, scfres.basis, scfres.eigenvalues, scfres.ψ, group; kwargs...)
+     for group in psp_groups]
+end
+
+# Build projection matrix for a single atom
+# Stored as projs[K][nk,lmi] = |<ψnk, f_lmi>|^2,
+# where K is running over all k-points, l, m are AM quantum numbers, 
+# and i is running over all pseudo-atomic wavefunctions for a given l.
+function compute_pdos_projs(basis, eigenvalues, ψ, psp::PspUpf, position)
+    position = vector_red_to_cart(basis.model, position)
+    G_plus_k_all = [to_cpu(Gplusk_vectors_cart(basis, basis.kpoints[ik])) 
+                    for ik = 1:length(basis.kpoints)]
+
+    # Build Fourier transform factors of pseudo-wavefunctions centered at 0.
+    fourier_form = atomic_centered_function_form_factors(psp, eval_psp_pswfc_fourier,
+                                    count_n_pswfc_radial, count_n_pswfc, G_plus_k_all)
+
+    projs = Vector{Matrix}(undef, length(basis.kpoints))
+    for (ik, ψk) in enumerate(ψ)
+        fourier_form_ik = fourier_form[ik]
+        structure_factor_ik = exp.(-im .* [dot(position, Gik) for Gik in G_plus_k_all[ik]])
+        @views for iproj = 1:size(fourier_form_ik, 2)
+            # TODO Pseudo-atomic wave functions need to be orthogonalized.
+            fourier_form_ik[:, iproj] .= (structure_factor_ik .* fourier_form_ik[:, iproj]
+                                          ./ sqrt(basis.model.unit_cell_volume))
+        end
+        projs[ik] = abs2.(ψk' * fourier_form_ik)
+    end
+    projs
+end
+
+"""
 Plot the density of states over a reasonable range. Requires to load `Plots.jl` beforehand.
 """
 function plot_dos end
+
+function plot_ldos end
+
+function plot_pdos end
