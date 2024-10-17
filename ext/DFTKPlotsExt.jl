d8a5454ccddbcbe813d4449713e0192e4ece061f
Xue Quan <56442655+xuequan818@users.noreply.github.com>
2024-10-17 08:54:33 +0200

    Projected density of states (#1002)
    Co-authored-by: xquan818 <840169780@qq.com>

--- ext/DFTKPlotsExt.jl
+++ ext/DFTKPlotsExt.jl
@@ -2,7 +2,7 @@ module DFTKPlotsExt
 using Brillouin: KPath
 using DFTK
 using DFTK: is_metal, data_for_plotting, spin_components, default_band_εrange
-import DFTK: plot_dos, plot_bandstructure
+import DFTK: plot_dos, plot_bandstructure, plot_ldos, plot_pdos
 using Plots
 using Unitful
 using UnitfulAtomic
@@ -107,5 +107,89 @@ function plot_dos(basis, eigenvalues; εF=nothing, unit=u"hartree",
     p
 end
 plot_dos(scfres; kwargs...) = plot_dos(scfres.basis, scfres.eigenvalues; scfres.εF, kwargs...)
+
+function plot_ldos(basis, eigenvalues, ψ; εF=nothing, unit=u"hartree",
+                   temperature=basis.model.temperature,
+                   smearing=basis.model.smearing,
+                   εrange=default_band_εrange(eigenvalues; εF), 
+                   n_points=1000, ldos_xyz=[:, 1, 1], kwargs...)
+    eshift = something(εF, 0.0)
+    εs = range(austrip.(εrange)..., length=n_points)
+
+    # Constant to convert from AU to the desired unit
+    to_unit = ustrip(auconvert(unit, 1.0))
+
+    # LDε has three dimensions (x, y, z)
+    # map on a single axis to plot the variation with εs
+    LDεs = dropdims.(compute_ldos.(εs, Ref(basis), Ref(eigenvalues), Ref(ψ); smearing, temperature); dims=4)
+    LDεs_slice = similar(LDεs[1], n_points, length(LDεs[1][ldos_xyz...]))
+    for (i, LDε) in enumerate(LDεs)
+        LDεs_slice[i, :] = LDε[ldos_xyz...]
+    end
+    p = heatmap(1:size(LDεs_slice, 2), (εs .- eshift) .* to_unit, LDεs_slice; kwargs...)
+    if !isnothing(εF)
+        Plots.hline!(p, [0.0], label="εF", color=:green, lw=1.5)
+    end
+
+    if isnothing(εF)
+        Plots.ylabel!(p, "eigenvalues  ($(string(unit)))")
+    else
+        Plots.ylabel!(p, "eigenvalues -ε_F  ($(string(unit)))")
+    end
+    p
+end
+plot_ldos(scfres; kwargs...) = plot_ldos(scfres.basis, scfres.eigenvalues, scfres.ψ; scfres.εF, kwargs...)
+
+function plot_pdos(basis, eigenvalues, ψ, i, l, 
+                   psp, position, el::Symbol;
+                   εF=nothing, unit=u"hartree",
+                   temperature=basis.model.temperature,
+                   smearing=basis.model.smearing,
+                   εrange=default_band_εrange(eigenvalues; εF), 
+                   n_points=1000, p=nothing, kwargs...)
+    eshift = something(εF, 0.0)
+    εs = range(austrip.(εrange)..., length=n_points)
+    
+    # Constant to convert from AU to the desired unit
+    to_unit = ustrip(auconvert(unit, 1.0))
+
+    # Calculate the projections of the atom with given i and l,
+    # and sum all angular momentums m=-l:l 
+    pdos = dropdims(sum(compute_pdos(εs, basis, eigenvalues, ψ, i, l, 
+                    psp, position; temperature, smearing), dims=2); dims=2)
+    label = String(el) * "-" * psp.pswfc_labels[l+1][i]
+    
+    # Plot pdos 
+    p = something(p, Plots.plot(; kwargs...))
+    Plots.plot!(p, (εs .- eshift) .* to_unit, pdos; label)
+
+    p
+end
+
+function plot_pdos(scfres; kwargs...)
+    # Plot DOS
+    p = plot_dos(scfres; scfres.εF, kwargs...)
+
+    # TODO do the symmetrization instead of unfolding    
+    scfres_unfold = DFTK.unfold_bz(scfres)
+    basis = scfres_unfold.basis
+    psp_groups = [group for group in basis.model.atom_groups
+                  if basis.model.atoms[first(group)] isa ElementPsp]
+
+    # Plot PDOS for the first atom of each atom group
+    for group in psp_groups
+        psp = basis.model.atoms[first(group)].psp
+        position = basis.model.positions[first(group)]
+        el = basis.model.atoms[first(group)].symbol
+        for l = 0:psp.lmax
+            for i = 1:DFTK.count_n_pswfc_radial(psp, l)
+                plot_pdos(basis, scfres_unfold.eigenvalues, scfres_unfold.ψ, 
+                          i, l, psp, position, el; scfres.εF, p, kwargs...)
+            end
+        end                  
+    end
+
+    p
+end
 
 end
