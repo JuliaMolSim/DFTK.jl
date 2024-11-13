aca594fcdfde2820ddd35c944c4da51f4dc7eda6
Technici4n <13494793+Technici4n@users.noreply.github.com>
2024-11-13 11:52:30 +0100

    Simplify the test

--- test/refine.jl
+++ test/refine.jl
@@ -45,31 +45,9 @@ scfres_ref = self_consistent_field(basis_ref; tol)
 # Forces from the reference solution:
 forces_ref = compute_forces(scfres_ref)
 
-# Forces from first order linearization with exact δψ
-# - Compute the error ``P-P_*`` on the associated orbitals ``ϕ-ψ`` after aligning
-#   them: this is done by solving ``\min |ϕ - ψU|`` for ``U`` unitary matrix of
-#   size ``N×N`` (``N`` being the number of electrons) whose solution is
-#   ``U = S(S^*S)^{-1/2}`` where ``S`` is the overlap matrix ``ψ^*ϕ``.
-function compute_error(ϕ, ψ)
-    map(zip(ϕ, ψ)) do (ϕk, ψk)
-        S = ψk'ϕk
-        U = S*(S'S)^(-1/2)
-        ϕk - ψk*U
-    end
-end
-δψ_exact = compute_error(refinement.ψ, ψ_ref)
-δρ_linear = DFTK.compute_δρ(basis_ref, refinement.ψ, δψ_exact, refinement.occupation)
-refinement_linear = DFTK.RefinementResult(basis_ref, refinement.ψ, refinement.ρ, refinement.occupation, DFTK.proj_tangent(δψ_exact, refinement.ψ), δρ_linear)
-forces_linear = refine_forces(refinement_linear, forces)
-
 # Low Ecut forces are imprecise:
 @test norm(forces - forces_ref) / norm(forces_ref) > 0.15
 # Refined forces are more precise:
-@test norm(forces_linear - forces_ref) / norm(forces_ref) < 0.1
 @test norm(forces_refined - forces_ref) / norm(forces_ref) < 0.1
-# Refinement is a good approximation of linearization:
-# TODO: This is apparently not true here! Hopefully this happens because the kgrid and Ecut are too low.
-#error("Rel error linear $(norm(forces_linear - forces_ref) / norm(forces_ref))\nRel error refinement $(norm(forces_refined - forces_ref) / norm(forces_ref))\nRel error lin-refinement $(norm(forces_refined - forces_linear) / norm(forces_linear))")
-#@test norm(forces_refined - forces_linear) / norm(forces_linear) < 0.1
 
 end
