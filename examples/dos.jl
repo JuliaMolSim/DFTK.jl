bcac8b0bc89e43f729dbae2e8d7831b17cb923fe
Michael F. Herbst <info@michael-herbst.com>
2024-11-16 22:06:57 +0100

    Fix some examples

--- examples/dos.jl
+++ examples/dos.jl
@@ -5,14 +5,14 @@
 using DFTK
 using Unitful
 using Plots
-using LazyArtifacts
+using PseudoPotentialData
 
 ## Define the geometry and pseudopotential
 a = 10.26  # Silicon lattice constant in Bohr
 lattice = a / 2 * [[0 1 1.0];
     [1 0 1.0];
     [1 1 0.0]]
-Si = ElementPsp(:Si; psp=load_psp(artifact"pd_nc_sr_lda_standard_0.4.1_upf", "Si.upf"))
+Si = ElementPsp(:Si, PseudoFamily("pd_nc_sr_lda_standard_0.4.1_upf"))
 atoms = [Si, Si]
 positions = [ones(3) / 8, -ones(3) / 8]
 
