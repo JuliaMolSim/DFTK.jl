a31d5a99a51642ef0bf78ea6a832d8b482ca12fa
Technici4n <13494793+Technici4n@users.noreply.github.com>
2024-11-19 16:47:35 +0100

    Explain the method with more details, and move apply_inv_M to the top level

--- src/postprocess/refine.jl
+++ src/postprocess/refine.jl
@@ -1,6 +1,31 @@
 # Refinement of some quantities of interest (density, forces) following the
 # strategy described in [CDKL2021].
 #
+# The overall strategy is to perform a first SCF,
+# transfer the result to a new basis set with a larger Ecut,
+# then perform an approximate first order correction in the larger space.
+#
+# To compute the first order correction ("refinement") δP in the larger space,
+# one needs to invert (Ω+K)δP = -R(P) where (Ω+K) is the Hessian matrix and
+# R(P) = [P, [P, H(P)]] is the residual.
+# See newton.jl for an explanation of these quantities.
+#
+# The basis elements of the smaller basis set are called low frequency (_1 suffix).
+# The additional basis elements are all high frequency (_2 suffix).
+# We approximate (Ω+K)_22 by M_22 which is cheap to compute and invert, and (Ω+K)_21 by 0.
+#
+# The metric operator M is as follows:
+# M applied to the i-th band is defined by
+#   M_i = P^⟂ T_i^{1/2} P^⟂ T_i^{1/2} P^⟂,
+# with the diagonal operator
+#   T_i = kinetic energy + mean kinetic energy of i-th band.
+# To invert M_i effectively, we use P^⟂ T_i^{-1} P^⟂ as the preconditioner.
+#
+# Solving the system then amounts to computing
+#   δP_2 = M^{-1}_22 R_2(P)
+#   δP_1 = (Ω+K)^{-1}_11 (R_1(P) - (Ω+K)_12 δP_2)
+# The inversion of (Ω+K)_11 is roughly as expensive as the SCF on the low frequency space.
+#
 # [CDKL2021]:
 #     E. Cancès, G. Dusson, G. Kemlin, and A. Levitt
 #     *Practical error bounds for properties in plane-wave electronic structure
@@ -25,8 +50,57 @@ struct RefinementResult
 end
 
 """
+Invert the metric operator M.
+"""
+function invert_refinement_metric(basis::PlaneWaveBasis{T}, ψ, res) where {T}
+    # Apply the M_i operator with i=n.
+    function apply_M(ψk, Pk, δψnk, n)
+        proj_tangent_kpt!(δψnk, ψk)
+        δψnk = sqrt.(Pk.mean_kin[n] .+ Pk.kin) .* δψnk
+        proj_tangent_kpt!(δψnk, ψk)
+        δψnk = sqrt.(Pk.mean_kin[n] .+ Pk.kin) .* δψnk
+        proj_tangent_kpt!(δψnk, ψk)
+    end
+
+    # Apply the M_i^{-1} operator with i=n.
+    function apply_inv_M(ψk, Pk, resk, n)
+        proj_tangent_kpt!(resk, ψk)
+        op(x) = apply_M(ψk, Pk, x, n)
+        function f_ldiv!(x, y)
+            x .= proj_tangent_kpt(y, ψk)
+            x ./= (Pk.mean_kin[n] .+ Pk.kin)
+            proj_tangent_kpt!(x, ψk)
+        end
+        J = LinearMap{eltype(ψk)}(op, size(resk, 1))
+        # This CG seems to converge very quickly in practice (often 1 iteration).
+        # Set a very low tolerance and warn if the iteration count is too high.
+        (δψk, history) = IterativeSolvers.cg(J, resk;
+                                             Pl=FunctionPreconditioner(f_ldiv!),
+                                             reltol=0, abstol=100*eps(T), maxiter=20,
+                                             log=true)
+        if !history.isconverged
+            @warn """CG for `invert_refinement_metric` did not converge, this is unexpected.
+                    Residual norm after $(history.iters) iterations is $(history[:resnorm][end])."""
+        end
+        proj_tangent_kpt!(δψk, ψk)
+    end
+
+    map(enumerate(res)) do (ik, resk)
+        P = PreconditionerTPA(basis, basis.kpoints[ik])
+        ψk = ψ[ik]
+        precondprep!(P, ψk)
+        δψk = similar(resk)
+        for n = 1:size(resk, 2)
+            # Apply M_i^{-1} to each band.
+            δψk[:, n] = apply_inv_M(ψk, P, resk[:, n], n)
+        end
+        δψk
+    end
+end
+
+"""
 Transfer the result of an SCF to a larger basis set,
-and compute first order corrections ("refinements") to the wavefunctions and density.
+and compute approximate first order corrections ("refinements") to the wavefunctions and density.
 
 Only full occupations are currently supported.
 
@@ -58,58 +132,8 @@ function refine_scfres(scfres, basis_ref::PlaneWaveBasis{T}; ΩpK_tol,
     resLF = transfer_blochwave(res, basis_ref, basis)
     resHF = res - transfer_blochwave(resLF, basis, basis_ref)
 
-    """
-    Apply M^{-1}.
-
-    M applied to the i-th band is defined by
-      M_i = P^⟂ T_i^{1/2} P^⟂ T_i^{1/2} P^⟂,
-    with the diagonal operator
-      T_i = kinetic energy + mean kinetic energy of i-th band.
-
-    To invert M_i, use P^⟂ T_i^{-1} P^⟂ as the preconditioner.
-
-    See [CDKL2021] for more details.
-    """
-    function apply_inv_M(ψ, res)
-        """Apply the M_i operator with i=n."""
-        function apply_M(ψk, Pk, δψnk, n)
-            proj_tangent_kpt!(δψnk, ψk)
-            δψnk = sqrt.(Pk.mean_kin[n] .+ Pk.kin) .* δψnk
-            proj_tangent_kpt!(δψnk, ψk)
-            δψnk = sqrt.(Pk.mean_kin[n] .+ Pk.kin) .* δψnk
-            proj_tangent_kpt!(δψnk, ψk)
-        end
-
-        """Apply the M_i^{-1} operator with i=n."""
-        function apply_inv_M(ψk, Pk, resk, n)
-            proj_tangent_kpt!(resk, ψk)
-            op(x) = apply_M(ψk, Pk, x, n)
-            function f_ldiv!(x, y)
-                x .= proj_tangent_kpt(y, ψk)
-                x ./= (Pk.mean_kin[n] .+ Pk.kin)
-                proj_tangent_kpt!(x, ψk)
-            end
-            J = LinearMap{eltype(ψk)}(op, size(resk, 1))
-            δψk = IterativeSolvers.cg(J, resk, Pl=FunctionPreconditioner(f_ldiv!),
-                      verbose=false, reltol=0, abstol=1e-15)
-            proj_tangent_kpt!(δψk, ψk)
-        end
-
-        map(enumerate(res)) do (ik, resk)
-            P = PreconditionerTPA(basis_ref, basis_ref.kpoints[ik])
-            ψk = ψ[ik]
-            precondprep!(P, ψk)
-            δψk = similar(resk)
-            for n = 1:size(resk, 2)
-                # Apply M_i^{-1} to each band.
-                δψk[:, n] = apply_inv_M(ψk, P, resk[:, n], n)
-            end
-            δψk
-        end
-    end
-
     # Compute M^{-1}_22 R_2(P) as an approximation of (Ω+K)^{-1}_22 R_2(P).
-    e2 = apply_inv_M(ψr, resHF)
+    e2 = invert_refinement_metric(basis_ref, ψr, resHF)
 
     # Apply Ω+K to M^{-1}_22 R_2(P)
     Λ = map(enumerate(ψr)) do (ik, ψk)
