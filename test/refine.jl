57f32b499bd9d8bbfd4382120031aa32ce06ee75
Technici4n <13494793+Technici4n@users.noreply.github.com>
2024-11-20 18:11:33 +0100

    Change test case to silicon

--- test/refine.jl
+++ test/refine.jl
@@ -1,26 +1,15 @@
-@testitem "Force refinement practical error bounds (TiO2)" begin
+@testitem "Force refinement practical error bounds" setup=[TestCases] begin
 using DFTK
 using LinearAlgebra
-
-Ti = ElementPsp(:Ti; psp=load_psp("hgh/lda/ti-q4.hgh"))
-O  = ElementPsp(:O; psp=load_psp("hgh/lda/o-q6.hgh"))
-atoms     = [Ti, Ti, O, O, O, O]
-positions = [[0.5,     0.5,     0.5],  # Ti
-             [0.0,     0.0,     0.0],  # Ti
-             [0.19542, 0.80458, 0.5],  # O
-             [0.80458, 0.19542, 0.5],  # O
-             [0.30458, 0.30458, 0.0],  # O
-             [0.69542, 0.69542, 0.0]]  # O
-lattice   = [[8.79341  0.0      0.0];
-             [0.0      8.79341  0.0];
-             [0.0      0.0      5.61098]]
+silicon = TestCases.silicon
 
 # Displace for nonzero forces
+positions = deepcopy(silicon.positions)
 positions[1] .+= [-0.022, 0.028, 0.035]
 
-model = model_DFT(lattice, atoms, positions; functionals=LDA())
+model = model_DFT(silicon.lattice, silicon.atoms, positions; functionals=LDA())
 kgrid = [1, 1, 1]
-tol = 1e-5
+tol = 1e-10
 
 # Low Ecut computation
 Ecut = 15
@@ -42,8 +31,8 @@ scfres_ref = self_consistent_field(basis_ref; tol)
 forces_ref = compute_forces(scfres_ref)
 
 # Low Ecut forces are imprecise:
-@test norm(F - forces_ref) / norm(forces_ref) > 0.15
+@test norm(F - forces_ref) / norm(forces_ref) > 0.005
 # Refined forces are more precise:
-@test norm(F + dF - forces_ref) / norm(forces_ref) < 0.1
+@test norm(F + dF - forces_ref) / norm(forces_ref) < 0.002
 
 end
