06235e1cf26280b88f33f8ac0f5658b70260175c
Augustin Bussy <augustin.bussy@cscs.ch>
2024-12-03 11:41:27 +0100

    clean-up

--- src/common/cis2pi.jl
+++ src/common/cis2pi.jl
@@ -1,14 +1,13 @@
 """Function to compute exp(2π i x)"""
 cis2pi(x) = cispi(2x)
-function cis2pi(x::AbstractFloat)
+function cis2pi(x::T) where {T <: AbstractFloat}
     # Special case when 2x is an integer, as exp(n*π*i) = +- 1. Saves expensive
     # exponential evaluations when called repeatedly
     if isinteger(2x)
-        isodd(2x) ? result = -Complex{typeof(x)}(1, 0) : result = Complex{typeof(x)}(1, 0)
+        return isodd(2x) ? -one(complex(T)) : one(complex(T))
     else
-        result = cispi(2x)
+        return cispi(2x)
     end
-    result
 end
 
 """Function to compute sin(2π x)"""
