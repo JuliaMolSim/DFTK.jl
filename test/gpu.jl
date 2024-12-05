ed7ae286a06507621fe44987504a8b06e8e91438
Augustin Bussy <augustin.bussy@cscs.ch>
2024-12-05 15:48:25 +0100

    Enable force calculations on GPU runs (#1031)

--- test/gpu.jl
+++ test/gpu.jl
@@ -18,6 +18,8 @@
     scfres_gpu = run_problem(; architecture=DFTK.GPU(CuArray))
     @test abs(scfres_cpu.energies.total - scfres_gpu.energies.total) < 1e-9
     @test norm(scfres_cpu.ρ - Array(scfres_gpu.ρ)) < 1e-9
+    # Test that forces compute: symmetric structure, forces are zero
+    @test norm(compute_forces(scfres_cpu) - compute_forces(scfres_gpu)) < 1e-10
 end
 
 @testitem "CUDA iron functionality test" tags=[:gpu] setup=[TestCases] begin
@@ -43,6 +45,34 @@ end
     scfres_gpu = run_problem(; architecture=DFTK.GPU(CuArray))
     @test abs(scfres_cpu.energies.total - scfres_gpu.energies.total) < 1e-7
     @test norm(scfres_cpu.ρ - Array(scfres_gpu.ρ)) < 1e-6
+    # Test that forces compute: symmetric structure, forces are zero
+    @test norm(compute_forces(scfres_cpu) - compute_forces(scfres_gpu)) < 1e-9
+end
+
+@testitem "CUDA aluminium forces test" tags=[:gpu] setup=[TestCases] begin
+    using DFTK
+    using CUDA
+    using LinearAlgebra
+    aluminium = TestCases.aluminium
+    positions = aluminium.positions
+    # Perturb equilibrium position for non-zero forces
+    positions[1] += [0.01, 0.0, -0.01]
+
+    function run_problem(; architecture)
+        # Test with a core-corrected PSP for maximal coverage
+        Al = ElementPsp(aluminium.atnum, :Al, aluminium.mass, load_psp(aluminium.psp_upf))
+        atoms = fill(Al, length(aluminium.atoms))
+        model = model_DFT(aluminium.lattice, atoms, positions;
+                          functionals=PBE(), temperature=0.01)
+        basis = PlaneWaveBasis(model; Ecut=32, kgrid=(1, 1, 1), architecture)
+        self_consistent_field(basis; tol=1e-10, mixing=SimpleMixing())
+    end
+
+    scfres_cpu = run_problem(; architecture=DFTK.CPU())
+    scfres_gpu = run_problem(; architecture=DFTK.GPU(CuArray))
+    @test abs(scfres_cpu.energies.total - scfres_gpu.energies.total) < 1e-10
+    @test norm(scfres_cpu.ρ - Array(scfres_gpu.ρ)) < 1e-8
+    @test norm(compute_forces(scfres_cpu) - compute_forces(scfres_gpu)) < 1e-7
 end
 
 
@@ -52,4 +82,3 @@ end
 # TODO meta GGA
 # TODO Aluminium with LdosMixing
 # TODO Anderson acceleration
-# TODO Norm-conserving pseudopotentials with non-linear core
