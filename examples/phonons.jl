19da369c1f3c57080c91aab700db37294fa70046
Michael F. Herbst <info@michael-herbst.com>
2025-01-03 22:12:04 +0100

    up

--- examples/phonons.jl
+++ examples/phonons.jl
@@ -10,12 +10,14 @@ using PseudoPotentialData
 pseudopotentials = PseudoFamily("cp2k.nc.sr.lda.v0_1.semicore.gth")
 model  = model_DFT(bulk(:Si); pseudopotentials, functionals=LDA())
 basis  = PlaneWaveBasis(model; Ecut=10, kgrid=[4, 4, 4])
-scfres = self_consistent_field(basis, tol=1e-8)
+scfres = self_consistent_field(basis, tol=1e-8);
+nothing  # hide
 
 # Next we compute the phonon modes at the q-point `[1/4, 1/4, 1/4]`.
 
 scfres = DFTK.unfold_bz(scfres)
-phret_q0 = @time DFTK.phonon_modes(scfres; q=[0.25, 0.25, 0.25])
+phret_q0 = @time DFTK.phonon_modes(scfres; q=[0.25, 0.25, 0.25]);
+nothing  # hide
 
 # These are the final phonon frequencies:
 
