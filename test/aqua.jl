43b997b239dd31e843d05ee963a733adee99fb76
Michael F. Herbst <info@michael-herbst.com>
2025-01-10 20:45:51 +0100

    Aqua

--- test/aqua.jl
+++ test/aqua.jl
@@ -1,13 +1,14 @@
-@testitem "Aqua" tags=[:dont_test_mpi] begin
+@testitem "Aqua" tags=[:dont_test_mpi, :dont_test_windows] begin
     # TODO For now disable type piracy check, as we use that at places to patch
     #      up missing functionality. Should disable this on a more fine-grained scale.
 
     using DFTK
     using Aqua
+    using LinearAlgebra
     Aqua.test_all(DFTK;
                   ambiguities=(; exclude=[
                                # Type piracies we do for FFT stuff
-                               *, mul!, dot, \, ldiv!,
+                               *, \, mul!, dot, ldiv!,
                                # Libxc stuff
                                DFTK.potential_terms, DFTK.kernel_terms]),
                   piracies=false,
