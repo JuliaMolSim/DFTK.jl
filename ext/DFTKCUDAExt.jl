d5c3d1389453e9e83527ed0e607d8e1ba2f9db4b
Michael F. Herbst <info@michael-herbst.com>
2025-02-14 15:59:01 +0100

    Check also for LibxcCudaExt

--- ext/DFTKCUDAExt.jl
+++ ext/DFTKCUDAExt.jl
@@ -17,7 +17,7 @@ for fun in (:potential_terms, :kernel_terms)
 end
 
 # Insure pre-compilation can proceed without error (old Julia/packages versions)
-if Libxc.has_cuda()
+if Libxc.has_cuda() && !isnothing(Base.get_extension(Libxc, :LibxcCudaExt))
 
     # Precompilation block with a basic workflow
     @setup_workload begin
