de0f0a93bd0cd271c5c8db9fdd4e4fd187142013
Michael F. Herbst <info@michael-herbst.com>
2025-03-01 19:53:51 +0100

    up

--- examples/cuda.jl
+++ examples/cuda.jl
@@ -5,10 +5,10 @@ using PseudoPotentialData
 
 model  = model_DFT(bulk(:Si);
                    functionals=PBE(),
-                   pseudopotentials=PseudoFamily("cp2k.nc.sr.pbe.v0_1.semicore.gth"))
+                   pseudopotentials=PseudoFamily("dojo.nc.sr.pbe.v0_4_1.standard.upf"))
 
 # If available, use CUDA to store DFT quantities and perform main computations
 architecture = has_cuda() ? DFTK.GPU(CuArray) : DFTK.CPU()
 
 basis  = PlaneWaveBasis(model; Ecut=30, kgrid=(5, 5, 5), architecture)
-scfres = self_consistent_field(basis; tol=1e-2, solver=scf_damping_solver())
+scfres = self_consistent_field(basis; tol=1e-6)
