d5725e273f5667aa5a0304de7e39d7234585edd2
Michael F. Herbst <info@michael-herbst.com>
2025-03-03 22:41:02 +0100

    fix

--- docs/src/tricks/gpu.jl
+++ docs/src/tricks/gpu.jl
@@ -14,14 +14,13 @@
 #     If you require GPU support in one of our routines, where this is not
 #     yet supported, feel free to open an issue on github or otherwise get in touch.
 #
-# First we load required packages.
 
 using AtomsBuilder
 using DFTK
 using PseudoPotentialData
 
-# Next we set up a PBE model for bulk silicon.
-# Note that this proceeds exactly as in the CPU case
+# **Model setup.** First step is to setup a [`Model`](@ref) in DFTK.
+# This proceeds exactly as in the standard CPU case
 # (see also our [Tutorial](@ref)).
 
 silicon = bulk(:Si)
@@ -30,26 +29,32 @@ model  = model_DFT(silicon;
                    functionals=PBE(),
                    pseudopotentials=PseudoFamily("dojo.nc.sr.pbe.v0_4_1.standard.upf"))
 
-# Now comes the selection of the computational architecture.
+# Next is the selection of the computational architecture.
 # This effectively makes the choice, whether the computation will be run
-# on the CPU or on a GPU. For example
-
+# on the CPU or on a GPU.
+#
+# **Nvidia GPUs.**
+# Supported via [CUDA.jl](https://github.com/JuliaGPU/CUDA.jl).
+# Right now `Libxc` only supports CUDA 11,
+# so we need to explicitly request the 11.8 CUDA runtime:
 using CUDA
+CUDA.set_runtime_version!(v"11.8")  # Note: This requires a restart of Julia
 architecture = DFTK.GPU(CuArray)
 
-# selects the computation to run on an Nvidia GPU while
+# **AMD GPUs.** Supported via [AMDGPU.jl](https://github.com/JuliaGPU/AMDGPU.jl).
+# Here you need to install ROCm manually and afterwards you just need to:
 
 using AMDGPU
 architecture = DFTK.GPU(ROCArray)
 
-# would select to run on an AMD GPU.
-#
+# **Portable architecture selection.**
 # To make sure this script runs on the github CI (where we don't have GPUs
 # available) we check for the availability of GPUs before selecting an
 # architecture:
 
 architecture = has_cuda() ? DFTK.GPU(CuArray) : DFTK.CPU()
 
+# **Basis and SCF.**
 # Based on the `architecture` we construct a `PlaneWaveBasis` object:
 
 basis  = PlaneWaveBasis(model; Ecut=30, kgrid=(5, 5, 5), architecture)
