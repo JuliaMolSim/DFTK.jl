4a60db4a5512d4bc8aad342d91aaeb8b0c548af0
Michael F. Herbst <info@michael-herbst.com>
2025-03-15 22:25:25 +0100

    Extend magnetic terms

--- src/terms/magnetic.jl
+++ src/terms/magnetic.jl
@@ -8,38 +8,33 @@ struct Magnetic
     Afunction::Function  # A(x,y,z) returns [Ax,Ay,Az]
                          # both [x,y,z] and [Ax,Ay,Az] are in *Cartesian* coordinates
 end
-(M::Magnetic)(basis) = TermMagnetic(basis, M.Afunction)
+function (M::Magnetic)(basis)
+    Apotential = [zeros(T, basis.fft_size) for α = 1:3]
+    N1, N2, N3 = basis.fft_size
+    rvecs = collect(r_vectors_cart(basis))
+    for i = 1:N1, j = 1:N2, k = 1:N3
+        Apotential[1][i, j, k],
+        Apotential[2][i, j, k],
+        Apotential[3][i, j, k] = M.Afunction(rvecs[i, j, k])
+    end
+    TermMagnetic(Apotential)
+end
 
 struct MagneticFromValues
     # Apotential[α] is an array of size fft_size for α=1:3
     Apotential::Vector{<:AbstractArray}
 end
-function (M::MagneticFromValues)(Apotential)
-    @assert length(Apotential) == 3
-    @assert size(Apotential[1]) == basis.fft_size
-    @assert size(Apotential[2]) == basis.fft_size
-    @assert size(Apotential[3]) == basis.fft_size
-    TermMagnetic(basis, Apotential)
+function (M::MagneticFromValues)(basis::PlaneWaveBasis)
+    @assert length(M.Apotential) == 3
+    @assert size(M.Apotential[1]) == basis.fft_size
+    @assert size(M.Apotential[2]) == basis.fft_size
+    @assert size(M.Apotential[3]) == basis.fft_size
+    TermMagnetic(M.Apotential)
 end
 
 struct TermMagnetic <: Term
     # Apotential[α] is an array of size fft_size for α=1:3
     Apotential::Vector{<:AbstractArray}
-end
-function TermMagnetic(basis::PlaneWaveBasis{T}, Afunction::Function) where {T}
-    Apotential = [zeros(T, basis.fft_size) for α = 1:3]
-    N1, N2, N3 = basis.fft_size
-    rvecs = collect(r_vectors_cart(basis))
-    for i = 1:N1
-        for j = 1:N2
-            for k = 1:N3
-                Apotential[1][i, j, k],
-                Apotential[2][i, j, k],
-                Apotential[3][i, j, k] = Afunction(rvecs[i, j, k])
-            end
-        end
-    end
-    TermMagnetic(Apotential)
 end
 
 function ene_ops(term::TermMagnetic, basis::PlaneWaveBasis{T}, ψ, occupation;
