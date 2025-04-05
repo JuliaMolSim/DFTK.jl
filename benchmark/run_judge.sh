d82696def62d1ebeaa4979de86b8fbccf5c44d13
Michael F. Herbst <info@michael-herbst.com>
2025-04-05 10:36:58 +0200

    Updates

--- benchmark/run_judge.sh
+++ benchmark/run_judge.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-julia -e '
+#!/bin/bash
+THISDIR=$(dirname ${BASH_SOURCE[0]})
+julia --project="$THISDIR/.." -e '
     include("common.jl")
     baseline::String = length(ARGS) > 1 ? ARGS[1] : "master"
     target::String   = length(ARGS) > 2 ? ARGS[2] : nothing
