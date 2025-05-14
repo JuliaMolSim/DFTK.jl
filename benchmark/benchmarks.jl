c77d5de85b0ca49b1cb182a01b21921c6820a3e7
Michael F. Herbst <info@michael-herbst.com>
2025-05-14 11:16:17 +0200

    More fixes

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -2,19 +2,14 @@ using BenchmarkTools
 using DFTK
 setup_threading()
 
-macro include_case(SUITE,name)
+macro include_case(SUITE, name)
   quote
     include("cases/$($name).jl")
     SUITE["$($name)"] = $(Symbol(name)).SUITE
   end
 end
 
-# For inspiration see
-# https://github.com/gridap/Gridap.jl/blob/master/benchmark/benchmarks.jl
-# https://github.com/gridap/Gridap.jl/blob/master/benchmark/README.md
-
 const SUITE = BenchmarkGroup()
-
-@include_case "silicon"
-@include_case "aluminium_rattled"
-@include_case "SrVO3"
+@include_case SUITE "silicon"
+@include_case SUITE "aluminium_rattled"
+@include_case SUITE "SrVO3"
