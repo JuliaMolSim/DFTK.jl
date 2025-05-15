4e44e5b95e2155389b08cdd6eea12fab4c279335
Augustin Bussy <augustin.bussy@cscs.ch>
2025-05-15 12:42:04 +0200

    GPU optimization of LOBPCG (#1068)

---
+++ src/gpu/linalg.jl
@@ -0,0 +1,45 @@
+### GPU-specific implementations of functions called during LOBPCG
+# The massive parallelism of the GPU can only be fully exploited when
+# operating on whole arrays. For performance reasons, one should avoid
+# explicitly looping over columns or elements. This approach is not
+# necessarily the most performant on CPU, as the allocation of large
+# temporary arrays hurts cache locality. It is also harder to read.
+
+using LinearAlgebra
+using GPUArraysCore
+
+function compute_λ(X::AbstractGPUArray{T}, AX::AbstractGPUArray{T}, BX::AbstractGPUArray{T}) where {T}
+    num = sum(conj(X) .* AX, dims=1)
+    den = sum(conj(X) .* BX, dims=1)
+    vec(real.(num ./ den))
+end
+
+function columnwise_dots(A::AbstractGPUArray{T}, B::AbstractGPUArray{T}) where {T}
+    sum(conj(A) .* B; dims=1)
+end
+
+function columnwise_dots(A::AbstractGPUArray{T}, M, B::AbstractGPUArray{T}) where {T}
+    sum(conj(A) .* (M * B); dims=1)
+end
+
+function columnwise_dots(A::AbstractGPUArray{T}, D::Diagonal, B::AbstractGPUArray{T}) where {T}
+    sum(conj(A) .* (D.diag .* B); dims=1)
+end
+
+function ldiv!(Y::AbstractGPUArray{T}, P::PreconditionerTPA, R::AbstractGPUArray{T}) where {T}
+    if P.mean_kin === nothing
+        ldiv!(Y, Diagonal(P.kin .+ P.default_shift), R)
+    else
+        Y .= (P.mean_kin' ./ (P.mean_kin' .+ P.kin)) .* R
+    end
+    Y
+end
+
+function mul!(Y::AbstractGPUArray{T}, P::PreconditionerTPA, R::AbstractGPUArray{T}) where {T}
+    if P.mean_kin === nothing
+        mul!(Y, Diagonal(P.kin .+ P.default_shift), R)
+    else
+        Y .= ((P.mean_kin' .+ P.kin) ./ P.mean_kin') .* R
+    end
+    Y
+end
