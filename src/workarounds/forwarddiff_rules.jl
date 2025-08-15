f57d7bc2261055dcb8eb769d4b1e5efdac00a4b8
Shivang Sachar <143157099+shivang57721@users.noreply.github.com>
2025-08-15 14:54:41 +0200

    Removed extra line

--- src/workarounds/forwarddiff_rules.jl
+++ src/workarounds/forwarddiff_rules.jl
@@ -211,7 +211,6 @@ function self_consistent_field(basis_dual::PlaneWaveBasis{T};
         scfres.n_bands_converge, scfres.n_matvec, scfres.diagonalization, scfres.stage,
         scfres.history_Δρ, scfres.history_Etot, scfres.timedout, scfres.mixing,
         scfres.algorithm, scfres.runtime_ns)
-
 end
 
 function hankel(r::AbstractVector, r2_f::AbstractVector, l::Integer, p::TT) where {TT <: ForwardDiff.Dual}
