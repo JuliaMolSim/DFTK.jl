d17b9c97ee07af17ad7dc489af8539bec4051f97
Francesco Sicignano <francesco.sicignano@sns.it>
2025-09-15 10:48:11 +0200

    Function compute_pdos now fully working
    Key features: - Pdos can now be computed and plotted form the scfres
                  - New "atomic_orbital_projectors" function
                  - Atomic projectors and projections available
                  - New OrbitalManifold struct to select atomic orbitals
                  - Compatible with subsequent Hubbard update
                  - New ortho_lowdin function
                  - New pswfc_label function to get atomic orbital labels
    		from PspUpf data
                  - Updated example dos.jl to show the pdos
                  - Updated example collinear_magnetism.jl to show Fe d
    		pdos, new Psp includes atomic orbitals

--- examples/dos.jl
+++ examples/dos.jl
@@ -7,7 +7,7 @@ using Unitful
 using Plots
 using PseudoPotentialData
 
-## Define the geometry and pseudopotential
+# Define the geometry and pseudopotential
 a = 10.26  # Silicon lattice constant in Bohr
 lattice = a / 2 * [[0 1 1.0];
                    [1 0 1.0];
@@ -16,16 +16,17 @@ Si = ElementPsp(:Si, PseudoFamily("dojo.nc.sr.lda.v0_4_1.standard.upf"))
 atoms = [Si, Si]
 positions = [ones(3) / 8, -ones(3) / 8]
 
-## Run SCF
+# Run SCF
 model = model_LDA(lattice, atoms, positions; temperature=5e-3)
 basis = PlaneWaveBasis(model; Ecut=15, kgrid=[4, 4, 4], symmetries_respect_rgrid=true)
 scfres = self_consistent_field(basis, tol=1e-8)
 
-## Plot the DOS
+# Plot the DOS
 plot_dos(scfres)
 
-## Plot the local DOS along one direction
+# Plot the local DOS along one direction
 plot_ldos(scfres; n_points=100, ldos_xyz=[:, 10, 10])
 
-## Plot the projected DOS
-plot_pdos(scfres; εrange=(-0.3, 0.5))
+# Plot the projected DOS
+p = plot_pdos(scfres; iatom=1, label="3S", εrange=(-0.3, 0.5))
+plot_pdos(scfres; p, colors=[:red], iatom=1, label="3P", εrange=(-0.3, 0.5))
