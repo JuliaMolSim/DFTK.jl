4a1d0e29d8ee7f693bad5a09c59af3a5a65d69fe
Francesco Sicignano <francesco.sicignano@sns.it>
2025-09-25 17:11:39 +0200

    Hubbard test now functional

---
+++ test/hubbard.jl
@@ -0,0 +1,45 @@
+@testitem "Nickel Oxide" setup=[TestCases] begin
+   using DFTK
+   using PseudoPotentialData
+   using Unitful
+   using UnitfulAtomic
+   
+   # Hubbard parameters
+   U        = 10
+   manifold = DFTK.OrbitalManifold(;species=:Ni, label="3D")
+   
+   a = 7.9  # Bohr
+   lattice = a * [[ 1.0  0.5  0.5];
+                  [ 0.5  1.0  0.5];
+                  [ 0.5  0.5  1.0]]
+   pseudopotentials = PseudoFamily("dojo.nc.sr.pbe.v0_4_1.standard.upf")
+   Ni = ElementPsp(:Ni, pseudopotentials)
+   O = ElementPsp(:O, pseudopotentials)
+   atoms = [Ni, O, Ni, O]
+   positions = [[0.0, 0.0, 0.0],
+                [0.25, 0.25, 0.25],
+                [0.5, 0.5, 0.5],
+                [0.75, 0.75, 0.75]]     
+   magnetic_moments = [2, 0, -1, 0]
+   
+   ψ=nothing
+   occupation=nothing
+   model = model_DFT(lattice, atoms, positions; 
+                     extra_terms=[DFTK.Hubbard(manifold, U)],
+                     temperature=0.01, functionals=PBE(),
+                     smearing=DFTK.Smearing.Gaussian(), magnetic_moments=magnetic_moments)
+   basis = PlaneWaveBasis(model; Ecut = 15, kgrid = [2, 2, 2])
+   ρ0 = guess_density(basis, magnetic_moments)
+   scfres = self_consistent_field(basis; tol=1e-10, ρ=ρ0)
+   
+   ref = -354.907446880021
+   e_total = scfres.energies.total
+   @test abs(e_total - ref) < 1e-8
+   for (term, value) in scfres.energies
+       if term == "Hubbard"
+          ref = 0.17629078433258719
+          e_hub = value
+          @test abs(e_hub - ref) < 1e-8
+       end
+   end  
+end
