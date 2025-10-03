c9d8328338ef89766967ebb2efef09f9adb52722
Francesco Sicignano <francesco.sicignano@sns.it>
2025-10-03 12:02:04 +0200

    use_nlcc=false for all mgga tests

--- test/hamiltonian_consistency.jl
+++ test/hamiltonian_consistency.jl
@@ -99,10 +99,10 @@ end
     test_consistency_term(Xc([:lda_xc_teter93]))
     test_consistency_term(Xc([:lda_xc_teter93]), spin_polarization=:collinear)
     test_consistency_term(Xc([:gga_x_pbe]), spin_polarization=:collinear)
-    test_consistency_term(Xc([:mgga_x_tpss]))
-    test_consistency_term(Xc([:mgga_x_scan]))
+    test_consistency_term(Xc([:mgga_x_tpss]; use_nlcc=false))
+    test_consistency_term(Xc([:mgga_x_scan]; use_nlcc=false))
     test_consistency_term(Xc([:mgga_c_scan]; use_nlcc=false), spin_polarization=:collinear)
-    test_consistency_term(Xc([:mgga_x_b00]))
+    test_consistency_term(Xc([:mgga_x_b00]; use_nlcc=false))
     test_consistency_term(Xc([:mgga_c_b94]; use_nlcc=false), spin_polarization=:collinear)
 
     let
