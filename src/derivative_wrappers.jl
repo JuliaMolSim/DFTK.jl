801b53d3445bf9c1eb496d434eefda80ccfca170
Augustin Bussy <augustin.bussy@cscs.ch>
2025-10-27 16:18:53 +0100

    Explicitly call ForwardDiff.Tag()

--- src/derivative_wrappers.jl
+++ src/derivative_wrappers.jl
@@ -12,17 +12,20 @@ using ForwardDiff
 
 struct DerivativeTag end
 function tagged_derivative(f, x::T; custom_tag=DerivativeTag) where T
-    TagType = ForwardDiff.Tag{custom_tag, T}
+    # explicit call to ForwardDiff.Tag() to trigger ForwardDiff.tagcount
+    TagType = typeof(ForwardDiff.Tag(custom_tag(), T))
     x_dual = ForwardDiff.Dual{TagType, T, 1}(x, ForwardDiff.Partials((one(T),)))
 
-    return ForwardDiff.extract_derivative(TagType, f(x_dual))
+    res = ForwardDiff.extract_derivative(TagType, f(x_dual))
+    return res
 end
 
 struct GradientTag end
 GradientConfig(f, x, ::Type{Tag}) where {Tag} = 
     ForwardDiff.GradientConfig(f, x, ForwardDiff.Chunk(x), Tag())
 function tagged_gradient(f, x::AbstractArray{T}; custom_tag=GradientTag) where T
-    TagType = ForwardDiff.Tag{custom_tag, T}
+    # explicit call to ForwardDiff.Tag() to trigger ForwardDiff.tagcount
+    TagType = typeof(ForwardDiff.Tag(custom_tag(), T)) 
     
     cfg = GradientConfig(f, x, TagType) 
     ForwardDiff.gradient(f, x, cfg, Val{false}()) 
@@ -32,7 +35,8 @@ struct JacobianTag end
 JacobianConfig(f, x, ::Type{Tag}) where {Tag} = 
     ForwardDiff.JacobianConfig(f, x, ForwardDiff.Chunk(x), Tag())
 function tagged_jacobian(f, x::AbstractArray{T}; custom_tag=JacobianTag) where T
-    TagType = ForwardDiff.Tag{custom_tag, T}
+    # explicit call to ForwardDiff.Tag() to trigger ForwardDiff.tagcount
+    TagType = typeof(ForwardDiff.Tag(custom_tag(), T))
     
     cfg = JacobianConfig(f, x, TagType) 
     ForwardDiff.jacobian(f, x, cfg, Val{false}()) 
