3c611b1cbcdd606fb43c14dfe07d59f460e5597f
Michael F. Herbst <info@michael-herbst.com>
2025-10-29 08:54:10 +0100

    Cosmetic fixes to example

--- examples/hubbard.jl
+++ examples/hubbard.jl
@@ -15,15 +15,14 @@ lattice = a * [[ 1.0  0.5  0.5];
                [ 0.5  0.5  1.0]]
 pseudopotentials = PseudoFamily("dojo.nc.sr.pbe.v0_4_1.standard.upf")
 Ni = ElementPsp(:Ni, pseudopotentials)
-O = ElementPsp(:O, pseudopotentials)
+O  = ElementPsp(:O, pseudopotentials)
 atoms = [Ni, O, Ni, O]
 positions = [zeros(3), ones(3) / 4, ones(3) / 2, ones(3) * 3 / 4]
 magnetic_moments = [2, 0, -1, 0]
  
 # First, we run an SCF and band computation without the Hubbard term
-model = model_DFT(lattice, atoms, positions; temperature=5e-3,
-                  functionals=PBE(), magnetic_moments=magnetic_moments)
-basis = PlaneWaveBasis(model; Ecut=32, kgrid=[2, 2, 2] )
+model = model_DFT(lattice, atoms, positions; temperature=5e-3, functionals=PBE(), magnetic_moments)
+basis = PlaneWaveBasis(model; Ecut=32, kgrid=[2, 2, 2])
 scfres = self_consistent_field(basis; tol=1e-10, ρ=guess_density(basis, magnetic_moments))
 bands = compute_bands(scfres, MonkhorstPack(4, 4, 4))
 lowest_unocc_band = findfirst(ε -> ε-bands.εF > 0, bands.eigenvalues[1])
@@ -38,10 +37,10 @@ plot_pdos(bands; p, iatom=1, label="3D", colors=[:yellow, :orange], εrange)
 
 # To perform and Hubbard computation, we have to define the Hubbard manifold and associated constant
 U = 10u"eV"
-manifold = OrbitalManifold(;species=:Ni, label="3D")
+manifold = OrbitalManifold(; species=:Ni, label="3D")
 
-# Run SCF
-model = model_DFT(lattice, atoms, positions; extra_terms=[DFTK.Hubbard(manifold, U)],
+# Run SCF with a DFT+U setup, notice the `extra_terms` keyword argument, setting up the Hubbard +U term.
+model = model_DFT(lattice, atoms, positions; extra_terms=[Hubbard(manifold, U)],
                   functionals=PBE(), temperature=5e-3, magnetic_moments)
 basis = PlaneWaveBasis(model; Ecut=32, kgrid=[2, 2, 2] )
 scfres = self_consistent_field(basis; tol=1e-10, ρ=guess_density(basis, magnetic_moments))
@@ -57,4 +56,3 @@ band_gap = bands_hub.eigenvalues[1][lowest_unocc_band] - bands_hub.eigenvalues[1
 εrange = (εF - austrip(width), εF + austrip(width))
 p = plot_dos(bands_hub; p, colors=[:blue, :blue], εrange)
 plot_pdos(bands_hub; p, iatom=1, label="3D", colors=[:green, :purple], εrange)
-
