fe207dab6756eabd00872457d3518b733fa0633b
Bruno Ploumhans <13494793+Technici4n@users.noreply.github.com>
2025-12-03 11:44:36 +0100

    Clean up DFTKAMDGPUExt.jl

--- ext/DFTKAMDGPUExt.jl
+++ ext/DFTKAMDGPUExt.jl
@@ -4,8 +4,6 @@ using PrecompileTools
 using LinearAlgebra
 import DFTK: GPU, precompilation_workflow
 using DFTK
-import ForwardDiff
-import ForwardDiff: Dual
 
 DFTK.synchronize_device(::GPU{<:AMDGPU.ROCArray}) = AMDGPU.synchronize()
 
@@ -41,4 +39,5 @@ if AMDGPU.functional()
         end
     end
 end
+
 end
