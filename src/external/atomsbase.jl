69950d889ea1d6c8a2e8dc4e4a339c85ccfc7c7d
Nathanael Bosch <nathanael.bosch@epfl.ch>
2025-12-16 09:42:51 +0100

    Enable `parse_system` for systems with `Dual` positions

--- src/external/atomsbase.jl
+++ src/external/atomsbase.jl
@@ -17,7 +17,9 @@ function parse_system(system::AbstractSystem{D},
 
     # Parse abstract system and return data required to construct model
     mtx = austrip.(stack(cell_vectors(system)))
-    T = eltype(mtx)
+    lattice_eltype = eltype(mtx)
+    pos_eltype = promote_type(eltype.((austrip.(atom.position) for atom in system))...)
+    T = promote_type(lattice_eltype, pos_eltype)
     lattice = zeros(T, 3, 3)
     lattice[1:D, 1:D] .= mtx
 
