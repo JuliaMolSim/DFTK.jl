3c377d99b50647642e1e048bf32260799375ebdb
Antoine Levitt <antoine.levitt@gmail.com>
2026-01-14 20:59:25 +0100

    Refactor from claude

--- examples/divAgrad_solver.jl
+++ examples/divAgrad_solver.jl
@@ -12,371 +12,178 @@ using LinearAlgebra
 using LinearMaps
 using Plots
 
-#
-# Define the term for the div(a(x)∇) operator
-#
-
-"""
-Term for -½∇⋅(A∇) operator where A is constructed from the coefficient field a(x).
-Note: DivAgradOperator implements -½∇⋅(A∇), so we need A = 2a to get -∇⋅(a∇).
-"""
+# Term for -∇⋅(a(x)∇) operator
 struct TermDivAGrad{AT} <: DFTK.TermLinear
-    A_values::AT  # The coefficient field A(x) = 2a(x) on the real-space grid
+    A_values::AT  # Coefficient field a(x) on real-space grid
 end
 
 @DFTK.timing "ene_ops: divAgrad" function DFTK.ene_ops(term::TermDivAGrad,
                                                         basis::DFTK.PlaneWaveBasis{T}, 
-                                                        ψ, occupation;
-                                                        kwargs...) where {T}
-    ops = [DFTK.DivAgradOperator(basis, kpt, term.A_values ./ 2) for kpt in basis.kpoints]
-    
-    # For a linear problem, we don't need to compute the energy during the solve
-    E = T(Inf)
-    
-    (; E, ops)
+                                                        ψ, occupation; kwargs...) where {T}
+    # DivAgradOperator implements -½∇⋅(A∇), so use A = 2a to get -∇⋅(a∇)
+    ops = [DFTK.DivAgradOperator(basis, kpt, 2 .* term.A_values) for kpt in basis.kpoints]
+    (; E=T(Inf), ops)
 end
 
-#
-# Helper function to build coefficient field a(x) from inclusions
-#
-
-"""
-Build the full coefficient field a(x) from elementary inclusions at given positions.
-Handles periodic images (one cell away) in real space.
-
-# Arguments
-- `basis`: PlaneWaveBasis
-- `inclusion_func`: Function that takes a position vector r::Vec3 and returns the inclusion value
-- `background_value`: Background constant value of a(x)
-- `positions`: List of inclusion center positions
-- `use_periodic_images`: Whether to sum over periodic images (default: true)
-"""
+# Build coefficient field a(x) from inclusions in real space
 function build_coefficient_field_real(basis, inclusion_func, background_value, positions)
     T = real(eltype(basis))
     a_values = fill(T(background_value), basis.fft_size...)
-    
-    # Get lattice vectors
     lattice = basis.model.lattice
     
-    # Iterate over all real-space points
     for (i, r) in enumerate(DFTK.r_vectors_cart(basis))
         inclusion_value = zero(T)
-        
-        for pos in positions
-            # Consider the main cell and neighboring cells
-            for i1 in -1:1, i2 in -1:1, i3 in -1:1
-                offset = lattice * [i1, i2, i3]
-                r_relative = r - (pos + offset)
-                inclusion_value += inclusion_func(r_relative)
-            end
+        for pos in positions, i1 in -1:1, i2 in -1:1, i3 in -1:1
+            offset = lattice * [i1, i2, i3]
+            inclusion_value += inclusion_func(r - (pos + offset))
         end
-        
         a_values[i] += inclusion_value
     end
-    
     a_values
 end
 
-"""
-Build the full coefficient field a(x) from Fourier-space inclusion functions.
-
-# Arguments
-- `basis`: PlaneWaveBasis
-- `inclusion_fourier_func`: Function that takes a G vector (Cartesian) and returns the Fourier coefficient
-- `background_value`: Background constant value of a(x)
-- `positions`: List of inclusion center positions
-"""
+# Build coefficient field a(x) from inclusions in Fourier space
 function build_coefficient_field_fourier(basis, inclusion_fourier_func, background_value, positions)
     T = real(eltype(basis))
-    
-    # Build in Fourier space
-    Gs = DFTK.G_vectors(basis)
     a_fourier = zeros(Complex{T}, basis.fft_size...)
     
-    for (iG, G) in enumerate(Gs)
+    for (iG, G) in enumerate(DFTK.G_vectors(basis))
         G_cart = basis.model.recip_lattice * G
-        
-        # Structure factor: sum over positions
-        value = zero(Complex{T})
-        if iG == 1
-            value += background_value * basis.model.unit_cell_volume
-        end
+        value = iG == 1 ? background_value * basis.model.unit_cell_volume : zero(Complex{T})
         for pos in positions
             value += DFTK.cis(-dot(G_cart, pos)) * inclusion_fourier_func(G_cart)
         end
         a_fourier[iG] = value / sqrt(basis.model.unit_cell_volume)
     end
     
-    # Transform to real space
     DFTK.enforce_real!(a_fourier, basis)
     DFTK.irfft(basis, a_fourier)
 end
 
-#
-# Constructors for TermDivAGrad from different representations
-#
-
-"""
-Construct TermDivAGrad from a real-space inclusion function.
-
-# Arguments
-- `inclusion_real`: Function r::Vec3 -> value, defining the inclusion in real space
-- `background_value`: Background constant value
-- `positions`: List of inclusion center positions
-"""
+# Constructors for TermDivAGrad from real or Fourier space
 struct DivAGradFromReal
     inclusion_real::Function
     background_value::Any
     positions::Vector
 end
+(divAgrad::DivAGradFromReal)(basis::DFTK.PlaneWaveBasis) =
+    TermDivAGrad(build_coefficient_field_real(basis, divAgrad.inclusion_real, 
+                                              divAgrad.background_value, divAgrad.positions))
 
-function (divAgrad::DivAGradFromReal)(basis::DFTK.PlaneWaveBasis{T}) where {T}
-    a_values = build_coefficient_field_real(basis, divAgrad.inclusion_real, 
-                                             divAgrad.background_value, divAgrad.positions)
-    TermDivAGrad(a_values)
-end
-
-"""
-Construct TermDivAGrad from a Fourier-space inclusion function.
-
-# Arguments
-- `inclusion_fourier`: Function G_cart::Vec3 -> value, defining the inclusion Fourier transform
-- `background_value`: Background constant value
-- `positions`: List of inclusion center positions
-"""
 struct DivAGradFromFourier
     inclusion_fourier::Function
     background_value::Any
     positions::Vector
 end
+(divAgrad::DivAGradFromFourier)(basis::DFTK.PlaneWaveBasis) =
+    TermDivAGrad(build_coefficient_field_fourier(basis, divAgrad.inclusion_fourier,
+                                                 divAgrad.background_value, divAgrad.positions))
 
-function (divAgrad::DivAGradFromFourier)(basis::DFTK.PlaneWaveBasis{T}) where {T}
-    a_values = build_coefficient_field_fourier(basis, divAgrad.inclusion_fourier,
-                                                divAgrad.background_value, divAgrad.positions)
-    TermDivAGrad(a_values)
-end
-
-#
-# Preconditioners
-#
-
-# Custom preconditioner type that implements pseudo-inverse (zeroing DC component)
+# Pseudo-inverse preconditioner (zero DC component)
 struct PseudoInversePreconditioner{T}
     diag::Vector{T}
     zero_idx::Int
 end
-
 function LinearAlgebra.ldiv!(y, P::PseudoInversePreconditioner, x)
-    # Pseudo-inverse: invert all diagonal elements except the DC component
     for i in eachindex(y)
-        if i == P.zero_idx
-            y[i] = 0  # Zero out the DC component (pseudo-inverse)
-        else
-            y[i] = x[i] / (P.diag[i])
-        end
+        y[i] = i == P.zero_idx ? 0 : x[i] / P.diag[i]
     end
-    return y
+    y
 end
 
-#
-# Solver function for the linear problem -div(a∇u) = f
-#
-
-"""
-Solve the linear PDE problem -div(a(x)∇u(x)) = f(x) using CG iteration.
-
-# Arguments
-- `basis`: PlaneWaveBasis for the problem
-- `f`: Right-hand side function values on the real-space grid
-
-# Returns
-- `u`: Solution on the real-space grid
-- `info`: Information from the CG solver
-"""
+# Solve -div(a(x)∇u(x)) = f(x) using CG iteration
 function solve_linear_problem(basis, f; tol=1e-6, maxiter=1000)
-    # Convert f to Fourier space and create right-hand side
-    # We solve for the first k-point and first band (single equation)
     kpt = only(basis.kpoints)
+    T = real(eltype(basis))
     
-    # Get the Hamiltonian (which represents our -div(a∇) operator)
-    # We pass a dummy ψ and occupation to construct the Hamiltonian
+    # Build Hamiltonian (represents -div(a∇) operator)
     ψ_dummy = [DFTK.random_orbitals(basis, kpt, 1) for kpt in basis.kpoints]
     occupation_dummy = [fill(1.0, 1) for _ in basis.kpoints]
+    hamk = DFTK.energy_hamiltonian(basis, ψ_dummy, occupation_dummy).ham.blocks[1]
     
-    eh = DFTK.energy_hamiltonian(basis, ψ_dummy, occupation_dummy)
-    ham = eh.ham
-    
-    # Get Hamiltonian block for first k-point
-    hamk = ham.blocks[1]
-    
-    # Setup the linear map for CG
+    # Linear map for CG
     n_G = length(DFTK.G_vectors(basis, kpt))
-    function apply_H(x)
-        result = similar(x)
-        LinearAlgebra.mul!(result, hamk, x)
-        return result
-    end
+    H_map = LinearMap{Complex{T}}((y, x) -> LinearAlgebra.mul!(y, hamk, x), 
+                                   n_G, n_G; ishermitian=true, isposdef=false)
     
-    T = real(eltype(basis))
-    H_map = LinearMap{Complex{T}}(apply_H, n_G, n_G; ishermitian=true, isposdef=false)
-    
-    # Find the index of G=0 for the pseudo-inverse
+    # Preconditioner and projection (zero DC component)
     G_zero_idx = findfirst(G -> all(iszero, G), DFTK.G_vectors(basis, kpt))
     @assert !isnothing(G_zero_idx)
-    
-    # Simple diagonal preconditioner based on kinetic energy
     kin_energies = [T(DFTK.norm2(kpt.coordinate + G) / 2) for G in DFTK.G_vectors_cart(basis, kpt)]
     P = PseudoInversePreconditioner(kin_energies, G_zero_idx)
+    proj(x) = (y = copy(x); y[G_zero_idx] = 0; y)
     
-    # Initial guess (zero)
+    # Solve in Fourier space
+    b = proj(DFTK.fft(basis, kpt, f))
     u_fourier = zeros(Complex{T}, n_G)
+    info = DFTK.cg!(u_fourier, H_map, b; precon=P, proj, tol, maxiter)
     
-    # Right-hand side in Fourier space
-    b = DFTK.fft(basis, kpt, f)
-    
-    # Projection operator to enforce zero average
-    function proj(x)
-        # Remove the zero Fourier mode (constant component)
-        x_copy = copy(x)
-        x_copy[G_zero_idx] = 0
-        return x_copy
-    end
-    
-    # Also project b
-    b = proj(b)
-    
-    # Solve using CG
-    info = DFTK.cg!(u_fourier, H_map, b; precon=P, proj=proj, tol=tol, maxiter=maxiter)
-    
-    # Convert solution back to real space
-    u = DFTK.ifft(basis, kpt, u_fourier)
-    
-    return u, info
+    DFTK.ifft(basis, kpt, u_fourier), info
 end
 
-#
-# Example: Solve a sample problem with rectangular inclusions
-#
+# Rectangular inclusion functions
+rectangular_inclusion_real(wx, wy, wz, a_inc) = 
+    r -> (abs(r[1]) <= wx && abs(r[2]) <= wy && abs(r[3]) <= wz) ? a_inc : 0.0
 
-# Setup a 2D lattice
-a = 10.0  # Box size
-lattice = a .* [[1 0 0.]; [0 1 0]; [0 0 0]]  # 2D system
-
-# Define rectangular inclusion function
-# Rectangle with half-widths (wx, wy, wz) and value a_inc
-function rectangular_inclusion_real(wx, wy, wz, a_inc)
-    return function(r)
-        if abs(r[1]) <= wx && abs(r[2]) <= wy && abs(r[3]) <= wz
-            return a_inc
-        else
-            return 0.0
-        end
-    end
+rectangular_inclusion_fourier(wx, wy, wz, a_inc) = function(G)
+    T = eltype(G)
+    my_sinc(x) = abs(x) < 1e-10 ? one(T) : sin(x) / x
+    8 * wx * wy * wz * a_inc * my_sinc(G[1] * wx) * my_sinc(G[2] * wy) * my_sinc(G[3] * wz)
 end
 
-# Fourier transform of rectangular inclusion
-# FT[χ_rect](G) = a_inc * (2wx)(2wy)(2wz) * sinc(G_x*wx)*sinc(G_y*wy)*sinc(G_z*wz)
-function rectangular_inclusion_fourier(wx, wy, wz, a_inc)
-    return function(G)
-        T = eltype(G)
-        volume = 8 * wx * wy * wz
-        
-        # sinc(x) = sin(πx)/(πx), but we need sin(x)/x
-        function my_sinc(x)
-            return abs(x) < 1e-10 ? one(T) : sin(x) / x
-        end
-
-        return a_inc * volume * my_sinc(G[1] * wx) * my_sinc(G[2] * wy) * my_sinc(G[3] * wz)
-    end
-end
-
-# Inclusion parameters
-wx, wy, wz = 1, 1, 1.0  # Half-widths (cartesian coordinates)
-a_inc = 3.0  # Inclusion value
-
-# Positions of inclusions (relative coordinates)
-positions = [a .* [0.5, 0.5, 0.0],]
-
-# Background value of a(x)
+# Setup problem: 2D lattice with rectangular inclusion
+a = 10.0
+lattice = a .* [[1 0 0.]; [0 1 0]; [0 0 0]]
+wx, wy, wz = 1, 1, 1.0
+a_inc = 3.0
+positions = [a .* [0.5, 0.5, 0.0]]
 background_a = 1.0
 
-terms_real = [DivAGradFromReal(rectangular_inclusion_real(wx, wy, wz, a_inc),
-                                background_a, positions)]
-terms_fourier = [DivAGradFromFourier(rectangular_inclusion_fourier(wx, wy, wz, a_inc),
-                                      background_a, positions)]
-terms = terms_fourier
-model = DFTK.Model(lattice; n_electrons=0, terms,
-                        spin_polarization=:spinless)
-Ecut = 200
-basis = DFTK.PlaneWaveBasis(model; Ecut, kgrid=(1, 1, 1))
+terms = [DivAGradFromFourier(rectangular_inclusion_fourier(wx, wy, wz, a_inc),
+                              background_a, positions)]
+model = DFTK.Model(lattice; n_electrons=0, terms, spin_polarization=:spinless)
+basis = DFTK.PlaneWaveBasis(model; Ecut=200, kgrid=(1, 1, 1))
 
+# Build RHS: f(x) = da/dx
 kpt = only(basis.kpoints)
-a_real = (basis.terms[1].A_values)
-a_fourier_kpt = fft(basis, kpt, complex.(a_real)) # exact fourier coefficients
+a_real = basis.terms[1].A_values
+a_fourier = fft(basis, kpt, complex.(a_real))
 
-# Derivative: multiply by i*G_x in Fourier space
-f = zeros(ComplexF64, length(kpt.G_vectors))
-for (iG, G) in enumerate(DFTK.G_vectors(basis, kpt))
-    G_cart = basis.model.recip_lattice * G
-    f[iG] = 2π * im * G_cart[1] * a_fourier_kpt[iG]
-end
-
-# Transform to real space
+# Compute ∂a/∂x in Fourier space
+f = [2π * im * (basis.model.recip_lattice * G)[1] * a_fourier[iG]
+     for (iG, G) in enumerate(DFTK.G_vectors(basis, kpt))]
 f_values = DFTK.ifft(basis, kpt, f)
+f_values .-= sum(f_values) / length(f_values)  # Ensure zero average
 
-# Ensure zero average (should already be satisfied, but enforce numerically)
-f_values .-= sum(f_values) / length(f_values)
-
-# Solve the problem
+# Solve and compute derivatives
 println("\n=== Solving -div(a(x)∇u(x)) = f(x) ===")
 u, info = solve_linear_problem(basis, f_values; tol=1e-6, maxiter=1000)
 println("CG converged: $(info.converged) after $(info.n_iter) iterations")
 println("Final residual: $(info.residual_norm)")
 
-# Compute derivatives of u for visualization
-u_coeff = DFTK.fft(basis, kpt, u)
+# Compute ∂u/∂x and ∂u/∂y
+u_fourier = DFTK.fft(basis, kpt, u)
+compute_derivative(u_f, dir) = DFTK.ifft(basis, kpt,
+    [2π * im * (basis.model.recip_lattice * G)[dir] * u_f[iG]
+     for (iG, G) in enumerate(DFTK.G_vectors(basis, kpt))])
+dudx = compute_derivative(u_fourier, 1)
+dudy = compute_derivative(u_fourier, 2)
 
-# du/dx
-dudx = zeros(ComplexF64, length(kpt.G_vectors))
-for (iG, G) in enumerate(DFTK.G_vectors(basis, kpt))
-    G_cart = basis.model.recip_lattice * G
-    dudx[iG] = 2π * im * G_cart[1] * u_coeff[iG]
-end
-dudx = DFTK.ifft(basis, kpt, dudx)
-
-# du/dy
-dudy = zeros(ComplexF64, length(kpt.G_vectors))
-for (iG, G) in enumerate(DFTK.G_vectors(basis, kpt))
-    G_cart = basis.model.recip_lattice * G
-    dudy[iG] = 2π * im * G_cart[2] * u_coeff[iG]
-end
-dudy = DFTK.ifft(basis, kpt, dudy)
-
-# Visualize the results
-r_vectors = DFTK.r_vectors_cart(basis)
-x_coords = [r[1] for r in r_vectors]
-y_coords = [r[2] for r in r_vectors]
-
-# Reshape for plotting (2D)
+# Visualize results
 nx, ny, nz = basis.fft_size
-X = reshape(x_coords, nx, ny, nz)[:, :, 1]
-Y = reshape(y_coords, nx, ny, nz)[:, :, 1]
-A = reshape(real.(a_real), nx, ny, nz)[:, :, 1]
-F = reshape(real.(f_values), nx, ny, nz)[:, :, 1]
-U = reshape(real.(u), nx, ny, nz)[:, :, 1]
-DUDx = reshape(real.(dudx), nx, ny, nz)[:, :, 1]
-DUDy = reshape(real.(dudy), nx, ny, nz)[:, :, 1]
+reshape_2d(v) = reshape(real.(v), nx, ny, nz)[:, :, 1]
 
-# Create plots
-p1 = heatmap(X[:, 1], Y[1, :], A', title="Coefficient a(x)", 
-             xlabel="x", ylabel="y", c=:viridis)
-p2 = heatmap(X[:, 1], Y[1, :], F', title="RHS f(x) = da/dx", 
-             xlabel="x", ylabel="y", c=:RdBu)
-p3 = heatmap(X[:, 1], Y[1, :], U', title="Solution u(x)", 
-             xlabel="x", ylabel="y", c=:plasma)
-p4 = heatmap(X[:, 1], Y[1, :], DUDx', title="du/dx", 
-             xlabel="x", ylabel="y", c=:viridis)
-p5 = heatmap(X[:, 1], Y[1, :], DUDy', title="du/dy", 
-             xlabel="x", ylabel="y", c=:viridis)
+r_vectors = DFTK.r_vectors_cart(basis)
+X = reshape([r[1] for r in r_vectors], nx, ny, nz)[:, :, 1]
+Y = reshape([r[2] for r in r_vectors], nx, ny, nz)[:, :, 1]
 
-p = plot(p1, p2, p3, p4, p5, layout=(2, 3), size=(1800, 1200))
+A, F, U = reshape_2d.((a_real, f_values, u))
+DUDx, DUDy = reshape_2d.((dudx, dudy))
+
+p1 = heatmap(X[:, 1], Y[1, :], A', title="a(x)", xlabel="x", ylabel="y", c=:viridis)
+p2 = heatmap(X[:, 1], Y[1, :], F', title="f(x) = ∂a/∂x", xlabel="x", ylabel="y", c=:RdBu)
+p3 = heatmap(X[:, 1], Y[1, :], U', title="u(x)", xlabel="x", ylabel="y", c=:plasma)
+p4 = heatmap(X[:, 1], Y[1, :], DUDx', title="∂u/∂x", xlabel="x", ylabel="y", c=:viridis)
+p5 = heatmap(X[:, 1], Y[1, :], DUDy', title="∂u/∂y", xlabel="x", ylabel="y", c=:viridis)
+
+plot(p1, p2, p3, p4, p5, layout=(2, 3), size=(1800, 1200))
