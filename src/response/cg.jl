43bcb4e208027f493239ff322eb353ce4985b232
Augustin Bussy <augustin.bussy@cscs.ch>
2026-01-27 18:02:19 +0100

    Basic locking in CG solver

--- src/response/cg.jl
+++ src/response/cg.jl
@@ -71,7 +71,7 @@ cg(A::AbstractMatrix, b::AbstractVector; kwargs...) = cg(LinearMap(A), b; kwargs
 # TEST TODO: generalize to a single cg! implementation. Prob need a new struct for A
 function cg!(x::AbstractArray{T}, A!, b::AbstractArray{T};
              precon=I, proj! =identity, callback=identity,
-             tol=1e-10, maxiter=100, miniter=1) where {T}
+             tol=1e-10*ones(T, size(b, 2)), maxiter=100, miniter=1) where {T}
 
     # initialisation
     # r = b - Ax is the residual
@@ -92,22 +92,53 @@ function cg!(x::AbstractArray{T}, A!, b::AbstractArray{T};
     # p is the descent direction
     p = copy(c)
     n_iter = 0
-    residual_norm = to_cpu(columnwise_norms(r)) #TODO: rename residual_norms consistently
+    residual_norms = columnwise_norms(r)
+    # move tols to GPU for efficiency
+    col_tols = oftype(residual_norms, tol)
 
     # convergence history
     converged = false
 
+    # Keep track of full arrays to enable locking of converged columns
+    full_x = x
+    full_r = r
+    full_c = c
+    full_p = p
+    full_b = b
+    full_buff = proj_buffer
+    full_γ = γ
+    full_residuals = residual_norms
+
     # preconditioned conjugate gradient
     while n_iter < maxiter
         # output
-        info = (; A!, b, n_iter, x, r, residual_norm, converged, stage=:iterate)
+        info = (; A!, full_b, n_iter, full_x, full_r, full_residuals, converged, stage=:iterate)
         callback(info)
         n_iter += 1
-        if (n_iter ≥ miniter) && all(residual_norm .≤ tol)
+        converged_cols = full_residuals .<= col_tols
+        if (n_iter ≥ miniter) && all(converged_cols)
             converged = true
             break
         end
-        A!(c, p)
+
+        # Lock columns that are already converged. Because we can only take views with
+        # contiguous ranges in GPU arrays, we lock the first and last columns.
+        locked_lb = findfirst(!, converged_cols)
+        locked_ub = findlast(!, converged_cols)
+        active = locked_lb : locked_ub
+
+        @views begin
+            x = full_x[:, active]
+            r = full_r[:, active]
+            c = full_c[:, active]
+            p = full_p[:, active]
+            b = full_b[:, active]
+            proj_buffer = full_buff[:, active]
+            γ = full_γ[active]
+            residual_norms = full_residuals[active]
+        end
+
+        A!(c, p; active)
         α = γ ./ columnwise_dots(p, c)
 
         # update iterate and residual while ensuring they stay in Ran(proj)
@@ -115,16 +146,18 @@ function cg!(x::AbstractArray{T}, A!, b::AbstractArray{T};
         proj!(x, proj_buffer)
         proj_buffer .= r .- c .* α'
         proj!(r, proj_buffer)
-        residual_norm = to_cpu(columnwise_norms(r))
+        residual_norms .= columnwise_norms(r)
 
-        # apply preconditioner and prepare next iteration
-        ldiv!(c, precon, r)
-        γ_prev, γ = γ, columnwise_dots(r, c)
+        # apply preconditioner and prepare next iteration. Preconditioner applied to full arrays,
+        # because the FunctionPreconditioner type makes it hard to change the active set (cheap enough).
+        ldiv!(full_c, precon, full_r)
+        γ_prev = copy(γ)
+        γ .= columnwise_dots(r, c)
         β = γ ./ γ_prev
         proj_buffer .= c .+ p .* β'
         proj!(p, proj_buffer)
     end
-    info = (; x, converged, tol, residual_norm, n_iter, maxiter, stage=:finalize)
+    info = (; x=full_x, converged, tol, residual_norms=to_cpu(full_residuals), n_iter, maxiter, stage=:finalize)
     callback(info)
     info
 end
