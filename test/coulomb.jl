e3f47acde22d21d89c1a04882e70b436e2db943b
Michael F. Herbst <info@michael-herbst.com>
2026-02-20 16:33:27 +0100

    Coulomb energy reference tests

--- test/coulomb.jl
+++ test/coulomb.jl
@@ -1,50 +1,66 @@
 @testitem "Reference tests for exx implementations" tags=[:exx] setup=[TestCases] begin
     using DFTK
-    using DFTK: exx_energy_only
+    using DFTK: exx_energy_only, compute_coulomb_kernel
     using .TestCases: silicon
+    using LinearAlgebra
 
-    # TODO: This is a bad test, better test properties, such as the fact that
-    #       the Coulomb energy converges to the same value as the supercell / k-grid
-    #       converges to larger values.
+    # TODO: This is a bad test, better test properties, see details at the end of file
 
     Si = ElementPsp(14, load_psp(silicon.psp_upf))
     model  = model_DFT(silicon.lattice, [Si, Si], silicon.positions; functionals=PBE())
     basis  = PlaneWaveBasis(model; Ecut=10, kgrid=(1, 1, 1))
-    scfres = self_consistent_field(basis; tol=1e-10, #=callback=identity=#)
+    scfres = self_consistent_field(basis; tol=1e-10, callback=identity)
 
     n_occ = 4
     kpt  = basis.kpoints[1]
     ψk   = scfres.ψ[1][:, 1:n_occ]
     occk = scfres.occupation[1][1:n_occ]
-    ψk_real = similar(ψk, complex(T), basis.fft_size..., n_occ)
+    ψk_real = similar(ψk, eltype(ψk), basis.fft_size..., n_occ)
     @views for n = 1:n_occ
         ifft!(ψk_real[:, :, :, n], basis, kpt, ψk[:, n])
     end
 
-    k_neglect = compute_coulomb_kernel(basis; coulomb_kernel_model=NeglectSingularity())
-    @testset "NeglectSingularity" begin
-        E_neglect = exx_energy_only(basis, kpt, k_neglect, ψk_real, occk)
-        E_ref = 0.0
-        @test abs(E_neglect - E_ref) < 1e-6
+    k_probe = compute_coulomb_kernel(basis; coulomb_kernel_model=ProbeCharge())
+    @testset "ProbeCharge" begin
+        E_probe = exx_energy_only(basis, kpt, k_probe, ψk_real, occk)
+        E_ref = -2.3383063575660987
+        @test abs(E_ref - E_probe) < 1e-6
     end
 
+    @testset "NeglectSingularity" begin
+        k_neglect = compute_coulomb_kernel(basis; coulomb_kernel_model=NeglectSingularity())
+        E_neglect = exx_energy_only(basis, kpt, k_neglect, ψk_real, occk)
+        E_ref = -0.7349457693125514
+        @test abs(E_ref - E_neglect) < 1e-6
+        @test norm(k_neglect[2:end] - k_probe[2:end]) < 1e-6
+    end
 
+    @testset "SphericallyTruncated" begin
+        k_strunc = compute_coulomb_kernel(basis; coulomb_kernel_model=SphericallyTruncated())
+        E_strunc = exx_energy_only(basis, kpt, k_strunc, ψk_real, occk)
+        E_ref = -2.360166200435632
+        @test abs(E_ref - E_strunc) < 1e-6
 
+        # TODO: Test this gives a spherically truncated function.
+    end
+
+    @testset "WignerSeitzTruncated" begin
+        k_wtrunc = compute_coulomb_kernel(basis; coulomb_kernel_model=WignerSeitzTruncated())
+        E_wtrunc = exx_energy_only(basis, kpt, k_wtrunc, ψk_real, occk)
+        E_ref = -2.345681352379346
+        @test abs(E_ref - E_wtrunc) < 1e-6
+    end
+
+    @testset "VoxelAveraged" begin
+        k_wtrunc = compute_coulomb_kernel(basis; coulomb_kernel_model=VoxelAveraged())
+        E_wtrunc = exx_energy_only(basis, kpt, k_wtrunc, ψk_real, occk)
+        E_ref = -2.2491082534455376
+        @test abs(E_ref - E_wtrunc) < 1e-6
+    end
 end
 
-
-
-
-
-# TODO: Put some tests here comparing the different coulomb models
-#       e.g. all coulomb models should have the same large-q behaviour
-#
-# Test type stability
-
-# Test reference values for all kernels ?
-
-#=
-Test that probe charge model gives the same elements but the first
-
-Test that spherically truncated gives a truncated function ?
-=#
+# TODO: Tests ot include in the future are
+#       - Test for type stability (i.e. when Float32 is used)
+#       - Test properties, such as the fact that
+#         the Coulomb energy converges to the same value as the supercell / k-grid
+#         more suprecells are used
