JuliaMolSim/DFTK.jl

commit a1308a1fa8e6e3ab92de4c1e40c77e75865a3738

Consistent used of basis.comm_kpts (#1241)
Co-authored-by: Michael F. Herbst <info@michael-herbst.com>

src/response/hessian.jl
@@ -76,7 +76,7 @@ function ResponseCallback()
     ResponseCallback(Ref(zero(UInt64)))
 end
 function (cb::ResponseCallback)(info)
-    mpi_master() || return info  # Only print on master
+    mpi_master(info.basis.comm_kpts) && return info  # Only print on master
 
     if info.stage == :finalize
         info.converged || @warn "solve_ΩplusK not converged."
@@ -162,7 +162,7 @@ that is return δψ where (Ω+K) δψ = -δHextψ.
         weighted_ksum(basis, [real(dot(δψx[ik], δψy[ik])) for ik in 1:length(basis.kpoints)])
     end
     res = cg(J, -δHextψ_pack; precon=FunctionPreconditioner(f_ldiv!), proj, tol,
-             callback, my_dot=weighted_dot)
+             callback=info -> callback(merge(info, (; basis=basis))), my_dot=weighted_dot)
     (; δψ=unpack(res.x), res.converged, res.tol, res.residual_norm,
      res.n_iter)
 end
@@ -177,6 +177,10 @@ function OmegaPlusKDefaultCallback(; show_σmin=false, show_time=true)
 end
 function (cb::OmegaPlusKDefaultCallback)(info)
     io = stdout
+    # Default to MPI.COMM_WORLD for logging if basis not provided
+    comm = MPI.COMM_WORLD
+    haskey(info, :basis) && (comm = info.basis.comm_kpts)
+
     avgCG = 0.0
     if haskey(info, :Axinfos) && haskey(first(info.Axinfos), :n_iter)
         # Axinfo: NamedTuple returned by mul_inexact(::DielectricAdjoint, ...)
@@ -186,10 +190,10 @@ function (cb::OmegaPlusKDefaultCallback)(info)
         avgCG = sum(info.Axinfos) do Axinfo
             mean(sum, Axinfo.n_iter)
         end
-        avgCG = mpi_mean(avgCG, first(info.Axinfos).basis.comm_kpts)
+        avgCG = mpi_mean(avgCG, comm)
     end
 
-    !mpi_master() && return info  # Rest is printing => only do on master
+    !mpi_master(comm) && return info  # Rest is printing => only do on master
 
     show_time  = (hasproperty(info, :runtime_ns) && cb.show_time)
     label_time = show_time    ? ("  Δtime ", "  ------", " "^8) : ("", "", "")
@@ -295,8 +299,8 @@ Input parameters:
                            maxiter=maxiter_sternheimer, tol=tol * factor_initial,
                            bandtolalg, occupation_threshold,
                            q, kwargs...)  # = χ04P * δHext
-        callback((; stage=:noninteracting, runtime_ns=time_ns() - start_ns,
-                    Axinfos=[(; basis, tol=tol*factor_initial, res0...)]))
+        callback((; stage=:noninteracting, runtime_ns=time_ns() - start_ns, basis,
+                    Axinfos=[(; tol=tol*factor_initial, res0...)]))
         compute_δρ(basis, ψ, res0.δψ, occupation, res0.δoccupation;
                    occupation_threshold, q)
     end
@@ -309,7 +313,7 @@ Input parameters:
         Pδρ .= vec(mix_density(mixing, basis, reshape(δρ, size(ρ));
                                ham, basis, ρin=ρ, εF, eigenvalues, ψ))
     end
-    callback_inner(info) = callback(merge(info, (; runtime_ns=time_ns() - start_ns)))
+    callback_inner(info) = callback(merge(info, (; runtime_ns=time_ns() - start_ns, basis=basis)))
     info_gmres = inexact_gmres(ε_adj, vec(δρ0);
                                tol, precon, krylovdim, maxiter, s,
                                callback=callback_inner, kwargs...)
@@ -337,8 +341,8 @@ Input parameters:
                            δtemperature,
                            maxiter=maxiter_sternheimer, tol=tol * factor_final,
                            bandtolalg, occupation_threshold, q, kwargs...)
-    callback((; stage=:final, runtime_ns=time_ns() - start_ns,
-                Axinfos=[(; basis, tol=tol*factor_final, resfinal...)]))
+    callback((; stage=:final, runtime_ns=time_ns() - start_ns, basis,
+                Axinfos=[(; tol=tol*factor_final, resfinal...)]))
     # Compute total change in eigenvalues
     δeigenvalues = map(ψ, δHtotψ) do ψk, δHtotψk
         map(eachcol(ψk), eachcol(δHtotψk)) do ψnk, δHtotψnk
@@ -392,7 +396,7 @@ end
                    ε_adj.bandtolalg, ε_adj.q, ε_adj.maxiter, kwargs...)
     χ0δV = res.δρ
     Ax = vec(δρ - χ0δV)  # (1 - χ0 K) δρ
-    (; Ax, info=(; rtol, basis, res...))
+    (; Ax, info=(; rtol, res...))
 end
 function size(ε_adj::DielectricAdjoint, i::Integer)
     if 1 ≤ i ≤ 2

ext/DFTKGeometryOptimizationExt.jl
@@ -2,6 +2,7 @@ module DFTKGeometryOptimizationExt
 using DFTK
 using Unitful
 using UnitfulAtomic
+using MPI
 import GeometryOptimization
 GO = GeometryOptimization
 
@@ -18,9 +19,10 @@ function GO.minimize_energy!(system, calc::DFTKCalculator, solver;
                              tol_forces=1e-4u"eV/Å",
                              tol_virial=1e-6u"eV",
                              verbosity::Integer=0,
-                             # Verbosity is 0 (silent) unless we are on master
-                             callback=GO.GeoOptDefaultCallback(mpi_master() * verbosity;
-                                                               show_virial=variablecell),
+                             # Verbosity is 0 (silent) unless we are on master.
+                             callback=GO.GeoOptDefaultCallback(
+                                mpi_master(MPI.COMM_WORLD) * verbosity;
+                                show_virial=variablecell),
                              kwargs...)
     if autoadjust_calculator
         has_modified_parameter = any(haskey(calc.params.scf_kwargs, key)

ext/DFTKJLD2Ext.jl
@@ -1,6 +1,7 @@
 module DFTKJLD2Ext
 using DftFunctionals
 using DFTK
+using DFTK: mpi_barrier
 using JLD2
 using MPI
 
@@ -8,7 +9,7 @@ DFTK.make_subdict!(jld::Union{JLD2.Group,JLD2.JLDFile}, name::AbstractString) = 
 
 function save_jld2(to_dict_function!, file::AbstractString, scfres::NamedTuple;
                    save_ψ=true, save_ρ=true, extra_data=Dict{String,Any}(), compress=false)
-    if mpi_master()
+    if mpi_master(scfres.basis.comm_kpts)
         JLD2.jldopen(file * ".new", "w"; compress) do jld
             to_dict_function!(jld, scfres; save_ψ, save_ρ)
             for (k, v) in pairs(extra_data)
@@ -25,7 +26,7 @@ function save_jld2(to_dict_function!, file::AbstractString, scfres::NamedTuple;
         dummy = Dict{String,Any}()
         to_dict_function!(dummy, scfres; save_ψ)
     end
-    MPI.Barrier(MPI.COMM_WORLD)
+    mpi_barrier(scfres.basis.comm_kpts)
     nothing
 end
 function DFTK.save_scfres(::Val{:jld2}, file::AbstractString, scfres::NamedTuple; kwargs...)
@@ -35,8 +36,8 @@ function DFTK.save_bands(::Val{:jld2}, file::AbstractString, band_data::NamedTup
     save_jld2(DFTK.band_data_to_dict!, file, band_data; kwargs...)
 end
 
-function load_basis(jld)
-    if mpi_master()
+function load_basis(jld; comm=MPI.COMM_WORLD)
+    if mpi_master(comm)
         basis_args = (jld["model"],
                       jld["Ecut"],
                       jld["fft_size"],
@@ -47,29 +48,31 @@ function load_basis(jld)
     else
         basis_args = nothing
     end
-    basis_args = MPI.bcast(basis_args, 0, MPI.COMM_WORLD)
-    PlaneWaveBasis(basis_args..., MPI.COMM_WORLD, DFTK.CPU())
+    basis_args = DFTK.mpi_bcast(basis_args, 0, comm)
+    PlaneWaveBasis(basis_args..., comm, DFTK.CPU())
 end
 
 
-function DFTK.load_scfres(::Val{:jld2}, filename::AbstractString, basis=nothing; kwargs...)
-    if mpi_master()
+function DFTK.load_scfres(::Val{:jld2}, filename::AbstractString, basis=nothing;
+                          comm=isnothing(basis) ? MPI.COMM_WORLD : basis.comm_kpts,
+                          kwargs...)
+    if mpi_master(comm)
         scfres = JLD2.jldopen(filename, "r") do jld
-            load_scfres_jld2(jld, basis; kwargs...)
+            load_scfres_jld2(jld, basis; comm, kwargs...)
         end
     else
-        scfres = load_scfres_jld2(nothing, basis; kwargs...)
+        scfres = load_scfres_jld2(nothing, basis; comm, kwargs...)
     end
-    MPI.Barrier(MPI.COMM_WORLD)
+    DFTK.mpi_barrier(comm)
     scfres
 end
-function load_scfres_jld2(jld, basis; skip_hamiltonian, strict)
+function load_scfres_jld2(jld, basis; skip_hamiltonian, strict, comm)
     consistent_kpts = true
     if isnothing(basis)
-        basis = load_basis(jld)
+        basis = load_basis(jld; comm)
     else
         errormsg = ""
-        if mpi_master()
+        if mpi_master(comm)
             # Check custom basis for consistency with the data extracted here
             if !(basis.architecture isa DFTK.CPU)  # TODO Else need to put things on the GPU
                 errormsg = "Only CPU architectures supported for now."
@@ -91,12 +94,12 @@ function load_scfres_jld2(jld, basis; skip_hamiltonian, strict)
                             "($(basis.model.n_spin_components))")
             end
         end
-        errormsg = MPI.bcast(errormsg, 0, MPI.COMM_WORLD)
+        errormsg = DFTK.mpi_bcast(errormsg, 0, comm)
         isempty(errormsg) || error(errormsg)
     end
 
     propmap = Dict(:damping_value => :α, )  # compatibility mapping
-    if mpi_master()
+    if mpi_master(comm)
         # Setup default energies
         e_keys   = filter!(!isequal("total"), collect(keys(jld["energies"])))
         e_values = [jld["energies"][k] for k in e_keys]
@@ -114,11 +117,11 @@ function load_scfres_jld2(jld, basis; skip_hamiltonian, strict)
     else
         scfdict = nothing
     end
-    scfdict = MPI.bcast(scfdict, 0, MPI.COMM_WORLD)
+    scfdict = DFTK.mpi_bcast(scfdict, 0, comm)
     scfdict[:basis] = basis
 
     function reshape_and_scatter(jld, key)
-        if mpi_master()
+        if mpi_master(comm)
             data = jld[key]
             n = ndims(data)
             data = reshape(data, size(data)[1:n-2]..., size(data, n-1) * size(data, n))
@@ -135,8 +138,8 @@ function load_scfres_jld2(jld, basis; skip_hamiltonian, strict)
         scfdict[:occupation]  = reshape_and_scatter(jld, "occupation")
     end
 
-    has_ψ = mpi_master() ? (consistent_kpts && haskey(jld, "ψ")) : nothing
-    has_ψ = MPI.bcast(has_ψ, 0, MPI.COMM_WORLD)
+    has_ψ = mpi_master(comm) ? (consistent_kpts && haskey(jld, "ψ")) : nothing
+    has_ψ = DFTK.mpi_bcast(has_ψ, 0, comm)
     if has_ψ
         n_G_vectors = reshape_and_scatter(jld, "kpt_n_G_vectors")
         basisok = all(n_G_vectors[ik] == length(DFTK.G_vectors(basis, kpt))
@@ -162,7 +165,7 @@ function load_scfres_jld2(jld, basis; skip_hamiltonian, strict)
         scfdict[:ham]      = ham
     end
 
-    MPI.Barrier(MPI.COMM_WORLD)
+    DFTK.mpi_barrier(comm)
     (; scfdict...)
 end
 

ext/DFTKJSON3Ext.jl
@@ -1,7 +1,6 @@
 module DFTKJSON3Ext
 using DFTK
 using JSON3
-using MPI
 
 function save_json(todict_function, filename::AbstractString, scfres::NamedTuple;
                    save_ψ=false, extra_data=Dict{String,Any}(), save_ρ=true, kwargs...)
@@ -12,13 +11,13 @@ function save_json(todict_function, filename::AbstractString, scfres::NamedTuple
     for (k, v) in pairs(extra_data)
         data[k] = v
     end
-    if mpi_master()
+    if mpi_master(scfres.basis.comm_kpts)
         open(filename * ".new", "w") do io
             JSON3.write(io, data)
         end
         mv(filename * ".new", filename; force=true)
     end
-    MPI.Barrier(MPI.COMM_WORLD)
+    DFTK.mpi_barrier(scfres.basis.comm_kpts)
     nothing
 end
 function DFTK.save_scfres(::Val{:json}, filename::AbstractString, scfres::NamedTuple; kwargs...)

ext/DFTKPlotsExt.jl
@@ -21,7 +21,8 @@ function plot_bandstructure(band_data::NamedTuple;
                             unit=u"hartree", kwargs_plot=(; ), kwargs...)
     # TODO Replace by a plot recipe once BandData is its own type.
 
-    mpi_nprocs() > 1 && error("Band structure plotting with MPI not supported yet")
+    mpi_nprocs(band_data.basis.comm_kpts) > 1 &&
+        error("Band structure plotting with MPI not supported yet")
 
     if !haskey(band_data, :kinter)
         @warn("Calling plot_bandstructure without first computing the band data " *

ext/DFTKWriteVTKExt.jl
@@ -9,7 +9,7 @@ function DFTK.save_scfres(::Val{:vts}, filename::AbstractString, scfres::NamedTu
                           save_ψ=false, extra_data=Dict{String,Any}(), kwargs...)
     # Initialise the grid and open the file on master
     vtkfile = nothing
-    if mpi_master()
+    if mpi_master(scfres.basis.comm_kpts)
         grid = zeros(3, scfres.basis.fft_size...)
         for (idcs, r) in zip(CartesianIndices(scfres.basis.fft_size),
                              r_vectors_cart(scfres.basis))
@@ -23,7 +23,7 @@ function DFTK.save_scfres(::Val{:vts}, filename::AbstractString, scfres::NamedTu
         ψblock_dist = blockify_ψ(scfres.basis, scfres.ψ).ψ
         ψblock = gather_kpts_block(scfres.basis, ψblock_dist)
 
-        if mpi_master()
+        if mpi_master(scfres.basis.comm_kpts)
             for ik = 1:length(basis.kpoints), n = 1:size(ψblock, 2)
                 kpt_n_G = length(G_vectors(basis, basis.kpoints[ik]))
                 ψnk_real = ifft(basis, basis.kpoints[ik], ψblock[1:kpt_n_G, n, ik])
@@ -34,7 +34,7 @@ function DFTK.save_scfres(::Val{:vts}, filename::AbstractString, scfres::NamedTu
         end
     end
 
-    if mpi_master()
+    if mpi_master(scfres.basis.comm_kpts)
         ρtotal, ρspin = total_density(scfres.ρ), spin_density(scfres.ρ)
         vtkfile["ρtotal"] = ρtotal
         if !isnothing(ρspin)
@@ -46,7 +46,7 @@ function DFTK.save_scfres(::Val{:vts}, filename::AbstractString, scfres::NamedTu
         WriteVTK.vtk_save(vtkfile)
     end
 
-    MPI.Barrier(MPI.COMM_WORLD)
+    DFTK.mpi_barrier(scfres.basis.comm_kpts)
     nothing
 end
 

src/PlaneWaveBasis.jl
@@ -539,7 +539,7 @@ function gather_kpts(basis::PlaneWaveBasis)
     # No need to allocate and setup a new basis object
     mpi_nprocs(basis.comm_kpts) == 1 && return basis
 
-    if mpi_master()
+    if mpi_master(basis.comm_kpts)
         PlaneWaveBasis(basis.model,
                        basis.Ecut,
                        basis.fft_size,
@@ -562,7 +562,7 @@ and save it in `dest` as a dense `(size(kdata[1])..., n_kpoints)` array. On the 
 """
 @views function gather_kpts_block!(dest, basis::PlaneWaveBasis, kdata::AbstractVector{A}) where {A}
     # Number of elements stored per k-point in `kdata` (as vector of arrays)
-    n_chunk = MPI.Bcast(length(kdata[1]), 0, basis.comm_kpts)
+    n_chunk = mpi_bcast(length(kdata[1]), 0, basis.comm_kpts)
     @assert all(length(k) == n_chunk for k in kdata)
 
     # Note: This function assumes that k-points are stored contiguously in rank-increasing
@@ -608,7 +608,7 @@ is a list over all k-points. On non-master processes `nothing` may be passed.
 function scatter_kpts_block(basis::PlaneWaveBasis, data::Union{Nothing,AbstractArray})
     T, N = (mpi_master(basis.comm_kpts) ? (eltype(data), ndims(data))
                                         : (nothing, nothing))
-    T, N = MPI.bcast((T, N), 0, basis.comm_kpts)
+    T, N = mpi_bcast((T, N), 0, basis.comm_kpts)
     splitted = Vector{Array{T,N-1}}(undef, length(basis.kpoints))
 
     for σ in 1:basis.model.n_spin_components
@@ -627,7 +627,7 @@ function scatter_kpts_block(basis::PlaneWaveBasis, data::Union{Nothing,AbstractA
             sendbuf = nothing
             chunkshape = nothing
         end
-        chunkshape = MPI.bcast(chunkshape, 0, basis.comm_kpts)
+        chunkshape = mpi_bcast(chunkshape, 0, basis.comm_kpts)
         destbuf = zeros(T, chunkshape..., length(basis.krange_thisproc[σ]))
 
         # Scatter and split

src/common/mpi.jl
@@ -4,9 +4,14 @@ import MPI
 """
 Number of processors used in MPI. Can be called without ensuring initialization.
 """
-mpi_nprocs(comm=MPI.COMM_WORLD) = (MPI.Init(); MPI.Comm_size(comm))
-mpi_master(comm=MPI.COMM_WORLD) = (MPI.Init(); MPI.Comm_rank(comm) == 0)
+mpi_nprocs(comm::MPI.Comm) = (MPI.Init(); MPI.Comm_size(comm))
+mpi_master(comm::MPI.Comm) = (MPI.Init(); MPI.Comm_rank(comm) == 0)
 
+# Calling MPI without explicit communcator is deprecated
+@deprecate mpi_nprocs() mpi_nprocs(MPI.COMM_WORLD)
+@deprecate mpi_master() mpi_master(MPI.COMM_WORLD)
+
+# Wrappers around standarf MPI operations. Avoid using MPI.COMM_WORLD unless strictly necessary.
 mpi_sum(  arr, comm::MPI.Comm) = MPI.Allreduce( arr,   +, comm)
 mpi_sum!( arr, comm::MPI.Comm) = MPI.Allreduce!(arr,   +, comm)
 mpi_min(  arr, comm::MPI.Comm) = MPI.Allreduce( arr, min, comm)
@@ -15,6 +20,10 @@ mpi_max(  arr, comm::MPI.Comm) = MPI.Allreduce( arr, max, comm)
 mpi_max!( arr, comm::MPI.Comm) = MPI.Allreduce!(arr, max, comm)
 mpi_mean( arr, comm::MPI.Comm) = mpi_sum(arr, comm) ./ mpi_nprocs(comm)
 mpi_mean!(arr, comm::MPI.Comm) = (mpi_sum!(arr, comm); arr ./= mpi_nprocs(comm))
+
+mpi_bcast(arr, comm::MPI.Comm; root::Int=0) = MPI.bcast(arr, root, comm)
+mpi_bcast(arr, root::Int, comm::MPI.Comm) = MPI.bcast(arr, root, comm)
+mpi_barrier(comm::MPI.Comm) = MPI.Barrier(comm)
 
 @static if Base.Sys.ARCH == :aarch64
     # Custom reduction operators are not natively supported on aarch64 (see

commit 943001f73128d07eb01728daee2f36a592da92a6

Fix printing only on the master rank
Regression introduced in https://github.com/JuliaMolSim/DFTK.jl/commit/a1308a1fa8e6e3ab92de4c1e40c77e75865a3738.

src/response/hessian.jl
@@ -82,7 +82,7 @@ function ResponseCallback()
     ResponseCallback(Ref(zero(UInt64)))
 end
 function (cb::ResponseCallback)(info)
-    mpi_master(info.basis.comm_kpts) && return info  # Only print on master
+    mpi_master(info.basis.comm_kpts) || return info  # Only print on master
 
     if info.stage == :finalize
         info.converged || @warn "solve_ΩplusK not converged."