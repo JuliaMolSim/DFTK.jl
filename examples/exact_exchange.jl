7a7779278f941cc69dfc1b952928fe58eb741115
Michael F. Herbst <info@michael-herbst.com>
2026-03-11 18:57:41 +0100

    Cleanup behind copilot (#1284)

--- examples/exact_exchange.jl
+++ examples/exact_exchange.jl
@@ -1,23 +1,26 @@
-# Very basic setup, useful for testing
+# # Exact exchange and Hybrid DFT
+#
+# This is a quick sketch how to run a simple hybrid DFT calculation using DFTK.
+#
+# !!! warning "Preliminary implementation"
+#     Hybrid-DFT is not yet performance-optimised and has only seen rudimentary testing
+#     so far. Further only Gamma point calculations are supported and the interface is not
+#     yet considered stable. We appreciate any issues, bug reports or PRs.
+#
+using AtomsBuilder
 using DFTK
 using PseudoPotentialData
 
-pd_pbe_family = PseudoFamily("dojo.nc.sr.pbe.v0_5.stringent.upf") 
-Si = ElementPsp(:Si, pd_pbe_family)
+pseudopotentials = PseudoFamily("dojo.nc.sr.pbe.v0_5.stringent.upf") 
+system = bulk(:Si)
 
-a = 10.26  # Silicon lattice constant in Bohr
-lattice = a / 2 * [[0 1 1.];
-                   [1 0 1.];
-                   [1 1 0.]]
-atoms     = [Si, Si]
-positions = [ones(3)/8, -ones(3)/8]
-
-model  = model_DFT(lattice, atoms, positions; functionals=PBE())
-basis  = PlaneWaveBasis(model; Ecut=20, kgrid=[1, 1, 1])
+# First perform a PBE calculation to get a good starting point
+model  = model_DFT(system; pseudopotentials, functionals=PBE())
+basis  = PlaneWaveBasis(model; Ecut=15, kgrid=[1, 1, 1])
 scfres = self_consistent_field(basis; tol=1e-6)
 
-# Run PBE0
-model  = model_DFT(lattice, atoms, positions; functionals=PBE0())
+# Then run PBE0, see also [PBE0](@ref) and [HybridFunctional](@ref) for more documentation.
+model  = model_DFT(system; pseudopotentials, functionals=PBE0())
 basis  = PlaneWaveBasis(model; basis.Ecut, basis.kgrid)
 scfres = self_consistent_field(basis; tol=1e-4, scfres.ρ, scfres.ψ, scfres.occupation,
                                scfres.eigenvalues, solver=DFTK.scf_damping_solver())
