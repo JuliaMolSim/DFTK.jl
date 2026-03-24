91d5be4dd45ccd9800340896124302007d603753
Bruno Ploumhans <13494793+Technici4n@users.noreply.github.com>
2026-03-24 15:26:51 +0100

    Fix phonon example and explain limitations (#1289)
    `unfold_bz` does not remove the symmetries which means that the computed density response for silicon is symmetrized and thus zero... Additionally I document the exact limitations of the implementation.

--- examples/phonons.jl
+++ examples/phonons.jl
@@ -5,7 +5,12 @@
 # !!! warning "Preliminary implementation"
 #     Practical phonon computations have only seen rudimentary testing as of now.
 #     As of now we do not yet recommend relying on this feature for production
-#     calculations. We appreciate any issues, bug reports or PRs.
+#     calculations. Some of the limitations are:
+#     - symmetries must be disabled (pass `symmetries=false` to the model)
+#     - only LDA functionals are supported
+#     - non-linear core corrections from the pseudopotentials are not supported
+#     - MPI parallelization over k-points is not supported (due to ``k`` and ``k+q`` interactions)
+#     We appreciate any issues, bug reports or PRs.
 #
 # First we run an SCF calculation.
 
@@ -14,14 +19,14 @@ using DFTK
 using PseudoPotentialData
 
 pseudopotentials = PseudoFamily("cp2k.nc.sr.lda.v0_1.semicore.gth")
-model  = model_DFT(bulk(:Si); pseudopotentials, functionals=LDA())
+## Make sure to disable symmetries:
+model  = model_DFT(bulk(:Si); pseudopotentials, functionals=LDA(), symmetries=false)
 basis  = PlaneWaveBasis(model; Ecut=10, kgrid=[4, 4, 4])
 scfres = self_consistent_field(basis, tol=1e-8);
 nothing  # hide
 
 # Next we compute the phonon modes at the q-point `[1/4, 1/4, 1/4]`.
 
-scfres = DFTK.unfold_bz(scfres)
 phret_q0 = @time DFTK.phonon_modes(scfres; q=[0.25, 0.25, 0.25]);
 nothing  # hide
 
