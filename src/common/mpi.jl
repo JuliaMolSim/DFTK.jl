5e666a56477239540e7becb5a5923b70a009797f
Augustin Bussy <augustin.bussy@cscs.ch>
2026-04-30 15:31:35 +0200

    Safe GPU-aware MPI communication

--- src/common/mpi.jl
+++ src/common/mpi.jl
@@ -11,20 +11,24 @@ mpi_master(comm::MPI.Comm) = (MPI.Init(); MPI.Comm_rank(comm) == 0)
 @deprecate mpi_nprocs() mpi_nprocs(MPI.COMM_WORLD)
 @deprecate mpi_master() mpi_master(MPI.COMM_WORLD)
 
-# Wrappers around standarf MPI operations. Avoid using MPI.COMM_WORLD unless strictly necessary.
-mpi_sum(  arr, comm::MPI.Comm) = MPI.Allreduce( arr,   +, comm)
-mpi_sum!( arr, comm::MPI.Comm) = MPI.Allreduce!(arr,   +, comm)
-mpi_min(  arr, comm::MPI.Comm) = MPI.Allreduce( arr, min, comm)
-mpi_min!( arr, comm::MPI.Comm) = MPI.Allreduce!(arr, min, comm)
-mpi_max(  arr, comm::MPI.Comm) = MPI.Allreduce( arr, max, comm)
-mpi_max!( arr, comm::MPI.Comm) = MPI.Allreduce!(arr, max, comm)
+# GPU-aware MPI requires device synchronization to avoid race conditions. No overhead on CPU.
+sync(arr::AbstractGPUArray) = synchronize_device(architecture(arr))
+sync(arr) = nothing
+
+# Wrappers around standard MPI operations. Avoid using MPI.COMM_WORLD unless strictly necessary.
+mpi_sum(  arr, comm::MPI.Comm) = (sync(arr); MPI.Allreduce( arr,   +, comm))
+mpi_sum!( arr, comm::MPI.Comm) = (sync(arr); MPI.Allreduce!(arr,   +, comm))
+mpi_min(  arr, comm::MPI.Comm) = (sync(arr); MPI.Allreduce( arr, min, comm))
+mpi_min!( arr, comm::MPI.Comm) = (sync(arr); MPI.Allreduce!(arr, min, comm))
+mpi_max(  arr, comm::MPI.Comm) = (sync(arr); MPI.Allreduce( arr, max, comm))
+mpi_max!( arr, comm::MPI.Comm) = (sync(arr); MPI.Allreduce!(arr, max, comm))
 mpi_mean( arr, comm::MPI.Comm) = mpi_sum(arr, comm) ./ mpi_nprocs(comm)
 mpi_mean!(arr, comm::MPI.Comm) = (mpi_sum!(arr, comm); arr ./= mpi_nprocs(comm))
 
-mpi_bcast(arr, comm::MPI.Comm; root::Int=0) = MPI.bcast(arr, root, comm)
-mpi_bcast(arr, root::Int, comm::MPI.Comm) = MPI.bcast(arr, root, comm)
-mpi_bcast!(arr, comm::MPI.Comm; root::Int=0) = MPI.Bcast!(arr, root, comm)
-mpi_bcast!(arr, root::Int, comm::MPI.Comm) = MPI.Bcast!(arr, root, comm)
+mpi_bcast(arr, comm::MPI.Comm; root::Int=0) = (sync(arr); MPI.bcast(arr, root, comm))
+mpi_bcast(arr, root::Int, comm::MPI.Comm) = (sync(arr); MPI.bcast(arr, root, comm))
+mpi_bcast!(arr, comm::MPI.Comm; root::Int=0) = (sync(arr); MPI.Bcast!(arr, root, comm))
+mpi_bcast!(arr, root::Int, comm::MPI.Comm) = (sync(arr); MPI.Bcast!(arr, root, comm))
 mpi_barrier(comm::MPI.Comm) = MPI.Barrier(comm)
 
 @static if Base.Sys.ARCH == :aarch64
