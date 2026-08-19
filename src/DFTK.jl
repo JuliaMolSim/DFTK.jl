d93dd4af55080a7125af204f6150f4fa44da8b25
Jona Mangold <jonamangold@gmx.net>
2026-08-19 11:46:00 +0200

    cleaned up and added comments

--- src/DFTK.jl
+++ src/DFTK.jl
@@ -171,7 +171,6 @@ export LdosMixing, HybridMixing, χ0Mixing
 export FixedBands, AdaptiveBands
 export ScfDampingSolver, ScfAndersonDensitySolver, ScfAndersonSolver
 export self_consistent_field, kwargs_scf_checkpoints
-#export self_consistent_field_agnostic
 export ScfConvergenceEnergy, ScfConvergenceDensity, ScfConvergenceForce
 export ScfSaveCheckpoints, ScfDefaultCallback, AdaptiveDiagtol
 export ResponseOptions
@@ -179,18 +178,15 @@ export direct_minimization
 export newton
 export load_scfres, save_scfres
 include("scf/chi0models.jl")
-#include("scf/state.jl")
 include("scf/mixing.jl")
 include("scf/acceleration.jl")
 include("scf/scf_solvers.jl")
 include("scf/nbands_algorithm.jl")
 include("scf/self_consistent_field.jl")
-#include("scf/self_consistent_field_agnostic.jl")
 include("scf/direct_minimization.jl")
 include("scf/newton.jl")
 include("scf/scfres.jl")
-include("scf/anderson.jl")
-#include("scf/pcdiis.jl")
+#include("scf/anderson.jl")
 include("scf/potential_mixing.jl")
 
 export symmetry_operations
